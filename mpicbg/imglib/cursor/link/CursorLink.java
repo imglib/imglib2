5e32725c6b1e283909ddc9d65b67822799e6c75a
Stephan <Stephan@mobilejimmy.(none)>
2010-02-19 12:07:33 +0100

    Added CursorLink Test, test if NullLink does not cost anything

---
+++ mpicbg/imglib/cursor/link/CursorLink.java
@@ -0,0 +1,8 @@
+package mpicbg.imglib.cursor.link;
+
+import mpicbg.imglib.cursor.LocalizableByDim;
+
+public interface CursorLink extends LocalizableByDim
+{
+
+}
---
+++ mpicbg/imglib/cursor/link/NullLink.java
@@ -0,0 +1,119 @@
+package mpicbg.imglib.cursor.link;
+
+import mpicbg.imglib.cursor.LocalizableCursor;
+
+public class NullLink implements CursorLink
+{
+	@Override
+	final public void bck(final int dim)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void fwd(final int dim)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void move(final int steps, final int dim)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void moveRel(final int[] position)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void moveTo(final LocalizableCursor<?> cursor)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void moveTo(final int[] position)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void setPosition(final LocalizableCursor<?> cursor)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void setPosition(final int[] position)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	final public void setPosition(final int position, final int dim)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getPosition(int[] position)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int[] getPosition()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPosition(int dim)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getPositionAsString()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fwd(long steps)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fwd()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
