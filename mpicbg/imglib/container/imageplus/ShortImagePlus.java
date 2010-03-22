bbfc85d93391cafcd7661a274710d18fefb4aae2
Stephan <Stephan@mobilejimmy.(none)>
2010-03-22 14:40:10 +0100

    Fixed inconsistend license authors

--- mpicbg/imglib/container/imageplus/ShortImagePlus.java
+++ mpicbg/imglib/container/imageplus/ShortImagePlus.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009--2010, Johannes Schindelin
+ * Copyright (c) 2009--2010, Stephan Preibisch & Johannes Schindelin
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
