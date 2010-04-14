8f05abb5a89718bdfa5028a8b684b7c42934f7d5
Larry Lindsey <llindsey@clm.utexas.edu>
2010-04-14 11:45:21 -0500

    Added static sobel operators

--- mpicbg/imglib/algorithm/roi/DirectConvolution.java
+++ mpicbg/imglib/algorithm/roi/DirectConvolution.java
@@ -1,17 +1,68 @@
 package mpicbg.imglib.algorithm.roi;
 
+
 import mpicbg.imglib.algorithm.ROIAlgorithm;
+import mpicbg.imglib.container.array.ArrayContainerFactory;
 import mpicbg.imglib.cursor.LocalizableByDimCursor;
 import mpicbg.imglib.cursor.special.RegionOfInterestCursor;
 import mpicbg.imglib.image.Image;
+import mpicbg.imglib.image.ImageFactory;
 import mpicbg.imglib.outside.OutsideStrategyFactory;
 import mpicbg.imglib.type.NumericType;
+import mpicbg.imglib.type.numeric.ShortType;
 
 public class DirectConvolution
 	<T extends NumericType<T>, R extends NumericType<R>, S extends NumericType<S>>
 		extends ROIAlgorithm<T, S>
 {
 
+	protected static void quickKernel2D(short[][] vals, Image<ShortType> kern)
+	{
+		final LocalizableByDimCursor<ShortType> cursor = kern.createLocalizableByDimCursor();
+		final int[] pos = new int[2];
+
+		for (int i = 0; i < vals.length; ++i)
+		{
+			for (int j = 0; j < vals[i].length; ++j)
+			{
+				pos[0] = i;
+				pos[1] = j;
+				cursor.setPosition(pos);
+				cursor.getType().set(vals[i][j]);
+			}
+		}
+		cursor.close();		
+	}
+	
+	public static Image<ShortType> sobelVertical()
+	{
+		final ImageFactory<ShortType> factory = new ImageFactory<ShortType>(new ShortType(),
+				new ArrayContainerFactory());
+		final Image<ShortType> sobel = factory.createImage(new int[]{3, 3}, "Vertical Sobel");
+		final short[][] vals = {{-1, -2, -1},
+				{0, 0, 0},
+				{1, 2, 1}};
+		
+		quickKernel2D(vals, sobel);		
+		
+		return sobel;
+	}
+	
+	public static Image<ShortType> sobelHorizontal()
+	{
+		final ImageFactory<ShortType> factory = new ImageFactory<ShortType>(new ShortType(),
+				new ArrayContainerFactory());
+		final Image<ShortType> sobel = factory.createImage(new int[]{3, 3}, "Horizontal Sobel");
+		final short[][] vals = {{1, 0, -1},
+				{2, 0, -2},
+				{1, 0, -1}};
+		
+		quickKernel2D(vals, sobel);		
+		
+		return sobel;
+	}
+	
+	
 	private final Image<R> kernel;
 	private final int[] kernelSize;
 	private LocalizableByDimCursor<S> outputImageCursor;
