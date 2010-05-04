d34e96317203549c5e99a923a411a27f42def19b
Curtis Rueden <ctrueden@wisc.edu>
2010-05-04 10:54:00 -0500

    Support for NIO-backed arrays in array containers.

---
+++ mpicbg/imglib/container/basictypecontainer/array/NIOByteArray.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2009--2010, Stephan Preibisch & Stephan Saalfeld
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.  Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.  Neither the name of the Fiji project nor
+ * the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Rick Lentz
+ */
+package mpicbg.imglib.container.basictypecontainer.array;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import mpicbg.imglib.container.basictypecontainer.ByteAccess;
+
+public class NIOByteArray implements ArrayDataAccess<NIOByteArray>, ByteAccess
+{
+	protected ByteBuffer data;
+	
+	public NIOByteArray( final int numEntities )
+	{
+		this.data = ByteBuffer.allocateDirect(numEntities * 4).order( ByteOrder.nativeOrder() );
+	}
+    		
+	public NIOByteArray( final byte[] data )
+	{
+		ByteBuffer bufferIn = ByteBuffer.wrap( data );
+		ByteBuffer copy = ByteBuffer.allocateDirect( bufferIn.capacity() ).order( ByteOrder.nativeOrder() );
+		this.data = copy.put( bufferIn );
+	}
+
+	@Override
+	public void close() { data = null; }
+
+	@Override
+	public byte getValue( final int index )
+	{
+		return data.get( index );
+	}
+
+	@Override
+	public void setValue( final int index, final byte value )
+	{
+		data.put(index, value);		
+	}
+	
+	public byte[] getCurrentStorageArray()
+	{ 		  
+		byte[] outData = new byte[ data.capacity() ];
+		data.get( outData );
+		return outData;
+	}
+	
+	@Override
+	public NIOByteArray createArray( final int numEntities ) { return new NIOByteArray( numEntities ); }
+}
