51b224afa6afac20cdc17ec1dcd7f540f8f5a3d3
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2010-05-06 15:00:10 +0200

    simplified ArrayLocalizableCursor.fwd()

--- mpicbg/imglib/cursor/array/ArrayLocalizableCursor.java
+++ mpicbg/imglib/cursor/array/ArrayLocalizableCursor.java
@@ -24,8 +24,6 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
- * @author Stephan Preibisch & Stephan Saalfeld
  */
 package mpicbg.imglib.cursor.array;
 
@@ -37,84 +35,85 @@ import mpicbg.imglib.image.Image;
 import mpicbg.imglib.type.Type;
 import mpicbg.imglib.type.label.FakeType;
 
-public class ArrayLocalizableCursor<T extends Type<T>> extends AbstractLocalizableIterableCursor<T>
+/**
+ * 
+ * @param <T>
+ *
+ * @author Stephan Preibisch and Stephan Saalfeld
+ */
+public class ArrayLocalizableCursor< T extends Type< T >> extends AbstractLocalizableIterableCursor< T >
 {
 	protected final T type;
-	protected final Array<T,?> container;
+
+	protected final Array< T, ? > container;
+
 	protected final int sizeMinus1;
-	
-	public ArrayLocalizableCursor( final Array< T, ? > container, final Image< T > image ) 
+
+	public ArrayLocalizableCursor( final Array< T, ? > container, final Image< T > image )
 	{
 		super( container, image );
 
 		this.container = container;
 		this.type = container.createLinkedType();
-		this.sizeMinus1 = (int)container.getNumPixels() - 1;
-		
+		this.sizeMinus1 = ( int )container.getNumPixels() - 1;
+
 		reset();
-	}	
-	
-	public static ArrayLocalizableCursor<FakeType> createLinearCursor( final int[] dim )
+	}
+
+	public static ArrayLocalizableCursor< FakeType > createLinearCursor( final int[] dim )
 	{
-		final Array<FakeType, FakeAccess> array = new Array<FakeType, FakeAccess>( null, new FakeArray(), dim, 1 );
+		final Array< FakeType, FakeAccess > array = new Array< FakeType, FakeAccess >( null, new FakeArray(), dim, 1 );
 		array.setLinkedType( new FakeType() );
-		return new ArrayLocalizableCursor<FakeType>( array, null );
+		return new ArrayLocalizableCursor< FakeType >( array, null );
 	}
 
 	@Override
-	public T type() { return type; }
+	public T type(){ return type; }
 
 	@Override
 	public boolean hasNext(){ return type.getIndex() < sizeMinus1; }
-	
+
 	@Override
 	public void fwd()
-	{ 
+	{
 		type.incIndex();
-		
-		for ( int d = 0; d < numDimensions; d++ )
+
+		for ( int d = 0; d < numDimensions; ++d )
 		{
-			if ( position[ d ] < dimensions[ d ] - 1 )
-			{
-				position[ d ]++;
-				
-				for ( int e = 0; e < d; e++ )
-					position[ e ] = 0;
-				
-				break;
-			}
+			if ( ++position[ d ] >= dimensions[ d ] ) position[ d ] = 0;
+			else break;
 		}
-		
+
 		linkedIterator.fwd();
 	}
 
 	@Override
 	public void jumpFwd( final long steps )
-	{	
-		type.incIndex( (int)steps );		
+	{
+		type.incIndex( ( int ) steps );
 		container.indexToPosition( type.getIndex(), position );
-		
+
 		linkedIterator.jumpFwd( steps );
 	}
-	
+
 	@Override
 	public void reset()
 	{
 		if ( dimensions != null )
 		{
 			type.updateIndex( -1 );
-			
+
 			position[ 0 ] = -1;
-			
+
 			for ( int d = 1; d < numDimensions; d++ )
 				position[ d ] = 0;
-			
+
 			type.updateContainer( this );
 		}
-		
+
 		linkedIterator.reset();
 	}
 
 	@Override
-	public Array<T,?> getContainer(){ return container; }
+	public Array< T, ? > getContainer(){ return container; }
 }
