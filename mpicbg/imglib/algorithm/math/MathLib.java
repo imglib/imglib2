fe42310445239976450893101c7a3cbd2b8b3464
Stephan <Stephan@mobilejimmy.(none)>
2010-07-26 13:17:31 +0200

    Added function to convert into Matrix4f

--- mpicbg/imglib/algorithm/math/MathLib.java
+++ mpicbg/imglib/algorithm/math/MathLib.java
@@ -17,6 +17,7 @@
 package mpicbg.imglib.algorithm.math;
 
 import javax.media.j3d.Transform3D;
+import javax.vecmath.Matrix4f;
 
 import mpicbg.models.AffineModel3D;
 import mpicbg.models.CoordinateTransform;
@@ -432,6 +433,33 @@ public class MathLib
 		out += ")";
 		
 		return out;
+	}
+
+	public static Matrix4f getMatrix4f( final AffineModel3D model )
+	{
+		final Matrix4f matrix = new Matrix4f();
+		
+		final float[] m = new float[ 12 ];
+		model.getMatrix( m );
+		
+		matrix.m00 = m[ 0 ];
+		matrix.m01 = m[ 1 ];
+		matrix.m02 = m[ 2 ];
+		matrix.m03 = m[ 3 ];
+		matrix.m10 = m[ 4 ];
+		matrix.m11 = m[ 5 ];
+		matrix.m12 = m[ 6 ];
+		matrix.m13 = m[ 7 ];
+		matrix.m20 = m[ 8 ];
+		matrix.m21 = m[ 9 ];
+		matrix.m22 = m[ 10 ];
+		matrix.m23 = m[ 11 ];
+		matrix.m30 = 0;
+		matrix.m31 = 0;
+		matrix.m32 = 0;
+		matrix.m33 = 0;
+		
+		return matrix;
 	}
 	
 	public static Transform3D getTransform3D( AffineModel3D model )
