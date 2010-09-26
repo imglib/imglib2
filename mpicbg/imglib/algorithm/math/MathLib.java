9c09d5903255c0cdb4a2a7e101c8764e174f46d1
Stephan Preibisch <preibisch@mpi-cbg.de>
2010-09-26 21:04:06 +0200

    Small changes in gauss kernel creation method

--- mpicbg/imglib/algorithm/math/MathLib.java
+++ mpicbg/imglib/algorithm/math/MathLib.java
@@ -373,7 +373,7 @@ public class MathLib
     public static double[] createGaussianKernel1DDouble( final double sigma, final boolean normalize )
     {
             int size = 3;
-            double[] gaussianKernel;
+            final double[] gaussianKernel;
 
             if (sigma <= 0)
             {
@@ -384,12 +384,12 @@ public class MathLib
             {
                     size = Math.max(3, (int) (2 * (int) (3 * sigma + 0.5) + 1));
 
-                    double two_sq_sigma = 2 * sigma * sigma;
+                    final double two_sq_sigma = 2 * sigma * sigma;
                     gaussianKernel = new double[size];
 
                     for (int x = size / 2; x >= 0; --x)
                     {
-                            double val = Math.exp( -(x * x) / two_sq_sigma);
+                            final double val = Math.exp( -(x * x) / two_sq_sigma);
 
                             gaussianKernel[size / 2 - x] = val;
                             gaussianKernel[size / 2 + x] = val;
@@ -402,7 +402,7 @@ public class MathLib
                     for (double value : gaussianKernel)
                             sum += value;
 
-                    for (int i = 0; i < gaussianKernel.length; i++)
+                    for (int i = 0; i < gaussianKernel.length; ++i)
                             gaussianKernel[i] /= sum;
             }
 
