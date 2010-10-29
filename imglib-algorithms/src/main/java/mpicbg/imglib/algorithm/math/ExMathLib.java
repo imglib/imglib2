050feaabae11e49aaf19d129869e07735207e1ea
Stephan Preibisch <preibisch@mpi-cbg.de>
2010-10-29 09:54:25 +0200

    Removed dependency on Java3d

--- imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/math/ExMathLib.java
+++ imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/math/ExMathLib.java
@@ -16,11 +16,7 @@
  */
 package mpicbg.imglib.algorithm.math;
 
-import javax.media.j3d.Transform3D;
-import javax.vecmath.Matrix4f;
-
 import mpicbg.imglib.algorithm.MathLib;
-import mpicbg.models.AffineModel3D;
 import mpicbg.models.CoordinateTransform;
 
 public class ExMathLib
@@ -80,59 +76,4 @@ public class ExMathLib
 
 		return minMaxDim;
 	}
-
-	public static Matrix4f getMatrix4f( final AffineModel3D model )
-	{
-		final Matrix4f matrix = new Matrix4f();
-
-		final float[] m = new float[ 12 ];
-		model.getMatrix( m );
-
-		matrix.m00 = m[ 0 ];
-		matrix.m01 = m[ 1 ];
-		matrix.m02 = m[ 2 ];
-		matrix.m03 = m[ 3 ];
-		matrix.m10 = m[ 4 ];
-		matrix.m11 = m[ 5 ];
-		matrix.m12 = m[ 6 ];
-		matrix.m13 = m[ 7 ];
-		matrix.m20 = m[ 8 ];
-		matrix.m21 = m[ 9 ];
-		matrix.m22 = m[ 10 ];
-		matrix.m23 = m[ 11 ];
-		matrix.m30 = 0;
-		matrix.m31 = 0;
-		matrix.m32 = 0;
-		matrix.m33 = 0;
-
-		return matrix;
-	}
-
-	public static Transform3D getTransform3D( AffineModel3D model )
-	{
-		final Transform3D transform = new Transform3D();
-		final float[] m = model.getMatrix( null );
-
-		final float[] m2 = new float[ 16 ];
-		transform.get( m2 );
-
-		for ( int i = 0; i < m.length; ++i )
-			m2[ i ] = m[ i ];
-
-		transform.set( m2 );
-
-		return transform;
-	}
-
-	public static AffineModel3D getAffineModel3D( Transform3D transform )
-	{
-		final float[] m = new float[16];
-		transform.get( m );
-
-		AffineModel3D model = new AffineModel3D();
-		model.set( m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11] );
-
-		return model;
-	}
-
 }
