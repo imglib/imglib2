9d552c68d3d6f3be29892a5e821b6e2397ae322e
Curtis Rueden <ctrueden@wisc.edu>
2010-11-01 17:35:24 +0100

    Add ability to save results to a CSV file.

--- imglib-ij/src/test/java/tests/PerformanceBenchmark.java
+++ imglib-ij/src/test/java/tests/PerformanceBenchmark.java
@@ -1,8 +1,5 @@
 package tests;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import ij.ImagePlus;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
@@ -12,6 +9,16 @@ import ijaux.hypergeom.BaseIndex;
 import ijaux.hypergeom.PixelCube;
 import ijaux.iter.array.ByteForwardIterator;
 import ijaux.iter.seq.RasterForwardIterator;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import mpicbg.imglib.container.ContainerFactory;
 import mpicbg.imglib.container.array.Array;
 import mpicbg.imglib.container.array.ArrayContainerFactory;
@@ -37,23 +44,51 @@ import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
  */
 public class PerformanceBenchmark {
 
-	final byte[] rawData;
-	final ByteProcessor byteProc;
-	final Image<UnsignedByteType> imgArray;
-	final Image<UnsignedByteType> imgCell;
-	final Image<UnsignedByteType> imgPlanar;
-	final Image<UnsignedByteType> imgImagePlus;
-	final PixelCube<Byte, BaseIndex> pixelCube;
+	private static final boolean SAVE_RESULTS_TO_DISK = true;
 
-	public static void main(final String[] args) {
-		final PerformanceBenchmark bench = new PerformanceBenchmark();
-		bench.testPerformance(10);
+	private static final String METHOD_RAW = "Raw";
+	private static final String METHOD_IMAGEJ = "ImageJ";
+	private static final String METHOD_IMGLIB_ARRAY = "Imglib (Array)";
+	private static final String METHOD_IMGLIB_CELL = "Imglib (Cell)";
+	private static final String METHOD_IMGLIB_PLANAR = "Imglib (Planar)";
+	private static final String METHOD_IMGLIB_IMAGEPLUS = "Imglib (ImagePlus)";
+	private static final String METHOD_PIXLIB_REFLECTION = "Pixlib (Reflection)";
+	private static final String METHOD_PIXLIB_GENERIC = "Pixlib (Generic Array)";
+	private static final String METHOD_PIXLIB_BYTE = "Pixlib (Byte Array)";
+
+	private final int width, height;
+	private final byte[] rawData;
+	private final ByteProcessor byteProc;
+	private final Image<UnsignedByteType> imgArray;
+	private final Image<UnsignedByteType> imgCell;
+	private final Image<UnsignedByteType> imgPlanar;
+	private final Image<UnsignedByteType> imgImagePlus;
+	private final PixelCube<Byte, BaseIndex> pixelCube;
+
+	/**
+	 * List of timing results.
+	 *
+	 * Each element of the list represents an iteration.
+	 * Each entry maps the method name to the time measured.
+	 */
+	private final List<Map<String, Long>> results =
+		new ArrayList<Map<String, Long>>();
+
+	public static void main(final String[] args) throws IOException {
+		final int iterations = 10;
+		final int size;
+		if (args.length > 0) size = Integer.parseInt(args[0]);
+		else size = 4000;
+		final PerformanceBenchmark bench = new PerformanceBenchmark(size);
+		bench.testPerformance(iterations);
 		System.exit(0);
 	}
 
 	/** Creates objects and measures memory usage. */
-	public PerformanceBenchmark() {
-		final int width = 4000, height = width;
+	public PerformanceBenchmark(final int imageSize) {
+		width = height = imageSize;
+		System.out.println();
+		System.out.println("===== " + width + " x " + height + " =====");
 
 		final List<Long> memUsage = new ArrayList<Long>();
 		memUsage.add(getMemUsage());
@@ -75,9 +110,67 @@ public class PerformanceBenchmark {
 		reportMemoryUsage(memUsage);
 	}
 
-	public void testPerformance(final int iterationCount) {
+	public void testPerformance(final int iterationCount) throws IOException {		
+		// initialize results map
+		results.clear();
+		for (int i = 0; i < iterationCount; i++) {
+			final Map<String, Long> entry = new HashMap<String, Long>();
+			results.add(entry);
+		}
 		testCheapPerformance(iterationCount);
+		if (SAVE_RESULTS_TO_DISK) saveResults("cheap");
 		testExpensivePerformance(iterationCount);
+		if (SAVE_RESULTS_TO_DISK) saveResults("expensive");
+	}
+
+	/**
+	 * Saves benchmark results to the given CSV file on disk.
+	 *
+	 * The motivation is to produce two charts:
+	 *
+	 * 1) performance by iteration number (on each size of image)
+	 *    one line graph per method
+	 *    X axis = iteration number
+	 *    Y axis = time needed
+	 *
+	 * 2) average performance by image size (for first iteration, and 10th)
+	 *    one line graph per method
+	 *    X axis = size of image
+	 *    Y axis = time needed
+	 *
+	 * The CSV file produced enables graph #1 very easily.
+	 * For graph #2, results from several files must be combined.
+	 */
+	public void saveResults(final String prefix) throws IOException {
+		final StringBuilder sb = new StringBuilder();
+
+		// write header
+		final Map<String, Long> firstEntry = results.get(0);
+		final String[] methods = firstEntry.keySet().toArray(new String[0]);
+		Arrays.sort(methods);
+		sb.append("Iteration");
+		for (final String method : methods) {
+			sb.append("\t");
+			sb.append(method);
+		}
+		sb.append("\n");
+
+		// write data
+		for (int iter = 0; iter < results.size(); iter++) {
+			final Map<String, Long> entry = results.get(iter);
+			sb.append(iter + 1);
+			for (String method : methods) {
+				sb.append("\t");
+				sb.append(entry.get(method));
+			}
+			sb.append("\n");
+		}
+
+		// write to disk
+		final String path = "results-" + prefix + "-" + width + "x" + height + ".csv";
+		final PrintWriter out = new PrintWriter(new FileWriter(path));
+		out.print(sb.toString());
+		out.close();
 	}
 
 	// -- Helper methods --
@@ -103,14 +196,14 @@ public class PerformanceBenchmark {
 			times.add(System.currentTimeMillis());
 			invertImagePlusImage(imgImagePlus);
 			times.add(System.currentTimeMillis());
-			invertPixelCubeRefl(pixelCube);
+			invertPixelCubeReflection(pixelCube);
 			times.add(System.currentTimeMillis());
 			invertPixelCubeGenArray(pixelCube);
 			times.add(System.currentTimeMillis());
 			invertPixelCubeByteArray(pixelCube);
 			times.add(System.currentTimeMillis());
 
-			reportTimePerformance(times);
+			logTimePerformance(i, times);
 		}
 	}
 
@@ -135,14 +228,14 @@ public class PerformanceBenchmark {
 			times.add(System.currentTimeMillis());
 			randomizeImagePlusImage(imgImagePlus);
 			times.add(System.currentTimeMillis());
-			randomizePixelCubeRefl(pixelCube);
+			randomizePixelCubeReflection(pixelCube);
 			times.add(System.currentTimeMillis());
 			randomizePixelCubeGenArray(pixelCube);
 			times.add(System.currentTimeMillis());
 			randomizePixelCubeByteArray(pixelCube);
 			times.add(System.currentTimeMillis());
 
-			reportTimePerformance(times);
+			logTimePerformance(i, times);
 		}
 	}
 
@@ -163,16 +256,16 @@ public class PerformanceBenchmark {
 		final long pixLibMem          = computeDifference(memUsage);
 		System.out.println();
 		System.out.println("-- MEMORY OVERHEAD --");
-		System.out.println("Raw: " + rawMem + " bytes");
-		System.out.println("ImageJ: " + ipMem + " bytes");
-		System.out.println("Imglib (Array): " + imgLibArrayMem + " bytes");
-		System.out.println("Imglib (Cell): " + imgLibCellMem + " bytes");
-		System.out.println("Imglib (Planar): " + imgLibPlanarMem + " bytes");
-		System.out.println("Imglib (ImagePlus): " + imgLibImagePlusMem + " bytes");
+		System.out.println(METHOD_RAW + ": " + rawMem + " bytes");
+		System.out.println(METHOD_IMAGEJ + ": " + ipMem + " bytes");
+		System.out.println(METHOD_IMGLIB_ARRAY + ": " + imgLibArrayMem + " bytes");
+		System.out.println(METHOD_IMGLIB_CELL + ": " + imgLibCellMem + " bytes");
+		System.out.println(METHOD_IMGLIB_PLANAR + ": " + imgLibPlanarMem + " bytes");
+		System.out.println(METHOD_IMGLIB_IMAGEPLUS + ": " + imgLibImagePlusMem + " bytes");
 		System.out.println("PixLib: " + pixLibMem + " bytes");
 	}
 
-	private void reportTimePerformance(final List<Long> times) {
+	private void logTimePerformance(final int iter, final List<Long> times) {
 		final long rawTime             = computeDifference(times);
 		final long ipTime              = computeDifference(times);
 		final long imgLibArrayTime     = computeDifference(times);
@@ -182,15 +275,27 @@ public class PerformanceBenchmark {
 		final long pixLibReflTime      = computeDifference(times);
 		final long pixLibGenArrayTime  = computeDifference(times);
 		final long pixLibByteArrayTime = computeDifference(times);
-		reportTime("Raw", rawTime, rawTime, ipTime);
-		reportTime("ImageJ", ipTime, rawTime, ipTime);
-		reportTime("Imglib (Array)", imgLibArrayTime, rawTime, ipTime);
-		reportTime("Imglib (Cell)", imgLibCellTime, rawTime, ipTime);
-		reportTime("Imglib (Planar)", imgLibPlanarTime, rawTime, ipTime);
-		reportTime("Imglib (ImagePlus)", imgLibImagePlusTime, rawTime, ipTime);
-		reportTime("PixLib (Refl)", pixLibReflTime, rawTime, ipTime);
-		reportTime("PixLib (Array: generic)", pixLibGenArrayTime, rawTime, ipTime);
-		reportTime("PixLib (Array: byte)", pixLibByteArrayTime, rawTime, ipTime);
+
+		final Map<String, Long> entry = results.get(iter);
+		entry.put(METHOD_RAW, rawTime);
+		entry.put(METHOD_IMAGEJ, ipTime);
+		entry.put(METHOD_IMGLIB_ARRAY, imgLibArrayTime);
+		entry.put(METHOD_IMGLIB_CELL, imgLibCellTime);
+		entry.put(METHOD_IMGLIB_PLANAR, imgLibPlanarTime);
+		entry.put(METHOD_IMGLIB_IMAGEPLUS, imgLibImagePlusTime);
+		entry.put(METHOD_PIXLIB_REFLECTION, pixLibReflTime);
+		entry.put(METHOD_PIXLIB_GENERIC, pixLibGenArrayTime);
+		entry.put(METHOD_PIXLIB_BYTE, pixLibByteArrayTime);
+
+		reportTime(METHOD_RAW, rawTime, rawTime, ipTime);
+		reportTime(METHOD_IMAGEJ, ipTime, rawTime, ipTime);
+		reportTime(METHOD_IMGLIB_ARRAY, imgLibArrayTime, rawTime, ipTime);
+		reportTime(METHOD_IMGLIB_CELL, imgLibCellTime, rawTime, ipTime);
+		reportTime(METHOD_IMGLIB_PLANAR, imgLibPlanarTime, rawTime, ipTime);
+		reportTime(METHOD_IMGLIB_IMAGEPLUS, imgLibImagePlusTime, rawTime, ipTime);
+		reportTime(METHOD_PIXLIB_REFLECTION, pixLibReflTime, rawTime, ipTime);
+		reportTime(METHOD_PIXLIB_GENERIC, pixLibGenArrayTime, rawTime, ipTime);
+		reportTime(METHOD_PIXLIB_BYTE, pixLibByteArrayTime, rawTime, ipTime);
 	}
 
 	private long computeDifference(final List<Long> list) {
@@ -219,16 +324,16 @@ public class PerformanceBenchmark {
 		final int max = w + h;
 		byte[] data = new byte[size];
 		int index = 0;
-		for (int y=0; y<h; y++) {
-			for (int x=0; x<w; x++) {
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
 				data[index++] = (byte) (255 * (x + y) / max);
 			}
 		}
 		return data;
 	}
 
-	private ByteProcessor createByteProcessor(final byte[] data, final int width, final int height) {
-		return new ByteProcessor(width, height, data, null);
+	private ByteProcessor createByteProcessor(final byte[] data, final int w, final int h) {
+		return new ByteProcessor(w, h, data, null);
 	}
 
 	private PixelCube<Byte, BaseIndex> createPixelCube(ByteProcessor bp) {
@@ -237,27 +342,27 @@ public class PerformanceBenchmark {
 		return pc;
 	}
 
-	private Image<UnsignedByteType> createArrayImage(final byte[] data, final int width, final int height) {
+	private Image<UnsignedByteType> createArrayImage(final byte[] data, final int w, final int h) {
 		//return createImage(data, width, height, new ArrayContainerFactory());
 		// NB: Avoid copying the data.
 		final ByteAccess byteAccess = new ByteArray(data);
-		final Array<UnsignedByteType, ByteAccess> array = new Array<UnsignedByteType, ByteAccess>(new ArrayContainerFactory(), byteAccess, new int[] {width, height}, 1);
+		final Array<UnsignedByteType, ByteAccess> array = new Array<UnsignedByteType, ByteAccess>(new ArrayContainerFactory(), byteAccess, new int[] {w, h}, 1);
 		array.setLinkedType(new UnsignedByteType(array));
 		return new Image<UnsignedByteType>(array, new UnsignedByteType());
 		//return DevUtil.createImageFromArray(data, new int[] {width, height});
 	}
 
-	private Image<UnsignedByteType> createPlanarImage(final byte[] data, final int width, final int height) {
+	private Image<UnsignedByteType> createPlanarImage(final byte[] data, final int w, final int h) {
 		//return createImage(data, width, height, new PlanarContainerFactory());
 		// NB: Avoid copying the data.
-		PlanarContainer<UnsignedByteType, ByteArray> planarContainer = new PlanarContainer<UnsignedByteType, ByteArray>(new int[] {width, height}, 1);
+		PlanarContainer<UnsignedByteType, ByteArray> planarContainer = new PlanarContainer<UnsignedByteType, ByteArray>(new int[] {w, h}, 1);
 		planarContainer.setPlane(0, new ByteArray(data));
 		planarContainer.setLinkedType(new UnsignedByteType(planarContainer));
 		return new Image<UnsignedByteType>(planarContainer, new UnsignedByteType());
 	}
 
-	private Image<UnsignedByteType> createCellImage(final byte[] data, final int width, final int height) {
-		return createImage(data, width, height, new CellContainerFactory());
+	private Image<UnsignedByteType> createCellImage(final byte[] data, final int w, final int h) {
+		return createImage(data, w, h, new CellContainerFactory());
 	}
 
 	private Image<UnsignedByteType> createImagePlusImage(final ImageProcessor ip) {
@@ -265,10 +370,10 @@ public class PerformanceBenchmark {
 		return ImagePlusAdapter.wrapByte(imp);
 	}
 
-	private Image<UnsignedByteType> createImage(final byte[] data, final int width, final int height, final ContainerFactory cf) {
+	private Image<UnsignedByteType> createImage(final byte[] data, final int w, final int h, final ContainerFactory cf) {
 		final ImageFactory<UnsignedByteType> imageFactory =
 			new ImageFactory<UnsignedByteType>(new UnsignedByteType(), cf);
-		final int[] dim = {width, height};
+		final int[] dim = {w, h};
 		final Image<UnsignedByteType> img = imageFactory.createImage(dim);
 		int index = 0;
 		for (UnsignedByteType t : img) t.set(data[index++]);
@@ -349,7 +454,7 @@ public class PerformanceBenchmark {
 		cursor.close();
 	}
 
-	private void invertPixelCubeRefl(final PixelCube<Byte, BaseIndex> pc) {
+	private void invertPixelCubeReflection(final PixelCube<Byte, BaseIndex> pc) {
 		pc.setIterationPattern(Constants.IP_FWD + Constants.IP_SINGLE);
 		RasterForwardIterator<Byte> iterIn = (RasterForwardIterator<Byte>) pc.iterator();
 		while (iterIn.hasNext()) {
@@ -455,7 +560,7 @@ public class PerformanceBenchmark {
 		cursor.close();
 	}
 
-	private void randomizePixelCubeRefl(final PixelCube<Byte, BaseIndex> pc) {
+	private void randomizePixelCubeReflection(final PixelCube<Byte, BaseIndex> pc) {
 		pc.setIterationPattern(Constants.IP_FWD + Constants.IP_SINGLE);
 		RasterForwardIterator<Byte> iterIn = (RasterForwardIterator<Byte>) pc.iterator();
 		while (iterIn.hasNext()) {
