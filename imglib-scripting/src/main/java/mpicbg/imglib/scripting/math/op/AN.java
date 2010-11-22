8cde1823b84879c679d2046b89702ee4d3bf83d0
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-11-23 00:46:20 +0100

    Operations: cleaned up and fixed AN.asType(Number)

--- imglib-scripting/src/main/java/mpicbg/imglib/scripting/math/op/AN.java
+++ imglib-scripting/src/main/java/mpicbg/imglib/scripting/math/op/AN.java
@@ -1,39 +1,25 @@
 package mpicbg.imglib.scripting.math.op;
 
 import mpicbg.imglib.type.numeric.RealType;
+import mpicbg.imglib.type.numeric.integer.IntType;
 import mpicbg.imglib.type.numeric.integer.LongType;
+import mpicbg.imglib.type.numeric.integer.ShortType;
 import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
 import mpicbg.imglib.type.numeric.real.DoubleType;
 import mpicbg.imglib.type.numeric.real.FloatType;
 
-public abstract class AN< R extends RealType<R> > {
-
-	/*
-	// Can't use it: RealType lacks a generic "set" method.
-	static private final HashMap<Class<? extends Number>, Class<? extends RealType<?>>> m
-		= new HashMap<Class<? extends Number>, Class<? extends RealType<?>>>();
-	static {
-		m.put(Integer.class, LongType.class); // int as long!
-		m.put(Short.class, ShortType.class);
-		m.put(Byte.class, UnsignedByteType.class);
-		m.put(Long.class, LongType.class);
-		m.put(Float.class, FloatType.class);
-		m.put(Double.class, DoubleType.class);
-	}
-	*/
-
-	// TODO: Why does it need the cast?
-	protected final RealType<?> asType(final Number val) {
-		/*// FAILS
-		num = (A) m.get(val.getClass())
-		num.set(val);
-		*/
+public abstract class AN< R extends RealType<R> >
+{
+	/** Defaults to DoubleType, and treats Byte as unsigned. */
+	protected final RealType<?> asType(final Number val)
+	{
 		final Class<? extends Number> c = val.getClass();
-		if (c == Float.class) return new FloatType(val.floatValue());
+		if (c == Double.class) return new DoubleType(val.doubleValue());
 		else if (c == Long.class) return new LongType(val.longValue());
-		else if (c == Double.class) return new DoubleType(val.doubleValue());
+		else if (c == Float.class) return new FloatType(val.floatValue());
 		else if (c == Byte.class) return new UnsignedByteType(val.byteValue());
-		else if (c == Integer.class) return new LongType(val.intValue()); // int as long!
-		return new FloatType(val.floatValue()); // defaults to float
+		else if (c == Integer.class) return new IntType(val.intValue());
+		else if (c == Short.class) return new ShortType(val.shortValue());
+		return new DoubleType(val.doubleValue());
 	}
 }
