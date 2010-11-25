75d2efa1f4af54b6e80eaf3482487ccbe673d162
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-11-25 10:52:17 +0100

    Scripting: removed old math package and associated rgb package.

--- imglib-scripting/src/main/java/mpicbg/imglib/scripting/math/fn/FunctionReal.java
+++
@@ -1,8 +0,0 @@
-package mpicbg.imglib.scripting.math.fn;
-
-import mpicbg.imglib.type.numeric.RealType;
-
-public interface FunctionReal< R extends RealType<R> >
-{
-	public void compute( RealType<?> input1, RealType<?> input2, R output );
-}
--- imglib-scripting/src/main/java/mpicbg/imglib/scripting/math/fn/Operation.java
+++
@@ -1,7 +0,0 @@
-package mpicbg.imglib.scripting.math.fn;
-
-import mpicbg.imglib.scripting.math.op.Op;
-import mpicbg.imglib.type.numeric.RealType;
-
-public interface Operation< R extends RealType<R> >
-	extends Op<R>, FunctionReal< R > {}
