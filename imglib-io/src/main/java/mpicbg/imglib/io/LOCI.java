340b53746741bec3fce9213781adc1b45455dbc7
Curtis Rueden <ctrueden@wisc.edu>
2010-11-30 15:58:54 -0600

    First version of new imglib-io ImageOpener.
    Migrated from bf-imglib project at:
    http://dev.imagejdev.org/svn/imagej/trunk/bf-imglib
    
    Still needs work:
    	* Support additional container types besides planar.
      * ImgLib needs an API for labeling dimensional axes.

--- imglib-io/src/main/java/mpicbg/imglib/io/LOCI.java
+++ imglib-io/src/main/java/mpicbg/imglib/io/LOCI.java
@@ -1,19 +1,7 @@
-/**
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * @author Stephan Preibisch & Stephan Saalfeld
- */
+//
+// LOCI.java
+//
+
 package mpicbg.imglib.io;
 
 import java.io.IOException;
@@ -37,6 +25,7 @@ import mpicbg.imglib.type.numeric.integer.Unsigned12BitType;
 import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
 import mpicbg.imglib.type.numeric.real.FloatType;
 
+/** @deprecated Use {@link ImageOpener} instead. */
 public class LOCI
 {
 	public static <T extends RealType<T>> Image<T> openLOCI( final String fileName, final ContainerFactory containerFactory )
