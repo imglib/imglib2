049eead08156dfc8d5faefe6fd461987f2d1d2c3
Curtis Rueden <ctrueden@wisc.edu>
2010-12-08 14:26:36 -0600

    Convert HistogramTest to JUnit test.

--- imglib-algorithms/src/test/java/HistogramTest.java
+++ imglib-algorithms/src/test/java/HistogramTest.java
@@ -1,4 +1,25 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @author Larry Lindsey
+ *
+ */
+
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import mpicbg.imglib.algorithm.histogram.Histogram;
 import mpicbg.imglib.algorithm.histogram.HistogramBinFactory;
@@ -9,80 +30,60 @@ import mpicbg.imglib.image.Image;
 import mpicbg.imglib.image.ImageFactory;
 import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
 
+import org.junit.Test;
+
 public class HistogramTest {
-	
-	private static ArrayList<int[]> vectorList;
-	private static ArrayList<int[]> expectKeys;
-	private static ArrayList<int[]> expectCnts;
-	
-	private int[] vect;
-	private int[] expectKey;
-	private int[] expectCnt;
-		
-	private HistogramTest(int[] invect, int[] inExpectKey, int[] inExpectCnt)
-	{
-		vect = invect;
-		expectKey = inExpectKey;
-		expectCnt = inExpectCnt;
-		
-	}
-	
-	private boolean runTest()
+
+	private void runTest(int[] vect, int[] expectKey, int[] expectCnt)
 	{
 		ImageFactory<UnsignedByteType> imFactory =
 			new ImageFactory<UnsignedByteType>(new UnsignedByteType(),
-					new ArrayContainerFactory());
+				new ArrayContainerFactory());
 		Image<UnsignedByteType> im = imFactory.createImage(
-				new int[]{vect.length});
+			new int[]{vect.length});
 		Cursor<UnsignedByteType> cursor = im.createCursor();
 		Histogram<UnsignedByteType> histogram;
 		HistogramBinFactory<UnsignedByteType> binFactory = 
 			new DiscreteIntHistogramBinFactory<UnsignedByteType>();
 		UnsignedByteType k = new UnsignedByteType();
-		boolean check = true;
-		
 
 		for (int v: vect)
 		{
 			cursor.fwd();
 			cursor.getType().set(v);
 		}
-		
+
 		histogram = new Histogram<UnsignedByteType>(
-					binFactory,
-					im.createCursor());
-		
+			binFactory,
+			im.createCursor());
+
 		histogram.process();
-		
+
 		for (int i = 0; i < expectKey.length; ++i)
 		{
 			long cntVal;
 			k.set(expectKey[i]);
 			cntVal = histogram.getBin(k).getCount();
-			System.out.println("For bin " + expectKey[i] + " expected " + expectCnt[i] +
-					", got " + cntVal);
-			check &= cntVal == expectCnt[i];
+			assertEquals("Bin " + expectKey[i], expectCnt[i], cntVal);
 		}
-		
-		return check;
 	}
-	
-	public static void makeTests()
-	{
-		int n = 2;
-		vectorList = new ArrayList<int[]>(n);
-		expectKeys = new ArrayList<int[]>(n);
-		expectCnts = new ArrayList<int[]>(n);
-		
+
+	@Test
+	public final void testHistogram() {
+		final int n = 2;
+		final List<int[]> vectorList = new ArrayList<int[]>(n);
+		final List<int[]> expectKeys = new ArrayList<int[]>(n);
+		final List<int[]> expectCnts = new ArrayList<int[]>(n);
+
 		//Test 1
 		vectorList.add(new int[]{1, 2, 3, 4});
 		expectKeys.add(new int[]{1, 2, 3, 4, 5});
 		expectCnts.add(new int[]{1, 1, 1, 1, 0});
-		
+
 		//Test 2
-		int test2vect[] = new int[1024];
-		int test2keys[] = new int[256];
-		int test2cnts[] = new int[256];
+		int[] test2vect = new int[1024];
+		int[] test2keys = new int[256];
+		int[] test2cnts = new int[256];
 		for (int i = 0; i < 1024; ++i)
 		{
 			test2vect[i] = i % 256;
@@ -92,27 +93,15 @@ public class HistogramTest {
 			test2keys[i] = i;
 			test2cnts[i] = 4;
 		}
-		
+
 		vectorList.add(test2vect);
 		expectKeys.add(test2keys);
 		expectCnts.add(test2cnts);
-	}
-	
-	public static void main(String[] args)
-	{
-		makeTests();
-		boolean[] passedTests = new boolean[vectorList.size()];
+
 		for (int i = 0; i < vectorList.size(); ++i)
 		{
-			passedTests[i] = new HistogramTest(vectorList.get(i), expectKeys.get(i), expectCnts.get(i)).runTest();
-		}
-		
-		for (int i = 0; i < passedTests.length; ++i)
-		{
-			String indication = passedTests[i] ? "passed" : "FAILED";
-			
-			System.out.println("Test " + (i + 1) + " " + indication);
+			runTest(vectorList.get(i), expectKeys.get(i), expectCnts.get(i));
 		}
 	}
-	
+
 }
