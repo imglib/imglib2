091e985402107cc9722c4f4345d26285c18ff895
Stephan Preibisch <preibisch@mpi-cbg.de>
2010-12-13 17:08:52 +0100

    Added Radius based neighbor search and added GPL licensing

--- imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/kdtree/NNearestNeighborSearch.java
+++ imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/kdtree/NNearestNeighborSearch.java
@@ -1,7 +1,22 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @author Johannes Schindelin
+ */
 package mpicbg.imglib.algorithm.kdtree;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 import mpicbg.imglib.algorithm.kdtree.node.Leaf;
 import mpicbg.imglib.algorithm.kdtree.node.Node;
@@ -59,17 +74,7 @@ public class NNearestNeighborSearch<T extends Leaf<T>>
 			}
 			else 
 			{
-				index = Arrays.binarySearch( result, leaf,
-					new Comparator<T>() 
-					{
-						@Override
-						public int compare( final T a, final T b ) 
-						{
-							final float distA = point.distanceTo( a );
-							final float distB = point.distanceTo( b );
-							return distA < distB ? -1 : distA > distB ? +1 : 0;
-						}
-					});
+				index = Arrays.binarySearch( result, leaf, new DistanceComparator<T>( point ) );
 				
 				if (index < 0)
 					index = -1 - index;
--- imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/kdtree/TestKDTree.java
+++ imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/kdtree/TestKDTree.java
@@ -1,6 +1,23 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @author Stephan Preibisch
+ */
 package mpicbg.imglib.algorithm.kdtree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import mpicbg.imglib.algorithm.kdtree.node.SimpleNode;
@@ -80,8 +97,116 @@ public class TestKDTree
 
 		return true;
 	}
+	
+	protected static boolean testRadiusSearch( final int numDimensions, final int numPoints, final int numTests, final float min, final float max  )
+	{
+		final ArrayList<SimpleNode> points = new ArrayList<SimpleNode>();
+		final Random rnd = new Random(435435435);
 
-	protected static boolean testNearestNeighbor(final int numDimensions, final int numPoints, final int numTests, final float min, final float max) {
+		final float[] p = new float[numDimensions];
+
+		for (int i = 0; i < numPoints; ++i) {
+			for (int d = 0; d < numDimensions; ++d)
+				p[d] = rnd.nextFloat() * (max - min) + min;
+
+			final SimpleNode t = new SimpleNode(p);
+			points.add(t);
+		}
+
+		long start = System.currentTimeMillis();
+		final KDTree<SimpleNode> kdTree = new KDTree<SimpleNode>( points );
+		final RadiusNeighborSearch<SimpleNode> kd = new RadiusNeighborSearch<SimpleNode>( kdTree );
+		final long kdSetupTime = System.currentTimeMillis() - start;
+		System.out.println("kdtree setup took: " + (kdSetupTime) + " ms.");
+
+		start = System.currentTimeMillis();
+		
+		for (int i = 0; i < numTests; ++i) 
+		{
+			for (int d = 0; d < numDimensions; ++d)
+				p[d] = rnd.nextFloat() * (2*max - 2*min) + 2*min;
+
+			double radius = rnd.nextDouble() * (max-min) / 10;
+			
+			final SimpleNode t = new SimpleNode(p);
+
+			final ArrayList<SimpleNode> radiusKdtree = kd.findNeighborsSorted( t, radius );
+			final ArrayList<SimpleNode> radiusExhaustive = findNeighborsRadiusExhaustive( points, t, radius );
+
+			if ( radiusKdtree.size() != radiusExhaustive.size() )
+			{
+				
+				System.out.println("Not same number of points within radius(" + radius + ") of " + t + " found, kdTree = " + radiusKdtree.size() + ", exhaustive = " + radiusExhaustive.size() );
+				
+				System.out.println("KDTree:");				
+				for ( final SimpleNode n : radiusKdtree  )
+					System.out.println( n + ", distance: " + n.distanceTo( t ) );
+
+				System.out.println("Exhaustive:");				
+				for ( final SimpleNode n : radiusExhaustive )
+					System.out.println( n + ", distance: " + n.distanceTo( t ) );
+				
+				return false;
+			}
+			else
+			{
+				boolean success = true;
+				for ( int j = 0; j < radiusKdtree.size(); ++j )
+				{
+					if ( radiusKdtree.get( j ) != radiusExhaustive.get( j ) && radiusKdtree.get( j ) .distanceTo( t ) != radiusExhaustive.get( j ).distanceTo( t ) )
+					{
+						System.out.println( "Point " + j + " disagrees within radius(" + radius + ") of " + t + " found, \n\tkdTree = " + radiusKdtree.get( j )  + ", distance: " + radiusKdtree.get( j ) .distanceTo( t ) + ", \n\texhaustive = " + radiusExhaustive.get( j ) + ", distance: " + radiusExhaustive.get( j ).distanceTo( t ) );
+						success = false;
+					}					
+				}
+				
+				if ( !success )
+					return false;
+			}			
+		}
+		
+		final long compareTime = System.currentTimeMillis() - start;		
+		System.out.println("comparison (kdtree <-> exhaustive) successfull, took: " + (compareTime) + " ms.");
+
+		start = System.currentTimeMillis();
+		for (int i = 0; i < numTests; ++i) {
+			for (int d = 0; d < numDimensions; ++d)
+				p[d] = rnd.nextFloat() * (2*max - 2*min) + 2*min;
+			final SimpleNode t = new SimpleNode(p);
+			t.getClass();
+		}
+		final long initTime = System.currentTimeMillis() - start;
+
+		double radius = (max-min)/20;
+		
+		start = System.currentTimeMillis();
+		for (int i = 0; i < numTests; ++i) {
+			for (int d = 0; d < numDimensions; ++d)
+				p[d] = rnd.nextFloat() * (max - min) + min;
+			final SimpleNode t = new SimpleNode(p);
+			final ArrayList<SimpleNode> nnKdtree = kd.findNeighborsSorted( t, radius );
+			nnKdtree.getClass();
+		}
+		final long kdTime = System.currentTimeMillis() - start;
+		System.out.println("kdtree search took: " + (kdTime-initTime) + " ms.");
+		System.out.println("kdtree all together took: " + (kdSetupTime+kdTime-initTime) + " ms.");
+
+		start = System.currentTimeMillis();
+		for (int i = 0; i < numTests; ++i) {
+			for (int d = 0; d < numDimensions; ++d)
+				p[d] = rnd.nextFloat() * (max - min) + min;
+			final SimpleNode t = new SimpleNode(p);
+			final ArrayList<SimpleNode> nnExhaustive = findNeighborsRadiusExhaustive( points, t, radius );
+			nnExhaustive.getClass();
+		}
+		final long exhaustiveTime = System.currentTimeMillis() - start;
+		System.out.println("exhaustive search took: " + (exhaustiveTime-initTime) + " ms.");
+		
+		return true;
+	}
+
+	protected static boolean testNearestNeighbor( final int numDimensions, final int numPoints, final int numTests, final float min, final float max ) 
+	{
 		final ArrayList<SimpleNode> points = new ArrayList<SimpleNode>();
 		final Random rnd = new Random(435435435);
 
@@ -119,7 +244,7 @@ public class TestKDTree
 			}
 		}
 		final long compareTime = System.currentTimeMillis() - start;
-		System.out.println("comparison (kd-exhaustive) search took: " + (compareTime) + " ms.");
+		System.out.println("comparison (kdtree <-> exhaustive) search took: " + (compareTime) + " ms.");
 
 		start = System.currentTimeMillis();
 		for (int i = 0; i < numTests; ++i) {
@@ -171,6 +296,23 @@ public class TestKDTree
 		return new SimpleNode(nearest);
 	}
 
+	private static ArrayList<SimpleNode> findNeighborsRadiusExhaustive(final ArrayList<SimpleNode> points, final SimpleNode t, final double radius) 		
+	{
+		final ArrayList<SimpleNode> withinRadius = new ArrayList<SimpleNode>(); 
+		
+		for (final SimpleNode n : points) 
+		{
+			final float dist = n.distanceTo(t);
+			if (dist <= radius) 
+				withinRadius.add( n );
+		}
+		
+		// now sort
+		Collections.sort( withinRadius, new DistanceComparator<SimpleNode>( t ) );
+
+		return withinRadius;
+	}
+	
 	private static SimpleNode[] findNNearestNeighborExhaustive(final ArrayList<SimpleNode> points, final SimpleNode t, final int n) {
 		final SimpleNode[] nearest = new SimpleNode[n];
 		final float[] minDistance = new float[n];
@@ -199,32 +341,16 @@ A:		for (final SimpleNode node : points) {
 		return nearest;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		if ( testRadiusSearch( 3, 100000, 1000, -100, 100 ) )
+			System.out.println("Radius neighbor test (3) successfull\n");
+		
 		if (testNNearestNeighbor(3, 3, 100000, 1000, -5, 5))
 			System.out.println("N-Nearest neighbor test (3) successfull\n");
 
 		if (testNearestNeighbor(3, 100000, 1000, -5, 5))
 			System.out.println("Nearest neighbor test successfull\n");
 
-		final ArrayList<SimpleNode> points = new ArrayList<SimpleNode>();
-
-		points.add(new SimpleNode(new float[]{ 1, 1, 0 }));
-		points.add(new SimpleNode(new float[]{ 0, 1, 1 }));
-		points.add(new SimpleNode(new float[]{ 1, 0, 1 }));
-		//points.add(new SimpleNode(new float[]{ 1.2f,0,0}));
-		//points.add(new SimpleNode(new float[]{ 2f,1f,1f}));
-		//points.add(new SimpleNode(new float[]{ 0,0,10f}));
-
-		KDTree<SimpleNode> kdTree = new KDTree<SimpleNode>(points);
-		NNearestNeighborSearch<SimpleNode> nns = new NNearestNeighborSearch<SimpleNode>(kdTree);
-
-		SimpleNode ref = new SimpleNode(new float[]{ 2, 0, 0 });
-
-		SimpleNode[] nn = nns.findNNearestNeighbors(ref, 2);
-
-		System.out.println(nn[0]);
-		System.out.println(nn[1]);
-
-		System.exit(0);
 	}
 }
