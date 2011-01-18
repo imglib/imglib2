752db6c7af56845aa88d5151a05a13a1fdd7ba8a
Larry Lindsey <llindsey@clm.utexas.edu>
2011-01-18 15:41:54 -0600

    Speed improvements to HoughLineTransform

--- imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/transformation/HoughLineTransform.java
+++ imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/transformation/HoughLineTransform.java
@@ -1,10 +1,14 @@
 package mpicbg.imglib.algorithm.transformation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import mpicbg.imglib.container.array.ArrayContainerFactory;
 import mpicbg.imglib.cursor.LocalizableCursor;
 import mpicbg.imglib.image.Image;
+import mpicbg.imglib.image.ImageFactory;
 import mpicbg.imglib.type.Type;
+import mpicbg.imglib.type.numeric.integer.IntType;
 import mpicbg.imglib.util.Util;
 
 
@@ -31,165 +35,162 @@ import mpicbg.imglib.util.Util;
  * rho = y * sin(theta) - x * cos(theta)
  * @Override 
  */
-public class HoughLineTransform <T extends Type<T> & Comparable<T>> extends HoughTransform<T>
+public class HoughLineTransform <T extends Type<T> & Comparable<T>>
+    extends HoughTransform<T>
 {
-	public static final int DEFAULT_THETA = 180;
-	public final double dTheta;
-	public final double dRho;
-	private final T threshold;
-	private final int nRho;
-	private final int nTheta;
-	private final double[] rho;
-	private final double[] theta;
-	private ArrayList<double[]> rtPeaks;
-	public long voteTime;
-	
-	/**
-	 * Calculates a default number of rho bins, which corresponds to a resolution of one pixel.
-	 * @param inputImage the {@link Image} in question.
-	 * @return default number of rho bins.
-	 */
-	public static int defaultRho(final Image<?> inputImage)
-	{
-		return (int)(2 * Util.computeLength(inputImage.getDimensions()));
-	}
+    public static final int DEFAULT_THETA = 180;
+    private final double dTheta;
+    private final double dRho;
+    private final T threshold;
+    private final int nRho;
+    private final int nTheta;
+    private final double[] rho;
+    private final double[] theta;
+    private final int[][] voteMap;
+    private ArrayList<double[]> rtPeaks;
 
-	/**
-	 * Create a {@link HoughLineTransform} to operate against a given {@link Image}, with
-	 * a specific {@link Type} of vote space.
-	 * Defaults are used for rho- and theta-resolution.
-	 * @param inputImage the {@link Image} to operate against.
-	 * @param type the {@link Type} for the vote space.
-	 */
-	public HoughLineTransform(final Image<T> inputImage)
-	{
-		this(inputImage, DEFAULT_THETA);
-	}
-	
-	/**
-	 * Create a {@link HoughLineTransform} to operate against a given {@link Image}, with
-	 * a specific {@link Type} of vote space and theta-resolution.
-	 * Rho-resolution is set to the default.
-	 * @param inputImage the {@link Image} to operate against.
-	 * @param theta the number of bins for theta-resolution.
-	 * @param type the {@link Type} for the vote space.
-	 */
-	public HoughLineTransform(final Image<T> inputImage, final int theta)
-	{
-		this(inputImage, defaultRho(inputImage), theta);
-	}
-	
-	/**
-	 * Create a {@link HoughLineTransform} to operate against a given {@link Image}, with
-	 * a specific {@link Type} of vote space and rho- and theta-resolution.
-	 * @param inputImage the {@link Image} to operate against.
-	 * @param theta the number of bins for theta resolution.
-	 * @param type the {@link Type} for the vote space.
-	 */
-	public HoughLineTransform(final Image<T> inputImage, final int inNRho, final int inNTheta)
-	{
-		super(inputImage, new int[]{inNRho, inNTheta});
-		//Theta by definition is in [0..pi].
-		dTheta = Math.PI / (double)inNTheta;
-		/*The furthest a point can be from the origin is the length calculated
-		 * from the dimensions of the Image.
-		 */
-		dRho = 2 * Util.computeLength(inputImage.getDimensions()) / (double)inNRho;
-		threshold = inputImage.createType();
-		nRho = inNRho;
-		nTheta = inNTheta;
-		theta = new double[inNTheta];
-		rho = new double[inNRho];
-		rtPeaks = null;
-	}
-	
-	public void setThreshold(final T inThreshold)
-	{
-		threshold.set(inThreshold);
-	}
+    /**
+     * Calculates a default number of rho bins, which corresponds to a resolution of one pixel.
+     * @param inputImage the {@link Image} in question.
+     * @return default number of rho bins.
+     */
+    public static int defaultRho(final Image<?> inputImage)
+    {
+        return (int)(2 * Util.computeLength(inputImage.getDimensions()));
+    }
 
-	@Override
-	public boolean process()
-	{
-		final LocalizableCursor<T> imageCursor = getImage().createLocalizableCursor();
-		final int[] position = new int[getImage().getDimensions().length];
-		final double minTheta = -Math.PI/2;
-		final double minRho = -Util.computeLength(super.getImage().getDimensions());
-		final long sTime = System.currentTimeMillis();
-		boolean success;
-		
-		voteTime = 0;
-				
-		for (int t = 0; t < nTheta; ++t)
-		{
-			theta[t] = dTheta * (double)t + minTheta;
-		}		
-		
-		for (int r = 0; r < nRho; ++r)
-		{
-			rho[r] = dRho * (double)r + minRho;
-		}
+    /**
+     * Create a {@link HoughLineTransform} to operate against a given {@link Image}, with
+     * a specific {@link Type} of vote space.
+     * Defaults are used for rho- and theta-resolution.
+     * @param inputImage the {@link Image} to operate against.
+     * @param type the {@link Type} for the vote space.
+     */
+    public HoughLineTransform(final Image<T> inputImage)
+    {
+        this(inputImage, DEFAULT_THETA);
+    }
 
-		System.out.println("" + (System.currentTimeMillis() - sTime) + " ms to init");
-		
-		
-		
-		while (imageCursor.hasNext())
-		{
-			double fRho;
-			int r;
-			int[] voteLoc = new int[2];
-			
-			imageCursor.fwd();
-			imageCursor.getPosition(position);
+    /**
+     * Create a {@link HoughLineTransform} to operate against a given {@link Image}, with
+     * a specific {@link Type} of vote space and theta-resolution.
+     * Rho-resolution is set to the default.
+     * @param inputImage the {@link Image} to operate against.
+     * @param theta the number of bins for theta-resolution.
+     * @param type the {@link Type} for the vote space.
+     */
+    public HoughLineTransform(final Image<T> inputImage, final int theta)
+    {
+        this(inputImage, defaultRho(inputImage), theta);
+    }
+
+    /**
+     * Create a {@link HoughLineTransform} to operate against a given {@link Image}, with
+     * a specific {@link Type} of vote space and rho- and theta-resolution.
+     * @param inputImage the {@link Image} to operate against.
+     * @param theta the number of bins for theta resolution.
+     * @param type the {@link Type} for the vote space.
+     */
+    public HoughLineTransform(final Image<T> inputImage, final int inNRho, final int inNTheta)
+    {
+        super(inputImage, new int[]{inNRho, inNTheta});
+        //Theta by definition is in [0..pi].
+        dTheta = (float)Math.PI / (float)inNTheta;
+        /*The furthest a point can be from the origin is the length calculated
+         * from the dimensions of the Image.
+         */
+        dRho = 2 * Util.computeLength(inputImage.getDimensions()) / (float)inNRho;
+        threshold = inputImage.createType();
+        nRho = inNRho;
+        nTheta = inNTheta;
+        theta = new double[inNTheta];
+        rho = new double[inNRho];
+        rtPeaks = null;
+
+        voteMap = new int[getVoteSize()[0]][getVoteSize()[1]];
+
+        for (int[] array : voteMap)
+        {
+            Arrays.fill(array, 0);
+        }
+    }
+
+    public void setThreshold(final T inThreshold)
+    {
+        threshold.set(inThreshold);
+    }
+
+    @Override
+    public boolean process()
+    {
+        final LocalizableCursor<T> imageCursor = getImage().createLocalizableCursor();
+        final int[] position = new int[getImage().getDimensions().length];
+        final double[] cosines = new double[theta.length];
+        final double[] sines = new double[theta.length];
+        final double minTheta = -Math.PI/2;
+        final double minRho = -Util.computeLength(getImage().getDimensions());
+        final long sTime = System.currentTimeMillis();
+        boolean success;
+
+        for (int t = 0; t < nTheta; ++t)
+        {
+            theta[t] = dTheta * (double)t + minTheta;
+            sines[t] = Math.sin(theta[t]);
+            cosines[t] = Math.cos(theta[t]);
+        }
+
+        for (int r = 0; r < nRho; ++r)
+        {
+            rho[r] = dRho * (double)r + minRho;
+        }
+
+        while (imageCursor.hasNext())
+        {
+            double fRho;
+            int r;
+            int[] voteLoc = new int[2];
+
+            imageCursor.fwd();
+            imageCursor.getPosition(position);
 
             if (imageCursor.getType().compareTo(threshold) > 0)
             {
                 for (int t = 0; t < nTheta; ++t)
                 {
-					fRho = Math.cos(theta[t]) * (double)position[0] + Math.sin(theta[t]) * (double)position[1];
-					r = Math.round((float)((fRho - minRho)/ dRho));
-					voteLoc[0] = r;
-					voteLoc[1] = t;
-					
-					long cTime = System.currentTimeMillis();
-					
-					try
-					{
-						super.placeVote(voteLoc);
-					}
-					catch(Exception e)
-					{
-						System.err.println("Tried to place vote at " + r + " " + t + " for theta " + theta[t] + ", and rho " + fRho);
-						return false;
-					}
-					
-					voteTime += cTime - System.currentTimeMillis();
-				}
-			}			
-		}		
-		
-		success = super.pickPeaks();
-		super.pTime = System.currentTimeMillis() - sTime;
-		return success;
-	}
-	
-	public ArrayList<double[]> getTranslatedPeakList()
-	{
-		if (rtPeaks == null)
-		{
-			ArrayList<int[]> peaks = getPeakList();
-			rtPeaks = new ArrayList<double[]>(peaks.size());
-			for (int[] irt : peaks)
-			{
-				double[] rt = new double[2];
-				rt[0] = rho[irt[0]];
-				rt[1] = theta[irt[1]];
-				rtPeaks.add(rt);
-			}
-		}
-		
-		return rtPeaks;
-	}
+                    long mTime = System.currentTimeMillis();
+                    fRho = cosines[t] * position[0] + sines[t] * position[1];
+                    r = (int)( ((fRho - minRho)/ dRho) + 0.5);
+                    voteLoc[0] = r;
+                    voteLoc[1] = t;
+
+                    super.placeVote(voteLoc);
+                }
+            }
+        }
+
+        success = super.pickPeaks();
+
+        super.pTime = System.currentTimeMillis() - sTime;
+        return success;
+    }
+
+    public ArrayList<double[]> getTranslatedPeakList()
+    {
+        if (rtPeaks == null)
+        {
+            ArrayList<int[]> peaks = getPeakList();
+            rtPeaks = new ArrayList<double[]>(peaks.size());
+
+            for (int[] irt : peaks)
+            {
+                double[] rt = new double[2];
+                rt[0] = rho[irt[0]];
+                rt[1] = theta[irt[1]];
+                rtPeaks.add(rt);
+            }
+        }
+
+        return rtPeaks;
+    }
 
 }
--- imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/transformation/HoughTransform.java
+++ imglib-algorithms/src/main/java/mpicbg/imglib/algorithm/transformation/HoughTransform.java
@@ -24,179 +24,184 @@ import mpicbg.imglib.type.numeric.integer.IntType;
  * @param <T> the data type of the input image.
  */
 public abstract class HoughTransform<T extends Type<T> & Comparable<T>>
-implements Algorithm, Benchmark
+    implements Algorithm, Benchmark
 {
-	protected long pTime;
-	private String errorMsg;
-	private final Image<T> image;
-	private final  int[]  voteSpace;
-	private ArrayList<int[]> peaks;
-	private final double[] peakExclusion;
-	private final int[] voteSize;
-	private final int[] cumProd;
-	private Image<IntType> voteSpaceImage;
-	
-	/**
-	 * 
-	 * @param inputImage the image for the HoughTransform to operate over
-	 * @param voteSize and integer array indicating the size of the voteSpace.  This is passed
-	 * directly into ImageFactory to create a voteSpace image.
-	 * @param voteFactory the ImageFactory used to generate the voteSpace image.
-	 */
-	protected HoughTransform(final Image<T> inputImage, final int[] voteSize)
-	{
-	    int numel;
-	    
-	    pTime = 0;
-	    
-		image = inputImage;		
-		peaks = null;
-		peakExclusion = new double[voteSize.length];
-		cumProd = new int[voteSize.length];
-		Arrays.fill(peakExclusion, 0);
-		
-		numel = voteSize[0];
-		cumProd[0] = 1;
-		for (int d = 1; d < voteSize.length; ++d)
-		{
-		    cumProd[d] = cumProd[d - 1] * voteSize[d - 1];
-		    numel *= voteSize[d];		   
-		}
+    protected long pTime;
+    private String errorMsg;
+    private final Image<T> image;
+    private final  int[]  voteSpace;
+    private ArrayList<int[]> peaks;
+    private final double[] peakExclusion;
+    private final int[] voteSize;
+    private final int[] cumProd;
+    protected Image<IntType> voteSpaceImage;
 
-		voteSpace = new int[numel];
-		voteSpaceImage = null;
-		this.voteSize = voteSize;
-	}
-	
-	protected int locationToIndex(final int[] loc)
-	{
-	    int index = 0;
-	    
-	    for (int i = 0; i < loc.length; ++i)
-	    {
-	        index += cumProd[i] * loc[i];
-	    }
-	    
-	    return index;
-	}
-	
-	/**
-	 * Place a vote with a specific value.
-	 * @param loc the integer array indicating the location where the vote is to be placed in 
-	 * voteSpace.
-	 * @param vote the value of the vote
-	 */
-	protected void  placeVote(final int[] loc, int vote)
-	{
-	    voteSpace[locationToIndex(loc)] += vote;
-	}
-	
-	/**
-	 * Place a vote of value 1.
-	 * @param loc the integer array indicating the location where the vote is to be placed in 
-	 * voteSpace.
-	 */
-	protected void placeVote(final int[] loc)
-	{
-	    voteSpace[locationToIndex(loc)]++;
-	}
-	
-	/**
-	 * Returns an ArrayList of int arrays, representing the positions in the vote space
-	 * that correspond to peaks.
-	 * @return an ArrayList of vote space peak locations.
-	 */
-	public ArrayList<int[]> getPeakList()
-	{
-		return peaks;
-	}
-		
-	public boolean setExclusion(double[] newExclusion)
-	{
-		if (newExclusion.length >= peakExclusion.length)
-		{
-			System.arraycopy(newExclusion, 0, peakExclusion, 0, peakExclusion.length);
-			return true;
-		}
-		return false;
-	}
-	
-	protected void setErrorMsg(final String msg)
-	{
-		errorMsg = msg;
-	}
-	
-	/**
-	 * Pick vote space peaks with a {@link PickImagePeaks}.
-	 * @return whether peak picking was successful
-	 */
-	protected boolean pickPeaks()
-	{
-		final PickImagePeaks<IntType> peakPicker =
-		    new PickImagePeaks<IntType>(getVoteSpaceImage());
-		boolean ok;
-		
-		peakPicker.setSuppression(peakExclusion);
-		ok = peakPicker.process();
-		if (ok)
-		{
-			peaks = peakPicker.getPeakList();
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	@Override
-	public boolean checkInput() {
-		if (voteSpace == null)
-		{
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	}
+    /**
+     * 
+     * @param inputImage the image for the HoughTransform to operate over
+     * @param voteSize and integer array indicating the size of the voteSpace.  This is passed
+     * directly into ImageFactory to create a voteSpace image.
+     * @param voteFactory the ImageFactory used to generate the voteSpace image.
+     */
+    protected HoughTransform(final Image<T> inputImage, final int[] voteSize)
+    {
+        int numel;
 
-	@Override
-	public String getErrorMessage() {
-		return errorMsg;
-	}
-	
-	@Override
-	public long getProcessingTime() {		
-		return pTime;
-	}
-	
-	public Image<T> getImage()
-	{
-		return image;
-	}
-		
-	public Image<IntType> getVoteSpaceImage()
-	{
-	    if (voteSpaceImage == null)
-	    {
-	        ImageFactory<IntType> factory = new ImageFactory<IntType>(
-	                new IntType(), new ArrayContainerFactory());
-	        LocalizableCursor<IntType> cursor;
-	        final int[] loc = new int[voteSize.length];
-	        
-	        voteSpaceImage = factory.createImage(voteSize);
-	        cursor = voteSpaceImage.createLocalizableCursor();
-	        
-	        while(cursor.hasNext())
-	        {
-	            cursor.fwd();
-	            cursor.getPosition(loc);
-	            cursor.getType().set(voteSpace[locationToIndex(loc)]);
-	        }
-	    }
-	    
-		return voteSpaceImage;
-	}
+        pTime = 0;
+
+        image = inputImage;		
+        peaks = null;
+        peakExclusion = new double[voteSize.length];
+        cumProd = new int[voteSize.length];
+        Arrays.fill(peakExclusion, 0);
+
+        numel = voteSize[0];
+        cumProd[0] = 1;
+        for (int d = 1; d < voteSize.length; ++d)
+        {
+            cumProd[d] = cumProd[d - 1] * voteSize[d - 1];
+            numel *= voteSize[d];		   
+        }
+
+        voteSpace = new int[numel];
+        voteSpaceImage = null;
+        this.voteSize = voteSize;
+    }
+
+    protected int locationToIndex(final int[] loc)
+    {
+        int index = 0;
+
+        for (int i = 0; i < loc.length; ++i)
+        {
+            index += cumProd[i] * loc[i];
+        }
+
+        return index;
+    }
+
+    /**
+     * Place a vote with a specific value.
+     * @param loc the integer array indicating the location where the vote is to be placed in 
+     * voteSpace.
+     * @param vote the value of the vote
+     */
+    protected void  placeVote(final int[] loc, int vote)
+    {
+        voteSpace[locationToIndex(loc)] += vote;
+    }
+
+    /**
+     * Place a vote of value 1.
+     * @param loc the integer array indicating the location where the vote is to be placed in 
+     * voteSpace.
+     */
+    protected void placeVote(final int[] loc)
+    {
+        voteSpace[locationToIndex(loc)]++;
+    }
+
+    /**
+     * Returns an ArrayList of int arrays, representing the positions in the vote space
+     * that correspond to peaks.
+     * @return an ArrayList of vote space peak locations.
+     */
+    public ArrayList<int[]> getPeakList()
+    {
+        return peaks;
+    }
+
+    public boolean setExclusion(double[] newExclusion)
+    {
+        if (newExclusion.length >= peakExclusion.length)
+        {
+            System.arraycopy(newExclusion, 0, peakExclusion, 0, peakExclusion.length);
+            return true;
+        }
+        return false;
+    }
+
+    protected void setErrorMsg(final String msg)
+    {
+        errorMsg = msg;
+    }
+
+    /**
+     * Pick vote space peaks with a {@link PickImagePeaks}.
+     * @return whether peak picking was successful
+     */
+    protected boolean pickPeaks()
+    {
+        final PickImagePeaks<IntType> peakPicker =
+            new PickImagePeaks<IntType>(getVoteSpaceImage());
+        boolean ok;
+
+        peakPicker.setSuppression(peakExclusion);
+        ok = peakPicker.process();
+        if (ok)
+        {
+            peaks = peakPicker.getPeakList();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean checkInput() {
+        if (voteSpace == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return errorMsg;
+    }
+
+    @Override
+    public long getProcessingTime() {		
+        return pTime;
+    }
+
+    public Image<T> getImage()
+    {
+        return image;
+    }
+
+    public Image<IntType> getVoteSpaceImage()
+    {
+        if (voteSpaceImage == null)
+        {
+            ImageFactory<IntType> factory = new ImageFactory<IntType>(
+                    new IntType(), new ArrayContainerFactory());
+            LocalizableCursor<IntType> cursor;
+            final int[] loc = new int[voteSize.length];
+
+            voteSpaceImage = factory.createImage(voteSize);
+            cursor = voteSpaceImage.createLocalizableCursor();
+
+            while(cursor.hasNext())
+            {
+                cursor.fwd();
+                cursor.getPosition(loc);
+                cursor.getType().set(voteSpace[locationToIndex(loc)]);
+            }
+        }
+
+        return voteSpaceImage;
+    }
+
+    protected int[] getVoteSize()
+    {
+        return voteSize;
+    }
 
 }
