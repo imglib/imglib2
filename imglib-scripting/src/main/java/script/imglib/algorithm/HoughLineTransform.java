fffefbb92e6ecec0064d5ef1e408ce791ac04129
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-02-25 19:40:23 -0500

    Fixed scripting of HoughLineTransform (cherry picked from commit c419385ce896ae6192de823a3ba65adc584e4ff5)

--- imglib-scripting/src/main/java/script/imglib/algorithm/HoughLineTransform.java
+++ imglib-scripting/src/main/java/script/imglib/algorithm/HoughLineTransform.java
@@ -1,24 +1,25 @@
 package script.imglib.algorithm;
 
+import script.imglib.algorithm.fn.ImgProxy;
 import script.imglib.math.Compute;
 import script.imglib.math.fn.IFunction;
-import mpicbg.imglib.image.Image;
+import mpicbg.imglib.img.Img;
 import mpicbg.imglib.type.numeric.RealType;
 import mpicbg.imglib.type.numeric.integer.LongType;
 
-public class HoughLineTransform<T extends RealType<T>> extends Image<LongType>
+public class HoughLineTransform<T extends RealType<T>> extends ImgProxy<LongType>
 {
 	/** A {@link mpicbg.imglib.algorithm.transformation.HoughLineTransform} with a LongType vote space.*/
-	public HoughLineTransform(final Image<T> img) throws Exception {
-		super(process(img).getContainer(), new LongType());
+	public HoughLineTransform(final Img<T> img) throws Exception {
+		super(process(img));
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public HoughLineTransform(final IFunction fn) throws Exception {
-		this((Image)Compute.inDoubles(fn));
+		this((Img)Compute.inDoubles(fn));
 	}
 
-	static private final <S extends RealType<S>> Image<LongType> process(final Image<S> img) throws Exception {
+	static private final <S extends RealType<S>> Img<LongType> process(final Img<S> img) throws Exception {
 		mpicbg.imglib.algorithm.transformation.HoughLineTransform<LongType, S> h = 
 			new mpicbg.imglib.algorithm.transformation.HoughLineTransform<LongType, S>(img, new LongType());
 		if (!h.checkInput() || !h.process()) {
