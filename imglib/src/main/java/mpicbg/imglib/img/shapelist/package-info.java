fdc0091b9bd933f88ca6bb17c56c220b01b60a3a
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2011-02-26 14:59:44 +0100

    deleted remaining non-working packages from imglib core

--- imglib/src/main/java/mpicbg/imglib/img/shapelist/package-info.java
+++
@@ -1,5 +0,0 @@
-package mpicbg.imglib.img.shapelist;
-/**
- * TODO Unstable, interface names and function will change!
- * TODO Fix and test!
- */
--- imglib/src/main/java/mpicbg/imglib/interpolation/package-info.java
+++
@@ -1,6 +0,0 @@
-package mpicbg.imglib.interpolation;
-/**
- * TODO implement interpolators without OutOfBounds, instead use RandomAccess
- * TODO check for the need of OutOfBounds
- * TODO implement InterpolatedRandomAccessible
- */
