95e830aaf6e5038ce8b79685de3fb57a7f710a42
Barry DeZonia <bdezonia@wisc.edu>
2011-02-28 17:21:45 -0600

    implementation of AddNoise unary function. used in IJ2.

---
+++ imglib-ops/src/main/java/imglib/ops/operator/unary/AddNoise.java
@@ -0,0 +1,38 @@
+package imglib.ops.operator.unary;
+
+import imglib.ops.operator.UnaryOperator;
+
+import java.util.Random;
+
+public final class AddNoise implements UnaryOperator
+{
+	private final double min, max, stdDev;
+	private final Random rng;
+	
+	public AddNoise(final double min, final double max, final double stdDev)
+	{
+		this.min = min;
+		this.max = max;
+		this.stdDev = stdDev;
+		this.rng = new Random();
+		this.rng.setSeed(System.currentTimeMillis());
+	}
+	
+	@Override
+	public double computeValue(final double input)
+	{
+		int i = 0;
+		do
+		{
+			double newVal = input + (rng.nextGaussian() * stdDev);
+			
+			if ((min <= newVal) && (newVal <= max))
+				return newVal;
+			
+			if (i++ > 100)
+				throw new IllegalArgumentException("noise function failing to terminate. probably misconfigured.");
+		}
+		while(true);
+	}
+	
+}
