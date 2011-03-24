a5d327f66c7498e50411d13d3c54903bfc786365
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2011-03-24 12:43:56 +0100

    fixed view examples

--- imglib-io/src/test/java/tests/OpenAndDisplayRotated.java
+++ imglib-io/src/test/java/tests/OpenAndDisplayRotated.java
@@ -3,6 +3,7 @@ package tests;
 import ij.ImageJ;
 import ij.ImagePlus;
 import ij.process.ColorProcessor;
+import mpicbg.imglib.RandomAccessibleInterval;
 import mpicbg.imglib.display.ARGBScreenImage;
 import mpicbg.imglib.display.RealARGBConverter;
 import mpicbg.imglib.display.XYProjector;
@@ -11,7 +12,6 @@ import mpicbg.imglib.img.array.ArrayImgFactory;
 import mpicbg.imglib.io.LOCI;
 import mpicbg.imglib.type.numeric.ARGBType;
 import mpicbg.imglib.type.numeric.real.FloatType;
-import mpicbg.imglib.view.RandomAccessibleIntervalView;
 import mpicbg.imglib.view.Views;
 
 public class OpenAndDisplayRotated
@@ -22,7 +22,7 @@ public class OpenAndDisplayRotated
 		
 		Img< FloatType > img = LOCI.openLOCIFloatType( "/home/tobias/Desktop/73.tif",  new ArrayImgFactory<FloatType>() );
 		
-		RandomAccessibleIntervalView< FloatType > view = Views.rotatedView( img, 0, 2 );
+		RandomAccessibleInterval< FloatType > view = Views.rotatedView( img, 0, 1 );
 
 		final ARGBScreenImage screenImage = new ARGBScreenImage( ( int )view.dimension( 0 ), ( int )view.dimension( 1 ) );
 		final XYProjector< FloatType, ARGBType > projector = new XYProjector< FloatType, ARGBType >( view, screenImage, new RealARGBConverter< FloatType >( 0, 127 ) );
