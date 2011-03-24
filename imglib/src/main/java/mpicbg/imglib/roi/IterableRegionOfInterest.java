cc11e8987cfff7adc53345f96f7104a0e59867d4
Lee Kamentsky <leek@broadinstitute.org>
2011-03-24 12:56:14 -0400

    Greatly modified ROI defs. Added an AbstractROI class to act as a base and wrote tests for it.

--- imglib/src/main/java/mpicbg/imglib/roi/IterableRegionOfInterest.java
+++ imglib/src/main/java/mpicbg/imglib/roi/IterableRegionOfInterest.java
@@ -3,7 +3,9 @@
  */
 package mpicbg.imglib.roi;
 
-import mpicbg.imglib.Iterator;
+import mpicbg.imglib.IterableInterval;
+import mpicbg.imglib.RandomAccessible;
+import mpicbg.imglib.type.Type;
 
 /**
  * @author leek
@@ -12,6 +14,19 @@ import mpicbg.imglib.Iterator;
  * a user to iterate over the pixels in the region of interest.
  * 
  */
-public interface IterableRegionOfInterest extends RegionOfInterest, Iterator {
+public interface IterableRegionOfInterest extends RegionOfInterest {
+	/**
+	 * Given a sampler in the ROI's space, provide an iterable that can
+	 * make iterators over the space, sampling pixels from the sampler.
+	 * 
+	 * You can then use these cursors to get at all of the pixels in the ROI.
+	 * 
+	 * @param <T>
+	 * @param src - a RandomAccessible that can generate RandomAccess objects.
+	 *              The cursors that IterableInterval generates will use "src"
+	 *              to generate a RandomAccess to sample the space.
+	 * @return
+	 */
+	public <T extends Type<T>> IterableInterval<T> getIterableIntervalOverROI(RandomAccessible<T> src);
 
 }
--- imglib/src/main/java/mpicbg/imglib/roi/RegionOfInterest.java
+++ imglib/src/main/java/mpicbg/imglib/roi/RegionOfInterest.java
@@ -3,15 +3,14 @@
  */
 package mpicbg.imglib.roi;
 
-import mpicbg.imglib.RandomAccess;
+import mpicbg.imglib.RealRandomAccessible;
 import mpicbg.imglib.type.logic.BitType;
 
 /**
  * @author leek
  *
  * A RegionOfInterest defines a set of points in a space.
- * The "get" value will tell you whether a point is in the ROI.
+ * The "get" value from BitType will tell you whether a point is in or out.
  */
-public interface RegionOfInterest extends RandomAccess<BitType> {
-
+public interface RegionOfInterest extends RealRandomAccessible<BitType> {
 }
