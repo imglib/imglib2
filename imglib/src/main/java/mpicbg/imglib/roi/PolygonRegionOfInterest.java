8c0d8c9669054433d97d1e32a5076549b99a12fb
Lee Kamentsky <leek@broadinstitute.org>
2011-04-13 13:13:25 -0400

    Stub of PolygonRegionOfInterest

---
+++ imglib/src/main/java/mpicbg/imglib/roi/PolygonRegionOfInterest.java
@@ -0,0 +1,21 @@
+package mpicbg.imglib.roi;
+
+public class PolygonRegionOfInterest extends AbstractIterableRegionOfInterest {
+
+	protected PolygonRegionOfInterest() {
+		super(2);
+	}
+
+	@Override
+	protected boolean nextRaster(long[] position, long[] end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean isMember(double[] position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
