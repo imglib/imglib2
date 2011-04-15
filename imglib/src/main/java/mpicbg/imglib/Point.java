8e4075455d0fb699a6e07fcdd26cfeebf1f252f9
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2011-04-15 19:37:52 +0200

    Point and RealPoint: * position field final protected (allow extension but not change) * method parameters final (less room for trouble) * renamed method parameters shading fields (less room for trouble) * removed test for d < numDimensions * removed double test in loops, the callee dictates numDimensions
    TODO add assertions for the removed tests
    (cherry picked from commit 1f1f0dfbf5becd3740f3a3d9ac36ee5c75ce0516)

--- imglib/src/main/java/mpicbg/imglib/Point.java
+++ imglib/src/main/java/mpicbg/imglib/Point.java
@@ -3,16 +3,17 @@ package mpicbg.imglib;
 /**
  * @author leek
  *
- *A Point is a position in Euclidean space specified in integer coordinates.
+ * A Point is a position in Euclidean space specified in integer coordinates.
  */
 public class Point implements Localizable, Positionable, EuclideanSpace {
 
-	private long [] position;
+	final protected long[] position;
+	
 	/**
 	 * Create a point at 0 in <i>nDimensional</i> space.
 	 * @param nDimensions # of dimensions in the space
 	 */
-	public Point(int nDimensions) {
+	public Point(final int nDimensions) {
 		position = new long [nDimensions];
 	}
 	
@@ -20,7 +21,7 @@ public class Point implements Localizable, Positionable, EuclideanSpace {
 	 * Create a point at a definite position
 	 * @param position the initial position. The length of the array determines the dimensionality of the space.
 	 */
-	public Point(long [] position) {
+	public Point(final long [] position) {
 		this.position = position.clone();
 	}
 
@@ -28,42 +29,41 @@ public class Point implements Localizable, Positionable, EuclideanSpace {
 	 * Create a point at a definite position
 	 * @param position the initial position. The length of the array determines the dimensionality of the space.
 	 */
-	public Point(int [] position) {
+	public Point(final int [] position) {
 		this.position = new long[position.length];
-		for (int i=0; i<position.length; i++) {
+		for (int i=0; i < position.length; ++i) {
 			this.position[i] = position[i];
 		}
 	}
 	
 	/**
-	 * Create a point using the position of a localizable
+	 * Create a point using the position of a {@link Localizable}
 	 * @param localizable get position from here
 	 */
-	public Point(Localizable localizable) {
+	public Point(final Localizable localizable) {
 		position = new long [localizable.numDimensions()];
 		localizable.localize(position);
 	}
+	
 	@Override
-	public void localize(float[] position) {
-		for (int i=0; (i<numDimensions()) && (i<position.length); i++)
-			position[i] = this.position[i]; 
+	public void localize(final float[] pos) {
+		for (int i=0; i < position.length; ++i)
+			pos[i] = position[i]; 
 	}
 
 	@Override
-	public void localize(double[] position) {
-		for (int i=0; (i<numDimensions()) && (i<position.length); i++)
-			position[i] = this.position[i]; 
+	public void localize(final double[] pos) {
+		for (int i=0; i < position.length; ++i)
+			pos[i] = position[i];
 	}
 
 	@Override
-	public float getFloatPosition(int d) {
-		if (d >= numDimensions()) return 0; 
+	public float getFloatPosition(final int d) {
 		return position[d];
 	}
 
 	@Override
-	public double getDoublePosition(int d) {
-		if (d >= numDimensions()) return 0; 
+	public double getDoublePosition(final int d) {
 		return position[d];
 	}
 
@@ -73,111 +73,97 @@ public class Point implements Localizable, Positionable, EuclideanSpace {
 	}
 
 	@Override
-	public void fwd(int d) {
-		if (d < position.length)
-			position[d] += 1;
+	public void fwd(final int d) {
+		position[d] += 1;
 	}
 
 	@Override
-	public void bck(int d) {
-		if (d < position.length)
-			position[d] -= 1;
+	public void bck(final int d) {
+		position[d] -= 1;
 
 	}
 
 	@Override
-	public void move(int distance, int d) {
-		if (d < position.length)
-			position[d] += distance;
+	public void move(final int distance, final int d) {
+		position[d] += distance;
 	}
 
 	@Override
-	public void move(long distance, int d) {
-		if (d < position.length)
-			position[d] += distance;
+	public void move(final long distance, final int d) {
+		position[d] += distance;
 	}
 
 	@Override
-	public void move(Localizable localizable) {
-		for (int i=0; (i < localizable.numDimensions()) && (i < numDimensions()); i++) {
+	public void move(final Localizable localizable) {
+		for (int i=0; i < position.length; ++i)
 			position[i] += localizable.getLongPosition(i);
-		}
 	}
 
 	@Override
-	public void move(int[] position) {
-		for (int i=0; (i < position.length) && (i < numDimensions()); i++) {
-			this.position[i] += position[i];
-		}
+	public void move(final int[] pos) {
+		for (int i=0; i < position.length; ++i)
+			position[i] += pos[i];
 	}
 
 	@Override
-	public void move(long[] position) {
-		for (int i=0; (i < position.length) && (i < numDimensions()); i++) {
-			this.position[i] += position[i];
-		}
+	public void move(final long[] pos) {
+		for (int i=0; i < position.length; ++i)
+			position[i] += pos[i];
 	}
 
 	@Override
-	public void setPosition(Localizable localizable) {
-		localizable.localize(position);
+	public void setPosition(final Localizable localizable) {
+		for (int i=0; i < position.length; ++i)
+			position[i] = localizable.getLongPosition(i);
 	}
 
 	@Override
-	public void setPosition(int[] position) {
-		for (int i=0; (i < position.length) && (i < numDimensions()); i++) {
-			this.position[i] = position[i];
-		}
+	public void setPosition(final int[] pos) {
+		for (int i=0; i < position.length; ++i)
+			position[i] = pos[i];
 	}
 
 	@Override
-	public void setPosition(long[] position) {
-		for (int i=0; (i < position.length) && (i < numDimensions()); i++) {
-			this.position[i] = position[i];
-		}
+	public void setPosition(final long[] pos) {
+		for (int i=0; i < position.length; ++i)
+			position[i] = pos[i];
 	}
 
 	@Override
-	public void setPosition(int position, int d) {
-		if (d < numDimensions())
-			this.position[d] = position;
+	public void setPosition(final int pos, final int d) {
+		position[d] = pos;
 	}
 
 	@Override
-	public void setPosition(long position, int d) {
-		if (d < numDimensions())
-			this.position[d] = position;
+	public void setPosition(final long pos, final int d) {
+		position[d] = pos;
 	}
 
 	@Override
-	public void localize(int[] position) {
-		for (int i=0; (i < position.length) && (i < numDimensions()); i++) {
-			position[i] = (int)(this.position[i]);
-		}
+	public void localize(final int[] pos) {
+		for (int i=0; i < position.length; ++i)
+			pos[i] = (int)(this.position[i]);
 	}
 
 	@Override
-	public void localize(long[] position) {
-		for (int i=0; (i < position.length) && (i < numDimensions()); i++) {
-			position[i] = this.position[i];
-		}
+	public void localize(final long[] pos) {
+		for (int i=0; i < position.length; ++i)
+			pos[i] = position[i];
 	}
 
 	@Override
-	public int getIntPosition(int d) {
-		if (d >= numDimensions()) return 0;
+	public int getIntPosition(final int d) {
 		return (int)(position[d]);
 	}
 
 	@Override
-	public long getLongPosition(int d) {
-		if (d >= numDimensions()) return 0;
+	public long getLongPosition(final int d) {
 		return position[d];
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
+		final StringBuilder sb = new StringBuilder();
 		char c = '(';
 		for (int i=0; i<numDimensions(); i++) {
 			sb.append(c);
@@ -187,5 +173,4 @@ public class Point implements Localizable, Positionable, EuclideanSpace {
 		sb.append(")");
 		return sb.toString();
 	}
-
 }
