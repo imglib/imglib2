3664a5142af6dc13861a62e2fa3e24a499f5758c
Stephan Preibisch <preibisch@mpi-cbg.de>
2011-05-20 15:53:22 +0200

    Added another outofboundsstrategy

--- imglib1/GameOfDeath/src/main/java/game/Arena.java
+++ imglib1/GameOfDeath/src/main/java/game/Arena.java
@@ -67,7 +67,7 @@ public class Arena
 			}
 			
 			/* diffusion */
-			final GaussianConvolution<LifeForm> gauss = new GaussianConvolution<LifeForm>( arena, new OutOfBoundsStrategyValueFactory<LifeForm>(), 1.5f );
+			final GaussianConvolution<LifeForm> gauss = new GaussianConvolution<LifeForm>( arena, new OutOfBoundsStrategyMirrorFactory<LifeForm>(), 1.5f );
 			
 			if ( !gauss.checkInput() || ! gauss.process() )
 			{
