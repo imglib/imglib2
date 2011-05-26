302d03c69be0c3746b9f9dc6d72892e7c6f4be73
Stephan Preibisch <preibisch@mpi-cbg.de>
2011-05-26 08:42:37 +0200

    Added missing RGB Virtual stack

---
+++ imglib2/ij/src/main/java/net/imglib2/img/display/imagej/ImageJVirtualStackARGB.java
@@ -0,0 +1,16 @@
+package net.imglib2.img.display.imagej;
+
+import ij.ImagePlus;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converter;
+import net.imglib2.type.Type;
+import net.imglib2.type.numeric.ARGBType;
+
+public class ImageJVirtualStackARGB< S extends Type< S > > extends ImageJVirtualStack< S, ARGBType >
+{
+	public ImageJVirtualStackARGB( RandomAccessibleInterval< S > source, Converter< S, ARGBType > converter )
+	{
+		super( source, converter, new ARGBType(), ImagePlus.COLOR_RGB );
+		imageProcessor.setMinAndMax( 0, 255 );
+	}
+}
