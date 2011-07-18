5a15d8e0177737ce0be44307d1e32d7861283545
Stephan Preibisch <stephan.preibisch@gmx.de>
2011-07-18 19:01:36 +0200

    Added mirroring algorithm (n-dimensional)

--- imglib1/algorithms/src/main/java/mpicbg/imglib/algorithm/gauss/DownSample.java
+++ imglib1/algorithms/src/main/java/mpicbg/imglib/algorithm/gauss/DownSample.java
@@ -12,7 +12,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  * 
- * @author Stephan Saalfeld
+ * @author Stephan Saalfeld (imglib implementation by Stephan Preibisch)
  */
 package mpicbg.imglib.algorithm.gauss;
 
