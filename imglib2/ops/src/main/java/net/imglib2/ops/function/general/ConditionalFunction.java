db6a0cbc8aee3332b0aad37b7eef927defe274ba
Barry DeZonia <bdezonia@wisc.edu>
2011-08-17 14:39:00 -0500

    update all copyright notices

--- imglib2/ops/src/main/java/net/imglib2/ops/function/general/ConditionalFunction.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/function/general/ConditionalFunction.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2011, Stephan Preibisch & Stephan Saalfeld.
+Copyright (c) 2011, Barry DeZonia.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
