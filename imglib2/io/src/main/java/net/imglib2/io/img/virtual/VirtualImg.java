38b8dc06ceefdbb19424ec4e65920168cb02c8c2
Barry DeZonia <bdezonia@wisc.edu>
2011-09-02 14:42:13 -0500

    improve the construction of VirtualImg's. now they are only constructed via a public static factory method that relies on filename only.

--- imglib2/io/src/main/java/net/imglib2/io/img/virtual/VirtualImg.java
+++ imglib2/io/src/main/java/net/imglib2/io/img/virtual/VirtualImg.java
@@ -28,6 +28,9 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package net.imglib2.io.img.virtual;
 
+import java.io.IOException;
+
+import loci.formats.FormatException;
 import loci.formats.FormatTools;
 import loci.formats.IFormatReader;
 
@@ -37,6 +40,8 @@ import net.imglib2.RandomAccess;
 import net.imglib2.img.AbstractImg;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
+import net.imglib2.io.ImgIOException;
+import net.imglib2.io.ImgOpener;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.IntType;
@@ -65,23 +70,19 @@ public class VirtualImg<T extends NativeType<T>> extends AbstractImg<T> {
 	// that only one user of the reader can be defined. Unless reader is
 	// already thread safe.
 	
-	public VirtualImg(long[] dims, IFormatReader reader)
+	private VirtualImg(long[] dims, IFormatReader reader, T type)
 	{
 		super(dims);
 		this.dims = dims.clone();
 		this.reader = reader;
-		this.type = pixelType(reader.getPixelType());
+		this.type = type.copy();
 		checkPlaneShape();
 	}
 
-	// TODO
-	/*
-	public VirtualImg(String fileName) {
-		super(ProblemWhatDimsDoIPassHereCannotResetLater);
+	public static VirtualImg<?> create(String fileName) throws ImgIOException {
+		IFormatReader reader;
 		try {
-			reader = ImgOpener.createReader(fileName, false);
-			type = ImgOpener.makeType(reader.getPixelType());
-			dims = ImgOpener.getDimLengths(reader);
+			reader = ImgOpener.createReader(fileName, false);  // TODO - or true?
 		}
 		catch (final FormatException e) {
 			throw new ImgIOException(e);
@@ -89,8 +90,29 @@ public class VirtualImg<T extends NativeType<T>> extends AbstractImg<T> {
 		catch (final IOException e) {
 			throw new ImgIOException(e);
 		}
+		long[] dims = ImgOpener.getDimLengths(reader);
+		
+		switch (reader.getPixelType()) {
+			case FormatTools.UINT8:
+				return new VirtualImg<UnsignedByteType>(dims, reader, new UnsignedByteType());
+			case FormatTools.INT8:
+				return new VirtualImg<ByteType>(dims, reader, new ByteType());
+			case FormatTools.UINT16:
+				return new VirtualImg<UnsignedShortType>(dims, reader, new UnsignedShortType());
+			case FormatTools.INT16:
+				return new VirtualImg<ShortType>(dims, reader, new ShortType());
+			case FormatTools.UINT32:
+				return new VirtualImg<UnsignedIntType>(dims, reader, new UnsignedIntType());
+			case FormatTools.INT32:
+				return new VirtualImg<IntType>(dims, reader, new IntType());
+			case FormatTools.FLOAT:
+				return new VirtualImg<FloatType>(dims, reader, new FloatType());
+			case FormatTools.DOUBLE:
+				return new VirtualImg<DoubleType>(dims, reader, new DoubleType());
+			default:
+				throw new IllegalArgumentException("VirtualImg::create() : unsupported pixel format");
+		}
 	}
-	*/
 	
 	@Override
 	public RandomAccess<T> randomAccess() {
@@ -121,7 +143,7 @@ public class VirtualImg<T extends NativeType<T>> extends AbstractImg<T> {
 
 	@Override
 	public Img<T> copy() {
-		return new VirtualImg<T>(dims, reader);
+		return new VirtualImg<T>(dims, reader, type);
 	}
 
 	public T getType() {
@@ -165,40 +187,4 @@ public class VirtualImg<T extends NativeType<T>> extends AbstractImg<T> {
 				"VirtualImg : size of dimension "+d+" does not match IFormatReader");
 	}
 
-	// TODO - adapted from ImgOpener::makeType(). Could not fix compiler errors to reuse it.
-	//   Undo this borrowing.
-	
-	@SuppressWarnings("unchecked")
-	private T pixelType(int pixelType) {
-		final NativeType<?> imgLibType;
-		switch (pixelType) {
-			case FormatTools.UINT8:
-				imgLibType = new UnsignedByteType();
-				break;
-			case FormatTools.INT8:
-				imgLibType = new ByteType();
-				break;
-			case FormatTools.UINT16:
-				imgLibType = new UnsignedShortType();
-				break;
-			case FormatTools.INT16:
-				imgLibType = new ShortType();
-				break;
-			case FormatTools.UINT32:
-				imgLibType = new UnsignedIntType();
-				break;
-			case FormatTools.INT32:
-				imgLibType = new IntType();
-				break;
-			case FormatTools.FLOAT:
-				imgLibType = new FloatType();
-				break;
-			case FormatTools.DOUBLE:
-				imgLibType = new DoubleType();
-				break;
-			default:
-				imgLibType = null;
-		}
-		return (T) imgLibType;
-	}
 }
