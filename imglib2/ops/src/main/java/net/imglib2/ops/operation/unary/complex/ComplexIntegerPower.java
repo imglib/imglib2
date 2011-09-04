e0abff2b55787e96749bf80b0869fc2358652cab
Barry DeZonia <bdezonia@wisc.edu>
2011-09-04 11:46:21 -0500

    OPS - add more Complex and Real operations

---
+++ imglib2/ops/src/main/java/net/imglib2/ops/operation/unary/complex/ComplexIntegerPower.java
@@ -0,0 +1,70 @@
+/*
+
+Copyright (c) 2011, Barry DeZonia.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+  * Neither the name of the Fiji project developers nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package net.imglib2.ops.operation.unary.complex;
+
+import net.imglib2.ops.ComplexOutput;
+import net.imglib2.ops.UnaryOperation;
+import net.imglib2.ops.Complex;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class ComplexIntegerPower extends ComplexOutput
+	implements UnaryOperation<Complex,Complex>
+{
+	private static final ComplexReciprocal recipFunc = new ComplexReciprocal();
+	private static final Complex one = Complex.createCartesian(1,0);
+	
+	private int power;
+	private Complex variable;
+	
+	public ComplexIntegerPower(int power) {
+		this.power = power;
+		this.variable = new Complex();
+	}
+	
+	@Override
+	public void compute(Complex input, Complex output) {
+		if (power == 0)
+			output.setValue(one);
+		else {
+			if (power < 0)
+				recipFunc.compute(input, variable);
+			else // power > 0
+				variable.setValue(input);
+			double r = Math.pow(variable.getModulus(), power);
+			double theta = power * variable.getArgument();
+			output.setPolar(r, theta);
+			output.setPolar(r, output.getPrincipleArgument());
+		}
+	}
+}
