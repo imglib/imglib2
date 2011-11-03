6bf88a871b4c6e8fb3ff2259ce0ae06e6c80f741
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-03 19:18:08 -0400

    imglib2-scripting: new FloatImage(long[]) class to create an array float image.

---
+++ imglib2/scripting/src/main/java/net/imglib2/script/edit/FloatImage.java
@@ -0,0 +1,21 @@
+package net.imglib2.script.edit;
+
+import java.util.List;
+
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.basictypeaccess.array.FloatArray;
+import net.imglib2.script.algorithm.fn.AlgorithmUtil;
+import net.imglib2.type.numeric.real.FloatType;
+
+/** Create a new n-dimensional image based on an array of float. */
+public class FloatImage extends ArrayImg<FloatType, FloatArray>
+{
+	public FloatImage(final List<Number> dim) {
+		this(AlgorithmUtil.asLongArray(dim));
+	}
+	
+	public FloatImage(final long[] dim) {
+		super(new FloatArray(new float[AlgorithmUtil.size(dim)]), dim, 1);
+		setLinkedType(new FloatType(this));
+	}
+}
