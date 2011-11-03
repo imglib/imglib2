5e6a5334e0faa2a448af81fb0e003553abcde9cd
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-03 18:24:48 -0400

    imglib2-scripting: RectangleROI also accepts a List for the 4 numeric entries.

--- imglib2/scripting/src/main/java/net/imglib2/script/view/RectangleROI.java
+++ imglib2/scripting/src/main/java/net/imglib2/script/view/RectangleROI.java
@@ -1,5 +1,7 @@
 package net.imglib2.script.view;
 
+import java.util.List;
+
 import net.imglib2.RandomAccessible;
 import net.imglib2.type.numeric.NumericType;
 
@@ -9,16 +11,21 @@ import net.imglib2.type.numeric.NumericType;
  */
 public class RectangleROI<R extends NumericType<R>> extends ROI<R>
 {
-	public RectangleROI(RandomAccessible<R> img, Number x, Number y, Number width, Number height) {
+	public RectangleROI(final RandomAccessible<R> img, final List<Number> bounds) {
+		this(img, bounds.get(0), bounds.get(1), bounds.get(2), bounds.get(3));
+	}
+	
+	public RectangleROI(final RandomAccessible<R> img,
+			final Number x, final Number y, final Number width, final Number height) {
 		super(img,
 				toArray(img.numDimensions(), x.intValue(), y.intValue()),
 				toArray(img.numDimensions(), x.intValue() + width.intValue() - 1, y.intValue() + height.intValue() - 1));
 	}
 
-	static private final long[] toArray(final int nDim, final int d0, final int d1) {
-		final long[] dims = new long[nDim];
-		dims[0] = d0;
-		dims[1] = d1;
-		return dims;
+	static private final long[] toArray(final int nDim, final int p0, final int p1) {
+		final long[] pos = new long[nDim];
+		pos[0] = p0;
+		pos[1] = p1;
+		return pos;
 	}
 }
