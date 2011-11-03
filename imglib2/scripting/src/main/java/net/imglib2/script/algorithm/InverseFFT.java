11e141f4d5fde072b41c3b08129cfb2d8a7f883a
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-03 10:07:03 -0400

    imglib2-scripting: fixed InverseFFT

--- imglib2/scripting/src/main/java/net/imglib2/script/algorithm/InverseFFT.java
+++ imglib2/scripting/src/main/java/net/imglib2/script/algorithm/InverseFFT.java
@@ -2,21 +2,22 @@ package net.imglib2.script.algorithm;
 
 import net.imglib2.algorithm.fft.FourierTransform;
 import net.imglib2.algorithm.fft.InverseFourierTransform;
-import net.imglib2.img.Image;
+import net.imglib2.img.Img;
+import net.imglib2.script.algorithm.fn.ImgProxy;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.complex.ComplexDoubleType;
 
-public class InverseFFT<T extends RealType<T>> extends Image<T>
+public class InverseFFT<T extends RealType<T>> extends ImgProxy<T>
 {
 	public InverseFFT(final FFT<T> fftImage) throws Exception {
-		super(process(fftImage, fftImage.fft).getContainer(), fftImage.value.copy(), "Inverse FFT");
+		super(process(fftImage, fftImage.fft));
 	}
 
-	public InverseFFT(final Image<ComplexDoubleType> img, final FFT<T> fftImage) throws Exception {
-		super(process(img, fftImage.fft).getContainer(), fftImage.value.copy(), "Inverse FFT");
+	public InverseFFT(final Img<ComplexDoubleType> img, final FFT<T> fftImage) throws Exception {
+		super(process(img, fftImage.fft));
 	}
 
-	static private final <T extends RealType<T>> Image<T> process(final Image<ComplexDoubleType> fftImage, final FourierTransform<T, ComplexDoubleType> fft) throws Exception {
+	static private final <T extends RealType<T>> Img<T> process(final Img<ComplexDoubleType> fftImage, final FourierTransform<T, ComplexDoubleType> fft) throws Exception {
 		final InverseFourierTransform<T, ComplexDoubleType> ifft = new InverseFourierTransform<T, ComplexDoubleType>(fftImage, fft);
 		if (!ifft.checkInput() || !ifft.process()) {
 			throw new Exception("FFT: failed to process for image " + fftImage.getClass() + " -- " + ifft.getErrorMessage());
