f3d5a7a9fb83477487d8a9a75fd87089caa5160f
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-10 17:05:50 -0500

    imglib2-scripting: ARGBImage to create a new ARGB Img

---
+++ imglib2/scripting/src/main/java/net/imglib2/script/edit/ARGBImage.java
@@ -0,0 +1,26 @@
+package net.imglib2.script.edit;
+
+import java.util.List;
+
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.script.algorithm.fn.AlgorithmUtil;
+import net.imglib2.type.numeric.ARGBType;
+
+/** Create a new n-dimensional image based on an array of byte. */
+public class ARGBImage extends ArrayImg<ARGBType, IntArray>
+{
+	public ARGBImage(final List<Number> dim) {
+		this(AlgorithmUtil.asLongArray(dim));
+	}
+
+	public ARGBImage(final long[] dim) {
+		this(dim, new int[AlgorithmUtil.size(dim)]);
+	}
+
+	public ARGBImage(final long[] dim, final int[] pixels) {
+		super(new IntArray(pixels), dim, 1);
+		setLinkedType(new ARGBType(this));
+	}
+}
+
