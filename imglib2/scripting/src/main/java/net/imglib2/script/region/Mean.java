68e9ab8bbc7ec79bdfbb3bc0739ade509aaa1903
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-23 09:56:45 -0500

    imglib2-scripting: naive region operations. Versions that iterate less could be created.

---
+++ imglib2/scripting/src/main/java/net/imglib2/script/region/Mean.java
@@ -0,0 +1,31 @@
+package net.imglib2.script.region;
+
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.script.region.fn.ARegionFn;
+import net.imglib2.type.numeric.RealType;
+
+public class Mean<T extends RealType<T>> extends ARegionFn<T>
+{
+	private final double size;
+	
+	public Mean(Img<T> img, RandomAccess<T> ra, long span) {
+		super(img, ra, span);
+		this.size = img.size();
+	}
+
+	@Override
+	protected final double fn0(final double a) {
+		return a / size;
+	}
+	
+	@Override
+	protected final double fnE(final double r) {
+		return r;
+	}
+
+	@Override
+	protected final double fnR(final double r, final double a) {
+		return r + a / size;
+	}
+}
---
+++ imglib2/scripting/src/main/java/net/imglib2/script/region/Min.java
@@ -0,0 +1,18 @@
+package net.imglib2.script.region;
+
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.script.region.fn.ARegionFn;
+import net.imglib2.type.numeric.RealType;
+
+public class Min<T extends RealType<T>> extends ARegionFn<T>
+{
+	public Min(Img<T> img, RandomAccess<T> ra, long span) {
+		super(img, ra, span);
+	}
+
+	@Override
+	protected final double fnR(final double r, final double a) {
+		return Math.min(r, a);
+	}
+}
