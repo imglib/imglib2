91668b2bb3ae1646b25cdb87bfc6feefb0c04f22
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2011-11-23 14:49:44 +0100

    added javadoc.

--- imglib2/algorithms/src/main/java/net/imglib2/algorithm/mser/ComputeDeltaValue.java
+++ imglib2/algorithms/src/main/java/net/imglib2/algorithm/mser/ComputeDeltaValue.java
@@ -5,9 +5,17 @@ package net.imglib2.algorithm.mser;
  * For a given threshold value compute the threshold value delta steps down the component tree.
  * This might mean addition or subtraction, depending on whether it's a dark-to-bright or bright-to-dark pass.
  *  
+ * @author Tobias Pietzsch
+ *
  * @param <T>
+ *            value type of the input image.
  */
 public interface ComputeDeltaValue< T >
 {
+	/**
+	 * Compute the threshold = (value - delta).
+	 *
+	 * @return (value - delta)
+	 */
 	public T valueMinusDelta( T value );
 }
