5a2dcdc00b756bc85f226a392346ac45d9cf2673
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-24 08:50:58 -0500

    imglib2-scripting: properly test Saalfeld's mpicbg.integral.DoubleIntegralImage

--- imglib2/scripting/src/test/java/script/imglib/test/TestIntegralImagePerformance.java
+++ imglib2/scripting/src/test/java/script/imglib/test/TestIntegralImagePerformance.java
@@ -1,8 +1,15 @@
 package script.imglib.test;
 
+import java.lang.reflect.Field;
+
+import ij.ImageJ;
+import ij.ImagePlus;
 import ij.process.FloatProcessor;
 import mpicbg.ij.integral.DoubleIntegralImage;
+import net.imglib2.exception.ImgLibException;
+import net.imglib2.script.ImgLib;
 import net.imglib2.script.algorithm.IntegralImage;
+import net.imglib2.script.img.DoubleImage;
 import net.imglib2.script.img.FloatImage;
 
 
@@ -22,10 +29,26 @@ public class TestIntegralImagePerformance {
 		for (int i=0; i<10; ++i) {
 			long t0 = System.currentTimeMillis();
 			DoubleIntegralImage dii = new DoubleIntegralImage(fp);
+			FloatProcessor ip = new FloatProcessor(side, side);
+			for (int y=0; y<ip.getHeight(); ++y){
+				for (int x=0; x<ip.getWidth(); ++x){
+					ip.setf(x, y, (float)dii.getDoubleSum(x,y));
+				}
+			}
 			long t1 = System.currentTimeMillis();
 			IntegralImage dig = new IntegralImage(fii); // also uses double as type
 			long t2 = System.currentTimeMillis();
-			System.out.println("Saalfelds: " + (t2 - t1) + " ms -- imglib2.script: " + (t1 - t0) + " ms");
+			System.out.println("Saalfeld'ss: " + (t2 - t1) + " ms -- imglib2.script: " + (t1 - t0) + " ms");
+			if (i == 1) {
+				new ImageJ();
+				try {
+					new ImagePlus("Saalfeld's", ip).show();
+					ImgLib.show(dig, "Mine");
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
