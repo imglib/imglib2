0a6a53502d6d4e74d980b124261d02fc836dec24
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-27 02:21:52 -0500

    imglib2-scripting: minor fixes to Util

--- imglib2/scripting/src/main/java/net/imglib2/script/math/fn/Util.java
+++ imglib2/scripting/src/main/java/net/imglib2/script/math/fn/Util.java
@@ -1,11 +1,12 @@
 package net.imglib2.script.math.fn;
 
 import net.imglib2.IterableRealInterval;
+import net.imglib2.RealInterval;
 import net.imglib2.type.numeric.RealType;
 
 public class Util
 {
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	static public final IFunction wrap(final Object ob) {
 		if (ob instanceof IterableRealInterval<?>) return new ImageFunction((IterableRealInterval<? extends RealType<?>>)ob);
 		if (ob instanceof IFunction) return (IFunction)ob;
@@ -13,7 +14,7 @@ public class Util
 		throw new IllegalArgumentException("Cannot compose a function with " + ob);
 	}
 	
-	static public final long[] extractDimensions(final IterableRealInterval<?> iri) {
+	static public final long[] extractDimensions(final RealInterval iri) {
 		final long[] dim = new long[iri.numDimensions()];
 		for (int d=0; d<dim.length; ++d)
 			dim[d] = (long) (iri.realMax(d) - iri.realMin(d) + 1);
