d39e54a578635c8edf951f07e7458393651fded5
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-27 03:00:05 -0500

    imglib2-scripting: ROI and RectangleROI are views smaller or larger than the images. When larger, the image must be an extended view that knows how to deliver values beyond the interval of the image.
    Fixed errors in RectangleROI that prevented dimensions beyond X,Y from being present.

--- imglib2/scripting/src/main/java/net/imglib2/script/view/ROI.java
+++ imglib2/scripting/src/main/java/net/imglib2/script/view/ROI.java
@@ -1,21 +1,33 @@
 package net.imglib2.script.view;
 
-import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.script.algorithm.fn.RandomAccessibleIntervalImgProxy;
 import net.imglib2.type.numeric.NumericType;
 import net.imglib2.view.Views;
 
+/**
+ * ROI is a view of the image, either smaller or larger.
+ * If larger, the image should be an extension of an image that can provide data for the outside domain,
+ * for example {@code new ROI(new ExtendMirrorDouble(...), ...)}.
+ * 
+ * @author Albert Cardona
+ *
+ * @param <R>
+ */
 public class ROI<R extends NumericType<R>> extends RandomAccessibleIntervalImgProxy<R>
 {
-	/**
-	 * A new image which is just a view of the domain between {@param min} and {@param max}
-	 * so that the dimensions of the image are now (max - min + 1) for every dimension.
-	 */
-	public ROI(final RandomAccessible<R> img, final long[] min, final long[] max) {
-		super(Views.zeroMin(Views.interval(img, min, max)));
+	public ROI(final RandomAccessibleInterval<R> img, final long[] offset, final long[] dimensions) {
+		super(Views.zeroMin(Views.interval(img, offset, asMax(offset, dimensions))));
 	}
 
-	public ROI(final RandomAccessibleIntervalImgProxy<R> proxy, final long[] min, final long[] max) {
-		this(proxy.getRandomAccessibleInterval(), min, max);
+	public ROI(final RandomAccessibleIntervalImgProxy<R> proxy, final long[] offset, final long[] dimensions) {
+		this(proxy.getRandomAccessibleInterval(), offset, dimensions);
+	}
+
+	private static final long[] asMax(final long[] offset, final long[] dimensions) {
+		final long[] max = new long[offset.length];
+		for (int i=0; i<offset.length; ++i)
+			max[i] = offset[i] + dimensions[i] - 1;
+		return max;
 	}
 }
--- imglib2/scripting/src/main/java/net/imglib2/script/view/RectangleROI.java
+++ imglib2/scripting/src/main/java/net/imglib2/script/view/RectangleROI.java
@@ -2,7 +2,7 @@ package net.imglib2.script.view;
 
 import java.util.List;
 
-import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.NumericType;
 
 /** Create a Rectangular ROI on the x,y plane, with other dimensions left the same.
@@ -16,7 +16,7 @@ public class RectangleROI<R extends NumericType<R>> extends ROI<R>
 	 * @param img
 	 * @param bounds A list containing the bounds values x,y,width,height.
 	 */
-	public RectangleROI(final RandomAccessible<R> img, final List<Number> bounds) {
+	public RectangleROI(final RandomAccessibleInterval<R> img, final List<Number> bounds) {
 		this(img, bounds.get(0), bounds.get(1), bounds.get(2), bounds.get(3));
 	}
 	
@@ -26,21 +26,31 @@ public class RectangleROI<R extends NumericType<R>> extends ROI<R>
 	 * @param bounds An array containing the bounds values x,y,width,height.
 	 */
 	@SuppressWarnings("boxing")
-	public RectangleROI(final RandomAccessible<R> img, final long[] bounds) {
+	public RectangleROI(final RandomAccessibleInterval<R> img, final long[] bounds) {
 		this(img, bounds[0], bounds[1], bounds[2], bounds[3]);
 	}
 
-	public RectangleROI(final RandomAccessible<R> img,
+	public RectangleROI(final RandomAccessibleInterval<R> img,
 			final Number x, final Number y, final Number width, final Number height) {
 		super(img,
-				toArray(img.numDimensions(), x.intValue(), y.intValue()),
-				toArray(img.numDimensions(), x.intValue() + width.intValue() - 1, y.intValue() + height.intValue() - 1));
+				toMinArray(img, x.intValue(), y.intValue()),
+				toMaxArray(img, width.intValue(), height.intValue()));
 	}
 
-	static private final long[] toArray(final int nDim, final int p0, final int p1) {
-		final long[] pos = new long[nDim];
+	static private final long[] toMinArray(final RandomAccessibleInterval<?> img, final int p0, final int p1) {
+		final long[] pos = new long[img.numDimensions()];
 		pos[0] = p0;
 		pos[1] = p1;
+		return pos;
+	}
+	
+	static private final long[] toMaxArray(final RandomAccessibleInterval<?> img, final int p0, final int p1) {
+		final long[] pos = new long[img.numDimensions()];
+		pos[0] = p0;
+		pos[1] = p1;
+		for (int i=2; i<pos.length; ++i) {
+			pos[i] = img.dimension(i);
+		}
 		return pos;
 	}
 }
