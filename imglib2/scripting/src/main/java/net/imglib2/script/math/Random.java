436f497017db783ec47d0528c015ec1afdbe40a7
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-12-09 14:55:46 -0500

    imglib2-scripting: fixes for Random IFunction.

--- imglib2/scripting/src/main/java/net/imglib2/script/math/Random.java
+++ imglib2/scripting/src/main/java/net/imglib2/script/math/Random.java
@@ -1,5 +1,7 @@
 package net.imglib2.script.math;
 
+import net.imglib2.script.math.fn.IFunction;
+import net.imglib2.script.math.fn.NumberFunction;
 import net.imglib2.script.math.fn.UnaryOperation;
 
 public class Random extends UnaryOperation
@@ -15,9 +17,26 @@ public class Random extends UnaryOperation
 		super(seed);
 		rand = new java.util.Random(seed.longValue());
 	}
+	
+	public Random(final NumberFunction seed) {
+		super(seed);
+		rand = new java.util.Random((long)seed.eval());
+	}
+	
+	@SuppressWarnings("boxing")
+	public Random(final java.util.Random rand) {
+		super(0); // bogus
+		this.rand = rand;
+	}
 
 	@Override
 	public final double eval() {
 		return rand.nextDouble();
+	}
+	
+	/** Duplicates this IFunction but not the random number generator. */
+	@Override
+	public IFunction duplicate() {
+		return new Random(this.rand);
 	}
 }
