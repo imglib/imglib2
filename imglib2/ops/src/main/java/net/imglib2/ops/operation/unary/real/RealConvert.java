963a73c31f7265c6ce942900e1f69e8e66e14caf
Barry DeZonia <bdezonia@wisc.edu>
2011-12-12 20:42:25 +0100

    add a RealConvert operation that can do type scaling and conversion

---
+++ imglib2/ops/src/main/java/net/imglib2/ops/operation/unary/real/RealConvert.java
@@ -0,0 +1,77 @@
+/*
+
+Copyright (c) 2011, Barry DeZonia.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+  * Neither the name of the Fiji project developers nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package net.imglib2.ops.operation.unary.real;
+
+import net.imglib2.ops.UnaryOperation;
+import net.imglib2.type.numeric.RealType;
+
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public final class RealConvert<T extends RealType<T>, U extends RealType<U>>
+	implements UnaryOperation<T,U>
+{
+	U outType;
+	private double scale;
+	private double inputMin;
+	private double inputMax;
+	private double outputMin;
+	private double outputMax;
+	
+	public RealConvert(U outType, double inputMin, double inputMax,
+			double outputMin, double outputMax)
+	{
+		this.outType = outType;
+		this.inputMin = inputMin;
+		this.inputMax = inputMax;
+		this.outputMin = outputMin;
+		this.outputMax = outputMax;
+		this.scale = (outputMax - outputMin) / (inputMax - inputMin);
+	}
+	
+	@Override
+	public void compute(T x, U output) {
+		double value = (x.getRealDouble() - inputMin) * scale + outputMin;
+		output.setReal(value);
+	}
+
+	@Override
+	public RealConvert<T,U> copy() {
+		return new RealConvert<T,U>(outType, inputMin, inputMax, outputMin, outputMax);
+	}
+
+	@Override
+	public U createOutput(T dataHint) {
+		return outType.createVariable();
+	}
+}
