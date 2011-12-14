532313703a04c622819288f50d73ad0375699070
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2011-12-14 18:36:38 +0100

    fixed compile error.

--- imglib2/algorithms/src/main/java/net/imglib2/algorithm/gauss/Gauss.java
+++ imglib2/algorithms/src/main/java/net/imglib2/algorithm/gauss/Gauss.java
@@ -13,11 +13,11 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 
-public class Gauss 
+public class Gauss
 {
 	/**
 	 * Computes a Gaussian convolution with float precision on an entire {@link Img} using the {@link OutOfBoundsMirrorFactory} with single boundary
-	 * 
+	 *
 	 * @param sigma - the sigma for the convolution
 	 * @param input - the input {@link Img}
 	 * @return the convolved image
@@ -29,42 +29,44 @@ public class Gauss
 
 	/**
 	 * Computes a Gaussian convolution with float precision on an entire {@link Img}
-	 * 
+	 *
 	 * @param sigma - the sigma for the convolution
 	 * @param input - the input {@link Img}
 	 * @param outofbounds - the {@link OutOfBoundsFactory}
 	 * @return the convolved image
 	 */
-	public static <T extends RealType<T>> Img<FloatType> inFloat( final double[] sigma, final Img< T > input, 
+	public static <T extends RealType<T>> Img<FloatType> inFloat( final double[] sigma, final Img< T > input,
 			final OutOfBoundsFactory< FloatType, RandomAccessibleInterval< FloatType > > outofbounds )
 	{
-		GaussFloat gauss = null;		
-		try 
-		{			
+		GaussFloat gauss = null;
+		try
+		{
 			if ( FloatType.class.isInstance( input.firstElement() ) )
 			{
-				gauss = new GaussFloat( sigma, (Img<FloatType>) input );
+				@SuppressWarnings( "unchecked" )
+				final Img< FloatType > img = (Img<FloatType>) input;
+				gauss = new GaussFloat( sigma, img );
 			}
 			else
 			{
-				final RandomAccessibleInterval<FloatType> rIn = new ConvertedIterableRandomAccessibleInterval< T, FloatType, Img<T> >( input, new RealFloatSamplerConverter<T>());				
+				final RandomAccessibleInterval<FloatType> rIn = new ConvertedIterableRandomAccessibleInterval< T, FloatType, Img<T> >( input, new RealFloatSamplerConverter<T>());
 				gauss = new GaussFloat( sigma, Views.extend( rIn, outofbounds ), input, input.factory().imgFactory( new FloatType() ) );
 			}
-		} 
-		catch (IncompatibleTypeException e) 
+		}
+		catch (final IncompatibleTypeException e)
 		{
 			return null;
 		}
-		
+
 		gauss.call();
-		
+
 		return (Img<FloatType>)gauss.getResult();
 	}
 
 	/**
-	 * Computes a Gaussian convolution in-place (temporary images are necessary) with float precision on an entire {@link Img} 
+	 * Computes a Gaussian convolution in-place (temporary images are necessary) with float precision on an entire {@link Img}
 	 * using the {@link OutOfBoundsMirrorFactory} with single boundary
-	 * 
+	 *
 	 * @param sigma - the sigma for the convolution
 	 * @param input - the input {@link Img}
 	 * @return the convolved image
@@ -75,33 +77,34 @@ public class Gauss
 	}
 
 	/**
-	 * Computes a Gaussian convolution in-place (temporary images are necessary) with float precision on an entire {@link Img} 
-	 * 
+	 * Computes a Gaussian convolution in-place (temporary images are necessary) with float precision on an entire {@link Img}
+	 *
 	 * @param sigma - the sigma for the convolution
 	 * @param image - the image {@link Img} that will be convolved in place
 	 */
-	public static <T extends RealType<T>> void inFloatInPlace( final double[] sigma, final Img< T > image, 
+	public static <T extends RealType<T>> void inFloatInPlace( final double[] sigma, final Img< T > image,
 			final OutOfBoundsFactory< FloatType, RandomAccessibleInterval< FloatType > > outofbounds )
 	{
-		GaussFloat gauss = null;		
-		try 
-		{			
+		GaussFloat gauss = null;
+		try
+		{
 			if ( FloatType.class.isInstance( image.firstElement() ) )
 			{
+				@SuppressWarnings( "unchecked" )
 				final Img< FloatType > img = (Img<FloatType>) image;
 				gauss = new GaussFloat( sigma, Views.extend( img, outofbounds ), image, img, new Location( sigma.length ), image.factory().imgFactory( new FloatType() ) );
 			}
 			else
 			{
-				final RandomAccessibleInterval<FloatType> rIn = new ConvertedIterableRandomAccessibleInterval< T, FloatType, Img<T> >( image, new RealFloatSamplerConverter<T>());				
+				final RandomAccessibleInterval<FloatType> rIn = new ConvertedIterableRandomAccessibleInterval< T, FloatType, Img<T> >( image, new RealFloatSamplerConverter<T>());
 				gauss = new GaussFloat( sigma, Views.extend( rIn, outofbounds ), image, rIn, new Location( sigma.length ), image.factory().imgFactory( new FloatType() ) );
 			}
-		} 
-		catch (IncompatibleTypeException e) 
+		}
+		catch (final IncompatibleTypeException e)
 		{
 			return;
 		}
-		
+
 		gauss.call();
 	}
 }
