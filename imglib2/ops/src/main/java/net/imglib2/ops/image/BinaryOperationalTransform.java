3f2eeb40c1b586b18c102e798589054ce9b21f8c
Barry DeZonia <bdezonia@wisc.edu>
2011-12-15 16:42:58 +0100

    commiting additional experimental code

---
+++ imglib2/ops/src/main/java/net/imglib2/ops/image/BinaryOperationalTransform.java
@@ -0,0 +1,8 @@
+package net.imglib2.ops.image;
+
+public class BinaryOperationalTransform {
+	assigns to a 2nd img in place
+  todo
+  optimized to use minimal cursors to transform a 2nd img in place using
+    info from itself and another Img
+}
