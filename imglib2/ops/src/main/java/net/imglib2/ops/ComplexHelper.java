416bdc6a9756c7a5da6cff5688b49d4c178da294
Barry DeZonia <bdezonia@wisc.edu>
2011-12-15 14:46:14 +0100

    finish a complex helper/utils class.
    this may get refactored out later.

--- imglib2/ops/src/main/java/net/imglib2/ops/ComplexHelper.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/ComplexHelper.java
@@ -44,16 +44,60 @@ public class ComplexHelper {
 		// do not instantiate
 	}
 	
-	public static double getModulus(ComplexType<?> complex) {
+	public static double getModulus(ComplexType<?> z) {
+		return Math.sqrt(getModulus2(z));
 	}
 
-	public static double getArgument(ComplexType<?> complex) {
+	public static double getModulus2(ComplexType<?> z) {
+		return
+			z.getRealDouble()*z.getRealDouble() +
+			z.getImaginaryDouble()*z.getImaginaryDouble();
+	}
+	
+	public static double getArgument(ComplexType<?> z) {
+		double theta;
+		double x = z.getRealDouble();
+		double y = z.getImaginaryDouble();
+		if (x == 0) {
+			if (y > 0)
+				theta = Math.PI / 2;
+			else if (y < 0)
+				theta = 3 * Math.PI / 2;
+			else // y == 0 : theta indeterminate
+				theta = 0;  // sensible default (?)
+		}
+		else if (y == 0) {
+			if (x > 0)
+				theta = 0;
+			else if (x < 0)
+				theta = Math.PI;
+			else // x == 0 : theta indeterminate
+				theta = 0;  // sensible default (?)
+		}
+		else { // x && y both != 0
+			double angle = Math.atan2(x,y);
+			if (x > 0) {
+				if (y > 0)
+					theta = angle;
+				else // y < 0
+					theta = angle + 2*Math.PI;
+			}
+			else // x < 0
+				theta = angle + Math.PI;
+		}
+		return theta;
 	}
 
 	public static double getPrincipleArgument(double angle) {
+		double arg = angle;
+		while (arg <= -Math.PI) arg += 2*Math.PI;
+		while (arg > Math.PI) arg -= 2*Math.PI;
+		return arg;
 	}
 	
-	public static void setPolar(ComplexType<?> c, double r, double theta) {
-		blaggo;
+	public static void setPolar(ComplexType<?> z, double r, double theta) {
+		double x = r * Math.cos(theta);
+		double y = r * Math.sin(theta);
+		z.setComplexNumber(x, y);
 	}
 }
