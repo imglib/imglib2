74f6f816ddf460895bcbb408a51aae3026d3f3e2
Barry DeZonia <bdezonia@wisc.edu>
2011-12-15 10:56:01 +0100

    port a few more complex operations

--- imglib2/ops/src/main/java/net/imglib2/ops/operation/unary/complex/ComplexArcsin.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/operation/unary/complex/ComplexArcsin.java
@@ -30,12 +30,10 @@ POSSIBILITY OF SUCH DAMAGE.
 package net.imglib2.ops.operation.unary.complex;
 
 import net.imglib2.ops.UnaryOperation;
-import net.imglib2.ops.Complex;
 import net.imglib2.ops.operation.binary.complex.ComplexAdd;
 import net.imglib2.ops.operation.binary.complex.ComplexMultiply;
 import net.imglib2.ops.operation.binary.complex.ComplexPower;
 import net.imglib2.ops.operation.binary.complex.ComplexSubtract;
-import net.imglib2.ops.sandbox.ComplexOutput;
 import net.imglib2.type.numeric.ComplexType;
 
 //Handbook of Mathematics and Computational Science, Harris & Stocker, Springer, 2006
@@ -45,29 +43,63 @@ import net.imglib2.type.numeric.ComplexType;
  * @author Barry DeZonia
  *
  */
-public final class ComplexArcsin<T extends ComplexType<T>>
-	implements UnaryOperation<T,T> {
+public final class ComplexArcsin<T extends ComplexType<T>, U extends ComplexType<U>>
+	implements UnaryOperation<T,U> {
 
-	private static final Complex I = Complex.createCartesian(0, 1);
-	private static final Complex MINUS_I = Complex.createCartesian(0, -1);
-	private static final Complex ONE = Complex.createCartesian(1, 0);
-	private static final Complex ONE_HALF = Complex.createCartesian(0.5, 0);
-
-	private static final ComplexMultiply mulFunc = new ComplexMultiply();
-	private static final ComplexSubtract diffFunc = new ComplexSubtract();
-	private static final ComplexPower powFunc = new ComplexPower();
-	private static final ComplexAdd addFunc = new ComplexAdd();
-	private static final ComplexLog logFunc = new ComplexLog();
+	private final ComplexCopy<T,U> copyFunc;
+	private final ComplexMultiply<U,U,U> mulFunc;
+	private final ComplexSubtract<U,U,U> diffFunc;
+	private final ComplexPower<U,U,U> powFunc;
+	private final ComplexAdd<U,U,U> addFunc;
+	private final ComplexLog<U,U> logFunc;
 	
-	private final Complex iz = new Complex();
-	private final Complex zSquared = new Complex();
-	private final Complex miniSum = new Complex();
-	private final Complex root = new Complex();
-	private final Complex sum = new Complex();
-	private final Complex logSum = new Complex();
+	private final U I;
+	private final U MINUS_I;
+	private final U ONE;
+	private final U ONE_HALF;
+
+	private final U z;
+	private final U iz;
+	private final U zSquared;
+	private final U miniSum;
+	private final U root;
+	private final U sum;
+	private final U logSum;
+
+	private U type;
+	
+	public ComplexArcsin(U type) {
+		this.type = type;
+
+		copyFunc = new ComplexCopy<T,U>(type);
+		mulFunc = new ComplexMultiply<U,U,U>(type);
+		diffFunc = new ComplexSubtract<U,U,U>(type);
+		powFunc = new ComplexPower<U,U,U>(type);
+		addFunc = new ComplexAdd<U,U,U>(type);
+		logFunc = new ComplexLog<U,U>(type);
+
+		I = type.createVariable();
+		MINUS_I = type.createVariable();
+		ONE = type.createVariable();
+		ONE_HALF = type.createVariable();
+		
+		z = type.createVariable();
+		iz = type.createVariable();
+		zSquared = type.createVariable();
+		miniSum = type.createVariable();
+		root = type.createVariable();
+		sum = type.createVariable();
+		logSum = type.createVariable();
+		
+		I.setComplexNumber(0, 1);
+		MINUS_I.setComplexNumber(0, -1);
+		ONE.setComplexNumber(1, 0);
+		ONE_HALF.setComplexNumber(0.5, 0);
+	}
 	
 	@Override
-	public void compute(Complex z, Complex output) {
+	public void compute(T in, U output) {
+		copyFunc.compute(in, z);
 		mulFunc.compute(I, z, iz);
 		mulFunc.compute(z, z, zSquared);
 		diffFunc.compute(ONE, zSquared, miniSum);
@@ -78,12 +110,12 @@ public final class ComplexArcsin<T extends ComplexType<T>>
 	}
 	
 	@Override
-	public ComplexArcsin copy() {
-		return new ComplexArcsin();
+	public ComplexArcsin<T,U> copy() {
+		return new ComplexArcsin<T,U>(type);
 	}
 
 	@Override
-	public T createOutput(T dataHint) {
-		return dataHint.createVariable();
+	public U createOutput(T dataHint) {
+		return type.createVariable();
 	}
 }
