69c74bfeb34dba607ba87f738dbefb881ca00c68
Barry DeZonia <bdezonia@wisc.edu>
2012-01-10 16:04:35 -0600

    eliminate warning

--- imglib2/ops/src/main/java/net/imglib2/ops/operation/unary/complex/ComplexArcsech.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/operation/unary/complex/ComplexArcsech.java
@@ -30,7 +30,6 @@ POSSIBILITY OF SUCH DAMAGE.
 package net.imglib2.ops.operation.unary.complex;
 
 import net.imglib2.ops.UnaryOperation;
-import net.imglib2.ops.operation.binary.complex.ComplexDivide;
 import net.imglib2.type.numeric.ComplexType;
 import net.imglib2.type.numeric.complex.ComplexDoubleType;
 
