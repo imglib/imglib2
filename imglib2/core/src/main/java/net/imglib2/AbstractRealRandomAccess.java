d13c137e31f9a156500a707316f75fa3d4694d7a
Stephan Saalfeld <saalfeld@saalfeld-mac.(none)>
2012-01-17 16:18:09 +0100

    fixed AbstractRealRandomAccess setPosition calls with Localizable and RealLocalizable.  The previous version was not compatible with the agreement on how to deal with different size vectors.

--- imglib2/core/src/main/java/net/imglib2/AbstractRealRandomAccess.java
+++ imglib2/core/src/main/java/net/imglib2/AbstractRealRandomAccess.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009--2010, Stephan Preibisch & Stephan Saalfeld
+ * Copyright (c) 2009--2012, ImgLib2 developers
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -9,7 +9,7 @@
  * list of conditions and the following disclaimer.  Redistributions in binary
  * form must reproduce the above copyright notice, this list of conditions and
  * the following disclaimer in the documentation and/or other materials
- * provided with the distribution.  Neither the name of the Fiji project nor
+ * provided with the distribution.  Neither the name of the imglib project nor
  * the names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
  * 
@@ -28,209 +28,159 @@
 package net.imglib2;
 
 /**
+ * Abstract base class for typical {@link RealRandomAccess} implementations.
+ * 
  * @author Lee Kamentsky
- *
  */
-public abstract class AbstractRealRandomAccess<T> extends
-		AbstractRealLocalizableSampler<T> implements RealRandomAccess<T> {
+public abstract class AbstractRealRandomAccess< T > extends AbstractRealLocalizableSampler< T > implements RealRandomAccess< T >
+{
 
-	protected AbstractRealRandomAccess(final int n) {
-		super(n);
+	protected AbstractRealRandomAccess( final int n )
+	{
+		super( n );
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#fwd(int)
-	 */
 	@Override
-	public void fwd(final int d) {
-		++position[d];
+	public void fwd( final int d )
+	{
+		++position[ d ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#bck(int)
-	 */
 	@Override
-	public void bck(final int d) {
-		--position[d];
+	public void bck( final int d )
+	{
+		--position[ d ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#move(int, int)
-	 */
 	@Override
-	public void move(final int distance, final int d) {
-		position[d] += distance;
+	public void move( final int distance, final int d )
+	{
+		position[ d ] += distance;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#move(long, int)
-	 */
 	@Override
-	public void move(final long distance, final int d) {
-		position[d] += distance;
+	public void move( final long distance, final int d )
+	{
+		position[ d ] += distance;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#move(net.imglib2.Localizable)
-	 */
 	@Override
-	public void move(final Localizable localizable) {
-		for (int d=0; d< n; d++) {
-			this.position[d] += localizable.getDoublePosition(d);
-		}
+	public void move( final Localizable localizable )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] += localizable.getDoublePosition( d );
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#move(int[])
-	 */
 	@Override
-	public void move(final int[] distance) {
-		for (int d=0; d< n; d++) {
-			this.position[d] += distance[d];
-		}
+	public void move( final int[] distance )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] += distance[ d ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#move(long[])
-	 */
 	@Override
-	public void move(final long[] distance) {
-		for (int d=0; d< n; d++) {
-			this.position[d] += distance[d];
-		}
+	public void move( final long[] distance )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] += distance[ d ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#setPosition(net.imglib2.Localizable)
-	 */
 	@Override
-	public void setPosition(final Localizable localizable) {
-		localizable.localize(position);
+	public void setPosition( final Localizable localizable )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] = localizable.getDoublePosition( d );
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#setPosition(int[])
-	 */
 	@Override
-	public void setPosition(final int[] position) {
-		for (int d=0; d < n; d++) {
-			this.position[d] = position[d]; 
-		}
+	public void setPosition( final int[] position )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] = position[ d ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#setPosition(long[])
-	 */
 	@Override
-	public void setPosition(final long[] position) {
-		for (int d=0; d < n; d++) {
-			this.position[d] = position[d]; 
-		}
+	public void setPosition( final long[] position )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] = position[ d ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#setPosition(int, int)
-	 */
 	@Override
-	public void setPosition(final int position, final int d) {
-		this.position[d] = position;
+	public void setPosition( final int position, final int d )
+	{
+		this.position[ d ] = position;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.Positionable#setPosition(long, int)
-	 */
 	@Override
-	public void setPosition(final long position, final int d) {
-		this.position[d] = position;
+	public void setPosition( final long position, final int d )
+	{
+		this.position[ d ] = position;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#move(float, int)
-	 */
 	@Override
-	public void move(final float distance, final int d) {
-		this.position[d] += distance;
+	public void move( final float distance, final int d )
+	{
+		this.position[ d ] += distance;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#move(double, int)
-	 */
 	@Override
-	public void move(final double distance, final int d) {
-		this.position[d] += distance;
+	public void move( final double distance, final int d )
+	{
+		this.position[ d ] += distance;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#move(net.imglib2.RealLocalizable)
-	 */
 	@Override
-	public void move(final RealLocalizable localizable) {
-		for (int i=0; i< n; i++) {
-			this.position[i] += localizable.getDoublePosition(i);
-		}
+	public void move( final RealLocalizable localizable )
+	{
+		for ( int i = 0; i < n; i++ )
+			this.position[ i ] += localizable.getDoublePosition( i );
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#move(float[])
-	 */
 	@Override
-	public void move(final float[] distance) {
-		for (int i=0; i< n; i++) {
-			this.position[i] += distance[i];
-		}
+	public void move( final float[] distance )
+	{
+		for ( int i = 0; i < n; i++ )
+			this.position[ i ] += distance[ i ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#move(double[])
-	 */
 	@Override
-	public void move(final double[] distance) {
-		for (int i=0; i< n; i++) {
-			this.position[i] += distance[i];
-		}
+	public void move( final double[] distance )
+	{
+		for ( int i = 0; i < n; i++ )
+			this.position[ i ] += distance[ i ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#setPosition(net.imglib2.RealLocalizable)
-	 */
 	@Override
-	public void setPosition(final RealLocalizable localizable) {
-		localizable.localize(position);
+	public void setPosition( final RealLocalizable localizable )
+	{
+		for ( int d = 0; d < n; d++ )
+			this.position[ d ] = localizable.getDoublePosition( d );
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#setPosition(float[])
-	 */
 	@Override
-	public void setPosition(final float[] position) {
-		for (int i=0; i< n; i++) {
-			this.position[i] = position[i];
-		}
+	public void setPosition( final float[] position )
+	{
+		for ( int i = 0; i < n; i++ )
+			this.position[ i ] = position[ i ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#setPosition(double[])
-	 */
 	@Override
-	public void setPosition(final double[] position) {
-		for (int i=0; i< n; i++) {
-			this.position[i] = position[i];
-		}
+	public void setPosition( final double[] position )
+	{
+		for ( int i = 0; i < n; i++ )
+			this.position[ i ] = position[ i ];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#setPosition(float, int)
-	 */
 	@Override
-	public void setPosition(final float position, final int d) {
-		this.position[d] = position;
+	public void setPosition( final float position, final int d )
+	{
+		this.position[ d ] = position;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.imglib2.RealPositionable#setPosition(double, int)
-	 */
 	@Override
-	public void setPosition(final double position, final int d) {
-		this.position[d] = position;
+	public void setPosition( final double position, final int d )
+	{
+		this.position[ d ] = position;
 	}
 }
