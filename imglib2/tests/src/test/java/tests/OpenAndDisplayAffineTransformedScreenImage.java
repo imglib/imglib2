9e7ba65c13d54fa3adee5ad4903231a01c718f98
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2012-01-30 22:20:42 +0100

    test considers linear calibration relative to x

--- imglib2/tests/src/test/java/tests/OpenAndDisplayAffineTransformedScreenImage.java
+++ imglib2/tests/src/test/java/tests/OpenAndDisplayAffineTransformedScreenImage.java
@@ -6,13 +6,13 @@ import ij.ImagePlus;
 import ij.process.ColorProcessor;
 import mpicbg.util.Timer;
 import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.display.ARGBScreenImage;
 import net.imglib2.display.RealARGBConverter;
 import net.imglib2.display.XYRandomAccessibleProjector;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgPlus;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.interpolation.Interpolant;
-import net.imglib2.interpolation.InterpolatorFactory;
 import net.imglib2.interpolation.randomaccess.NearestNeighborInterpolatorFactory;
 import net.imglib2.io.ImgIOException;
 import net.imglib2.io.ImgOpener;
@@ -34,18 +34,19 @@ public class OpenAndDisplayAffineTransformedScreenImage
 		
 		final ImgOpener io = new ImgOpener();
 		//final RandomAccessibleInterval< UnsignedShortType > img = io.openImg( "/home/saalfeld/phd/light-microscopy/presentation/saalfeld-05-05-4-DPX-05_L1_Sum.lsm", new ArrayImgFactory< UnsignedShortType >(), new UnsignedShortType());
-		final RandomAccessibleInterval< UnsignedShortType > img = io.openImg( "/home/saalfeld/phd/light-microscopy/presentation/saalfeld-05-05-4-DPX-05_L1_Sum.0.tif", new ArrayImgFactory< UnsignedShortType >(), new UnsignedShortType());
+		final ImgPlus< UnsignedShortType > imgPlus = io.openImg( "/home/saalfeld/phd/light-microscopy/presentation/saalfeld-05-05-4-DPX-05_L1_Sum.0.tif", new ArrayImgFactory< UnsignedShortType >(), new UnsignedShortType());
+		final Img< UnsignedShortType > img = imgPlus.getImg();
 		
 		final double[][] matrix = new double[][]{
 				{ 0.5, 0, 0, img.dimension( 0 ) * 0.25 },
-				{ 0, 0.5, 0, img.dimension( 1 ) * 0.25 },
-				{ 0, 0, 0.5, 0 }
+				{ 0, 0.5 * imgPlus.calibration( 1 ) / imgPlus.calibration( 0 ), 0, img.dimension( 1 ) * 0.25 },
+				{ 0, 0, 0.5 * imgPlus.calibration( 0 ) / imgPlus.calibration( 0 ), 0 }
 		};
 		final AffineTransform affine = new AffineTransform( new Matrix( matrix ) );
 //		final AffineTransform3D affine = new AffineTransform3D();
 //		affine.set( matrix[ 0 ][ 0 ], matrix[ 0 ][ 1 ], matrix[ 0 ][ 2 ], matrix[ 0 ][ 3 ], matrix[ 1 ][ 0 ], matrix[ 1 ][ 1 ], matrix[ 1 ][ 2 ], matrix[ 1 ][ 3 ], matrix[ 2 ][ 0 ], matrix[ 2 ][ 1 ], matrix[ 2 ][ 2 ], matrix[ 2 ][ 3 ] );
 		
-		final InterpolatorFactory< UnsignedShortType, RandomAccessible< UnsignedShortType> > interpolatorFactory = new NearestNeighborInterpolatorFactory< UnsignedShortType >();
+		final NearestNeighborInterpolatorFactory< UnsignedShortType > interpolatorFactory = new NearestNeighborInterpolatorFactory< UnsignedShortType >();
 //		final InterpolatorFactory< UnsignedShortType, RandomAccessible< UnsignedShortType> > interpolatorFactory = new NLinearInterpolatorFactory< UnsignedShortType >();
 		
 		final RandomAccessible< UnsignedShortType > extendedImg = Views.extendValue( img, new UnsignedShortType() );
@@ -123,9 +124,10 @@ public class OpenAndDisplayAffineTransformedScreenImage
 				//System.out.println( affine );
 				
 				projector.map();
-				final ColorProcessor cpa = new ColorProcessor( screenImage.image() );
-				imp.setProcessor( cpa );
-				imp.updateAndDraw();
+				//final ColorProcessor cpa = new ColorProcessor( screenImage.image() );
+				//imp.setProcessor( cpa );
+				//imp.updateAndDraw();
+				imp.setImage( screenImage.image() );
 			}
 			IJ.log( "loop " + ( k + 1 ) + ": " + timer.stop() );
 		}
