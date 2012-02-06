e6cd88316fdeef28589f6f8e308900e9ac15f3cb
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2012-02-06 19:24:57 +0100

    bug fix. thanks to Christian Dietz for noticing.

--- imglib2/core/src/main/java/net/imglib2/subimg/SubImg.java
+++ imglib2/core/src/main/java/net/imglib2/subimg/SubImg.java
@@ -11,7 +11,7 @@ import net.imglib2.view.Views;
 
 public class SubImg< T extends Type< T > > extends IterableRandomAccessibleInterval< T > implements Img< T >
 {
-	private static final < T extends Type< T > > RandomAccessibleInterval< T > getView( Img< T > srcImg, Interval interval, boolean keepDimsWithSizeOne )
+	private static final < T extends Type< T > > RandomAccessibleInterval< T > getView( final Img< T > srcImg, final Interval interval, final boolean keepDimsWithSizeOne )
 	{
 		if ( !Util.contains( srcImg, interval ) )
 			throw new IllegalArgumentException( "In SubImgs the interval min and max must be inside the dimensions of the SrcImg" );
@@ -21,9 +21,9 @@ public class SubImg< T extends Type< T > > extends IterableRandomAccessibleInter
 		interval.min( min );
 		RandomAccessibleInterval< T > slice = Views.translate( srcImg, min );
 		if ( !keepDimsWithSizeOne )
-			for ( int d = 0; d < n; ++d )
+			for ( int d = n - 1; d >= 0; --d )
 				if ( interval.dimension( d ) == 1 )
-					slice = Views.hyperSlice( slice, d, interval.min( d ) );
+					slice = Views.hyperSlice( slice, d, 0 );
 		return slice;
 	}
 
@@ -31,7 +31,7 @@ public class SubImg< T extends Type< T > > extends IterableRandomAccessibleInter
 
 	private final long[] m_origin;
 
-	public SubImg( Img< T > srcImg, Interval interval, boolean keepDimsWithSizeOne )
+	public SubImg( final Img< T > srcImg, final Interval interval, final boolean keepDimsWithSizeOne )
 	{
 		super( getView( srcImg, interval, keepDimsWithSizeOne ) );
 		m_srcImg = srcImg;
@@ -39,13 +39,13 @@ public class SubImg< T extends Type< T > > extends IterableRandomAccessibleInter
 		interval.min( m_origin );
 	}
 
-	public final void getOrigin( long[] origin )
+	public final void getOrigin( final long[] origin )
 	{
 		for ( int d = 0; d < origin.length; d++ )
 			origin[ d ] = m_origin[ d ];
 	}
 
-	public final long getOrigin( int d )
+	public final long getOrigin( final int d )
 	{
 		return m_origin[ d ];
 	}
@@ -57,7 +57,7 @@ public class SubImg< T extends Type< T > > extends IterableRandomAccessibleInter
 		return m_srcImg;
 	}
 
-	
+
 	// Img implementation
 	@Override
 	public ImgFactory< T > factory()
@@ -68,7 +68,7 @@ public class SubImg< T extends Type< T > > extends IterableRandomAccessibleInter
 	@Override
 	public Img< T > copy()
 	{
-		Img< T > copy = m_srcImg.factory().create( this, m_srcImg.firstElement() );
+		final Img< T > copy = m_srcImg.factory().create( this, m_srcImg.firstElement() );
 		// TODO: copy...
 		return copy;
 	}
