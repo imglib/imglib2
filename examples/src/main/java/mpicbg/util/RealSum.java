f0594d58709f2de8d74cb22b2061077e2edf2c5e
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-02-07 15:51:44 -0600

    Add a stupid reimplementation of RealSum
    Now the examples compile.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ examples/src/main/java/mpicbg/util/RealSum.java
@@ -0,0 +1,22 @@
+/* BSD-licensed reimplementation */
+package mpicbg.util;
+
+/**
+ * A simple accumulator of double values.
+ *
+ * The original, GPL-licensed version took great pains of trying to maintain
+ * numerical stability. Since this reimplementation is simply supporting the
+ * ImgLib2 examples, we do not do that here.
+ *
+ * @author Johannes Schindelin
+ */
+public class RealSum { protected double sum;
+
+	public void add(double value) {
+		sum += value;
+	}
+
+	public double getSum() {
+		return sum;
+	}
+}
