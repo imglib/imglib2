2431c096a1a9ec356c4c72476e83258657984756
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-02-07 19:54:41 -0500

    Converter example1c

--- examples/src/main/java/net/imglib2/examples/Example1c.java
+++ examples/src/main/java/net/imglib2/examples/Example1c.java
@@ -1,7 +1,10 @@
 package net.imglib2.examples;
 
 import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.real.FloatType;
 import ij.ImageJ;
 
 /**
@@ -14,18 +17,14 @@ public class Example1c
 {
 	public Example1c()
 	{
-		// create the Factory that will instantiate the Image
-		final ImageFactory<FloatType> imageFactory = new ImageFactory<FloatType>( new FloatType(), new CellContainerFactory() );
+		// create the ImgFactory based on cells (cellsize = 5x5x5...x5) that will instantiate the Img
+		final ImgFactory< FloatType > imgFactory = new CellImgFactory< FloatType >( 5 );
 
-		// create an Image
-		final Img<FloatType> image1 = imageFactory.createImage( new int[] { 20, 30, 40 } );
-
-		// create another Image with exactly the same properties
-		final Img<FloatType> image2 = image1.createNewImage();
+		// create an 3d-Img with dimensions 20x30x40 (here cellsize is 5x5x5)Ø
+		final Img< FloatType > image1 = imgFactory.create( new long[] { 20, 30, 40 }, new FloatType() );
 
 		// display both (but they are empty)
 		ImageJFunctions.show( image1 ).show();
-		ImageJFunctions.show( image2 ).show();
 	}
 
 	public static void main( String[] args )
