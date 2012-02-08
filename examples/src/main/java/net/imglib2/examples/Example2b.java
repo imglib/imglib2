78d6a38ac73acd90c3064434f34c27ab243e2042
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-02-07 20:01:26 -0500

    Fixed example2b

--- examples/src/main/java/net/imglib2/examples/Example2b.java
+++ examples/src/main/java/net/imglib2/examples/Example2b.java
@@ -2,18 +2,22 @@ package net.imglib2.examples;
 
 import java.io.File;
 
+import net.imglib2.Cursor;
 import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
 import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.io.ImgIOException;
 import net.imglib2.io.ImgOpener;
+import net.imglib2.type.Type;
 import net.imglib2.type.numeric.real.FloatType;
 
 import ij.ImageJ;
-import ij.ImagePlus;
 
 /**
  * Here we want to copy an Image into another with a different Container one using a generic method,
- * but we cannot do it with simple Cursors
+ * but we cannot do it with simple Cursors as we use different {@link ImgFactory}s
  *
  * @author Stephan Preibisch &amp; Stephan Saalfeld
  *
@@ -21,32 +25,30 @@ import ij.ImagePlus;
 public class Example2b
 {
 
-	public Example2b()
+	public Example2b() throws ImgIOException
 	{
 		// define the file to open
 		File file = new File( "DrosophilaWing.tif" );
 
-		// open with ImgOpener using an ArrayContainer
-		Img<FloatType> image = new ImgOpener().openImg( file.getAbsolutePath(), new ArrayImgFactory<FloatType>() );
+		// open with ImgOpener using an ArrayImgFactory as FloatType
+		Img< FloatType > image = new ImgOpener().openImg( file.getAbsolutePath(), new ArrayImgFactory<FloatType>(), new FloatType() );
 
 		// copy the image
-		Img<FloatType> duplicate = copyImage( image, new CellContainerFactory( 20 ) );
+		Img<FloatType> duplicate = copyImage( image, new CellImgFactory< FloatType >( 20 ) );
 
 		// display the copy
-		final ImagePlus imp = ImageJFunctions.show( duplicate );
-		imp.resetDisplayRange();
-		imp.show();
+		ImageJFunctions.show( duplicate );
 	}
 
-	public <T extends Type<T>> Img<T> copyImage( final Img<T> input, final ContainerFactory containerFactory )
+	public <T extends Type<T>> Img<T> copyImage( final Img<T> input, final ImgFactory<T> imgFactory )
 	{
-		// create a new Image with the same dimensions
-		ImageFactory<T> imageFactory = new ImageFactory<T>( input.createType(), containerFactory );
-		Img<T> output = imageFactory.createImage( input.getDimensions(), "Copy of " + input.getName() );
+		// create a new Image with the same dimensions but the other imgFactory
+		// note that the input provides the size for the new image as it implements the Interval interface
+		Img<T> output = imgFactory.create( input, input.firstElement() );
 
 		// create a cursor for both images
-		Cursor<T> cursorInput = input.createCursor();
-		Cursor<T> cursorOutput = output.createCursor();
+		Cursor<T> cursorInput = input.cursor();
+		Cursor<T> cursorOutput = output.cursor();
 
 		// iterate over the input cursor
 		while ( cursorInput.hasNext() )
@@ -56,12 +58,8 @@ public class Example2b
 			cursorOutput.fwd();
 
 			// set the value of this pixel of the output image, every Type supports T.set( T type )
-			cursorOutput.getType().set( cursorInput.getType() );
+			cursorOutput.get().set( cursorInput.get() );
 		}
-
-		// close the cursors
-		cursorInput.close();
-		cursorOutput.close();
 
 		//. return the copy
 		return output;
@@ -73,6 +71,14 @@ public class Example2b
 		new ImageJ();
 
 		// run the example
-		new Example2b();
+		try
+		{
+			new Example2b();
+		}
+		catch (ImgIOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
