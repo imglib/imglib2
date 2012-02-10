da2997be73e978e31db383350eccce1838cd5f0a
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-02-10 18:55:27 -0500

    Added a direct implementation of the summing of each line for long and double basic type. Gain is around 10%, easy to implement as only the 1d summing needs to be implemented.

---
+++ imglib1/algorithms/src/main/java/mpicbg/imglib/algorithm/integral/IntegralImageDouble.java
@@ -0,0 +1,59 @@
+package mpicbg.imglib.algorithm.integral;
+
+import mpicbg.imglib.cursor.LocalizableByDimCursor;
+import mpicbg.imglib.function.Converter;
+import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.numeric.NumericType;
+import mpicbg.imglib.type.numeric.real.DoubleType;
+
+/**
+ * Special implementation for double using the basic type to sum up the individual lines. 
+ * 
+ * @author Stephan Preibisch
+ *
+ * @param <R>
+ */
+public class IntegralImageDouble< R extends NumericType< R > > extends IntegralImage2< R, DoubleType >
+{
+
+	public IntegralImageDouble( final Image<R> img, final DoubleType type, final Converter<R, DoubleType> converter) 
+	{
+		super( img, type, converter );
+	}
+
+	@Override
+	protected void integrateLineDim0( final Converter< R, DoubleType > converter, final LocalizableByDimCursor< R > cursorIn, final LocalizableByDimCursor< DoubleType > cursorOut, final DoubleType sum, final DoubleType tmpVar, final int size )
+	{
+		// compute the first pixel
+		converter.convert( cursorIn.getType(), sum );
+		cursorOut.getType().set( sum );
+		
+		double sum2 = sum.get();
+
+		for ( int i = 2; i < size; ++i )
+		{
+			cursorIn.fwd( 0 );
+			cursorOut.fwd( 0 );
+
+			converter.convert( cursorIn.getType(), tmpVar );
+			sum2 += tmpVar.get();
+			cursorOut.getType().set( sum2 );
+		}		
+	}
+
+	@Override
+	protected void integrateLine( final int d, final LocalizableByDimCursor< DoubleType > cursor, final DoubleType sum, final int size )
+	{
+		// init sum on first pixel that is not zero
+		double sum2 = cursor.getType().get();
+
+		for ( int i = 2; i < size; ++i )
+		{
+			cursor.fwd( d );
+
+			sum2 += cursor.getType().get();
+			cursor.getType().set( sum2 );
+		}
+	}
+	
+}
---
+++ imglib1/algorithms/src/main/java/mpicbg/imglib/algorithm/integral/IntegralImageLong.java
@@ -0,0 +1,58 @@
+package mpicbg.imglib.algorithm.integral;
+
+import mpicbg.imglib.cursor.LocalizableByDimCursor;
+import mpicbg.imglib.function.Converter;
+import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.numeric.NumericType;
+import mpicbg.imglib.type.numeric.integer.LongType;
+
+/**
+ * Special implementation for long using the basic type to sum up the individual lines. 
+ * 
+ * @author Stephan Preibisch
+ *
+ * @param <R>
+ */
+public class IntegralImageLong< R extends NumericType< R > > extends IntegralImage2< R, LongType >
+{
+
+	public IntegralImageLong( final Image<R> img, final LongType type, final Converter<R, LongType> converter) 
+	{
+		super( img, type, converter );
+	}
+	
+	@Override
+	protected void integrateLineDim0( final Converter< R, LongType > converter, final LocalizableByDimCursor< R > cursorIn, final LocalizableByDimCursor< LongType > cursorOut, final LongType sum, final LongType tmpVar, final int size )
+	{
+		// compute the first pixel
+		converter.convert( cursorIn.getType(), sum );
+		cursorOut.getType().set( sum );
+		
+		long sum2 = sum.get();
+
+		for ( int i = 2; i < size; ++i )
+		{
+			cursorIn.fwd( 0 );
+			cursorOut.fwd( 0 );
+
+			converter.convert( cursorIn.getType(), tmpVar );
+			sum2 += tmpVar.get();
+			cursorOut.getType().set( sum2 );
+		}		
+	}
+
+	@Override
+	protected void integrateLine( final int d, final LocalizableByDimCursor< LongType > cursor, final LongType sum, final int size )
+	{
+		// init sum on first pixel that is not zero
+		long sum2 = cursor.getType().get();
+
+		for ( int i = 2; i < size; ++i )
+		{
+			cursor.fwd( d );
+
+			sum2 += cursor.getType().get();
+			cursor.getType().set( sum2 );
+		}
+	}
+}
