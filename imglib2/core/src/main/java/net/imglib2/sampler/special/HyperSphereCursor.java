debd809792402925b017bd3a93f46bca0342e8c0
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-03-05 18:25:49 -0500

    HyperspherCursor does not need Type, but only T as generic parameter Added the license

--- imglib2/core/src/main/java/net/imglib2/sampler/special/HyperSphereCursor.java
+++ imglib2/core/src/main/java/net/imglib2/sampler/special/HyperSphereCursor.java
@@ -1,3 +1,30 @@
+/**
+ * Copyright (c) 2011, Stephan Preibisch & Stephan Saalfeld
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.  Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.  Neither the name of the imglib project nor
+ * the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.imglib2.sampler.special;
 
 import net.imglib2.Cursor;
@@ -5,7 +32,6 @@ import net.imglib2.Localizable;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.Sampler;
-import net.imglib2.type.Type;
 
 /**
  * Iterate over all pixels in an n-dimensional sphere.
@@ -15,7 +41,7 @@ import net.imglib2.type.Type;
  * @param <T>
  */
 
-public class HyperSphereCursor< T extends Type< T > > implements Cursor< T >
+public class HyperSphereCursor< T > implements Cursor< T >
 {
 	final RandomAccessible< T > source;
 	final protected Localizable center;
