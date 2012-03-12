a18e5e806e8ab4c2e878d729482be6d7bf9a3cad
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-03-12 15:09:47 -0400

    Small addition so that 1-bit and 12-bit are displayed correctly

--- imglib2/ij/src/main/java/net/imglib2/img/display/imagej/ImageJVirtualStackUnsignedShort.java
+++ imglib2/ij/src/main/java/net/imglib2/img/display/imagej/ImageJVirtualStackUnsignedShort.java
@@ -3,13 +3,26 @@ package net.imglib2.img.display.imagej;
 import ij.ImagePlus;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.converter.Converter;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.integer.Unsigned12BitType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.util.Util;
 
 public class ImageJVirtualStackUnsignedShort< S > extends ImageJVirtualStack< S, UnsignedShortType >
 {
 	public ImageJVirtualStackUnsignedShort( RandomAccessibleInterval< S > source, Converter< S, UnsignedShortType > converter )
 	{
 		super( source, converter, new UnsignedShortType(), ImagePlus.GRAY16 );
-		imageProcessor.setMinAndMax( 0, (1 << 16) - 1 );
+
+		int maxDisplay = (1 << 16) - 1;
+		
+		final S s = Util.getTypeFromInterval( source );
+		
+		if ( BitType.class.isInstance( s ) )
+			maxDisplay = 1;
+		else if ( Unsigned12BitType.class.isInstance( s ) )
+			maxDisplay = 4095;
+		
+		imageProcessor.setMinAndMax( 0, maxDisplay );
 	}
 }
