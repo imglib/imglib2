6f0c8762c27ccdd9fbdd4919c2ac728f65f81563
Christian Dietz <christian.dietz@uni-konstanz.de>
2012-03-19 21:19:58 +0100

    Fixed two bugs in BinaryOperationAssigment and BinaryRelationAssigment

--- imglib2/ops/src/main/java/net/imglib2/ops/image/BinaryRelationAssigment.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/image/BinaryRelationAssigment.java
@@ -42,45 +42,48 @@ import net.imglib2.type.numeric.RealType;
  * @author Christian Dietz
  * 
  */
-public class BinaryRelationAssigment< T extends RealType< T >, V extends RealType< V >> implements BinaryOperation< IterableInterval< T >, IterableInterval< V >, IterableInterval< BitType >>
-{
+public class BinaryRelationAssigment<T extends RealType<T>, V extends RealType<V>>
+		implements
+		BinaryOperation<IterableInterval<T>, IterableInterval<V>, IterableInterval<BitType>> {
 
-	private BinaryRelation< T, V > m_rel;
+	private BinaryRelation<T, V> m_rel;
 
-	private ImgFactory< BitType > m_fac;
+	private ImgFactory<BitType> m_fac;
 
-	public BinaryRelationAssigment( ImgFactory< BitType > fac, BinaryRelation< T, V > rel )
-	{
+	public BinaryRelationAssigment(ImgFactory<BitType> fac,
+			BinaryRelation<T, V> rel) {
 		m_rel = rel;
 		m_fac = fac;
 	}
 
 	@Override
-	public IterableInterval< BitType > compute( IterableInterval< T > input1, IterableInterval< V > input2, IterableInterval< BitType > output )
-	{
-		if ( !IterationOrderUtil.equalIterationOrder( input1, input2 ) || !IterationOrderUtil.equalInterval( input1, input2 ) ) { throw new IllegalArgumentException( "Intervals are not compatible" ); }
+	public IterableInterval<BitType> compute(IterableInterval<T> input1,
+			IterableInterval<V> input2, IterableInterval<BitType> output) {
+		if (!IterationOrderUtil.equalIterationOrder(input1, input2)
+				|| !IterationOrderUtil.equalInterval(input1, input2)
+				|| !IterationOrderUtil.equalIterationOrder(input1, output)) {
+			throw new IllegalArgumentException("Intervals are not compatible");
+		}
 
-		Cursor< T > inCursor1 = input1.cursor();
-		Cursor< V > inCursor2 = input2.cursor();
+		Cursor<T> inCursor1 = input1.cursor();
+		Cursor<V> inCursor2 = input2.cursor();
 
-		Cursor< BitType > outCursor = output.cursor();
+		Cursor<BitType> outCursor = output.cursor();
 
-		while ( outCursor.hasNext() )
-		{
+		while (outCursor.hasNext()) {
 			outCursor.fwd();
 			inCursor1.fwd();
 			inCursor2.fwd();
 
-			outCursor.get().set( m_rel.holds( inCursor1.get(), inCursor2.get() ) );
+			outCursor.get().set(m_rel.holds(inCursor1.get(), inCursor2.get()));
 		}
 		return output;
 
 	}
 
 	@Override
-	public BinaryOperation< IterableInterval< T >, IterableInterval< V >, IterableInterval< BitType >> copy()
-	{
-		return new BinaryRelationAssigment< T, V >( m_fac, m_rel.copy() );
+	public BinaryOperation<IterableInterval<T>, IterableInterval<V>, IterableInterval<BitType>> copy() {
+		return new BinaryRelationAssigment<T, V>(m_fac, m_rel.copy());
 	}
 
 }
