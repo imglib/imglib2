11c059310ee94cc0b324727ba363819700ebcc9f
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2012-03-21 20:13:37 +0100

    made AbstractRealInterval extend EuclideanSpace.

--- imglib2/core/src/main/java/net/imglib2/AbstractRealInterval.java
+++ imglib2/core/src/main/java/net/imglib2/AbstractRealInterval.java
@@ -2,26 +2,25 @@ package net.imglib2;
 
 /**
  * Implementation of the {@link RealInterval} interface.
- * 
+ *
  * @author Stephan Preibisch
  */
-public class AbstractRealInterval implements RealInterval
+public class AbstractRealInterval extends AbstractEuclideanSpace  implements RealInterval
 {
-	final protected int n;
 	final protected double[] min;
 	final protected double[] max;
 
 	/**
 	 * Creates a new {@link AbstractRealInterval} using an existing {@link RealInterval}
-	 * 
+	 *
 	 * @param interval
 	 */
 	public AbstractRealInterval( final RealInterval interval )
 	{
-		this.n = interval.numDimensions();
+		super( interval.numDimensions() );
 		this.min = new double[ n ];
 		this.max = new double[ n ];
-		
+
 		for ( int d = 0; d < n; ++d )
 		{
 			this.min[ d ] = min[ d ];
@@ -31,47 +30,44 @@ public class AbstractRealInterval implements RealInterval
 
 	/**
 	 * Creates a new {@link AbstractRealInterval} from min and max coordinates
-	 * 
+	 *
 	 * @param min
 	 * @param max
 	 */
 	public AbstractRealInterval( final double[] min, final double[] max )
 	{
-		this.n = min.length;
+		super( min.length );
 		this.min = min.clone();
 		this.max = max.clone();
 	}
-	
+
 	/**
 	 * Creates a new zero-bounded {@link AbstractRealInterval} with a certain size
-	 * 
+	 *
 	 * @param dimensions
 	 */
 	public AbstractRealInterval( final double[] dimensions )
 	{
-		this.n = dimensions.length;
+		super( dimensions.length );
 		this.min = new double[ n ];
 		this.max = new double[ n ];
-		
+
 		for ( int d = 0; d < n; ++d )
 			this.max[ d ] = dimensions[ d ] - 1;
-	}	
-
-	@Override
-	public int numDimensions() { return n; }
+	}
 
 	@Override
 	public double realMin( final int d) { return min[ d ]; }
 
 	@Override
-	public void realMin( final double[] min ) 
+	public void realMin( final double[] min )
 	{
 		for ( int d = 0; d < n; ++d )
 			min[ d ] = this.min[ d ];
 	}
 
 	@Override
-	public void realMin( final RealPositionable min ) 
+	public void realMin( final RealPositionable min )
 	{
 		for ( int d = 0; d < n; ++d )
 			min.setPosition( this.min[ d ], d );
