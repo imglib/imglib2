4e972082c62ae372c8dad13dff7663bdcdfc4feb
Lee Kamentsky <leek@broadinstitute.org>
2012-04-11 14:56:51 -0400

    Fixed ellipse ROI bug reported by Christian Dietz + other esoteric corner cases

--- imglib2/core/src/main/java/net/imglib2/roi/EllipseRegionOfInterest.java
+++ imglib2/core/src/main/java/net/imglib2/roi/EllipseRegionOfInterest.java
@@ -263,18 +263,36 @@ public class EllipseRegionOfInterest extends AbstractIterableRegionOfInterest {
 				d0 = getRasterDisplacement(position, i);
 				position[i] = (long)Math.ceil(origin.getDoublePosition(i)-d0);
 			}
-			System.arraycopy(position,1,end,1,numDimensions()-1);
-			end[0] = (long)Math.floor(origin.getDoublePosition(0)+d0) + 1;
-			return true;
+			if (isMember(position)) {
+				System.arraycopy(position,1,end,1,numDimensions()-1);
+				end[0] = (long)Math.floor(origin.getDoublePosition(0)+d0) + 1;
+				return true;
+			}
 		}
-		dimensionloop: for (int i=1; i < numDimensions(); i++) {
+		for (int i=1; i < numDimensions(); i++) {
 			/*
 			 * Advance the position until we get a position
 			 * within the ellipse.
 			 */
 			position[i]++;
 			final double partialDisplacement = getPartialDisplacement(position, i);
-			if (partialDisplacement < 1) {
+			if (partialDisplacement <= 1) {
+				/*
+				 * Check that we can find a point within the ellipse. It may be the case
+				 * that, for dimension # 0,  both the pixel at the floor of the origin 
+				 * and at the ceiling of the origin are outside of the ellipse even 
+				 * though the origin itself is within the ellipse.
+				 */
+				double d = 0;
+				for (int j = i; j < numDimensions(); j++) {
+					double diff = (position[j] - origin.getDoublePosition(j)) / radii[j]; 
+					d += diff * diff;
+				}
+				for (int j = 0; j < i; j++) {
+					double diff = (origin.getDoublePosition(j) - Math.round(origin.getDoublePosition(j))) / radii[j];
+					d += diff * diff;
+				}
+				if (d > 1) continue;
 				/*
 				 * Adjust the lesser positions to the start of
 				 * the ellipse.
@@ -287,19 +305,6 @@ public class EllipseRegionOfInterest extends AbstractIterableRegionOfInterest {
 					} else {
 						end[j] = position[j];
 					}
-				}
-				return true;
-			} else if (partialDisplacement == 1) {
-				/*
-				 * All remaining must be at the origin position
-				 */
-				for (int j=i-1; j >= 0; j--) {
-					double originPosition = origin.getDoublePosition(j);
-					if (Math.floor(originPosition) != originPosition) continue dimensionloop;
-				}
-				for (int j=i-1; j >= 0; j--) {
-					position[j] = (long) origin.getDoublePosition(j);
-					end[j] = (j > 0)? position[j] : (position[j] + 1);
 				}
 				return true;
 			}
@@ -342,6 +347,21 @@ public class EllipseRegionOfInterest extends AbstractIterableRegionOfInterest {
 	 */
 	@Override
 	protected boolean isMember(double[] position) {
+		double accumulator = 0;
+		for (int i=0; i<numDimensions(); i++) {
+			double diff = ((position[i] - origin.getDoublePosition(i)) / radii[i]);
+			accumulator += diff * diff;
+		}
+		return accumulator <= 1;
+	}
+	
+	/**
+	 * Test to see if an integer position is inside the ellipse.
+	 * 
+	 * @param position
+	 * @return
+	 */
+	public boolean isMember(long [] position) {
 		double accumulator = 0;
 		for (int i=0; i<numDimensions(); i++) {
 			double diff = ((position[i] - origin.getDoublePosition(i)) / radii[i]);
--- imglib2/core/src/test/java/net/imglib2/roi/EllipseRegionOfInterestTest.java
+++ imglib2/core/src/test/java/net/imglib2/roi/EllipseRegionOfInterestTest.java
@@ -382,40 +382,8 @@ public class EllipseRegionOfInterestTest {
         EllipseRegionOfInterest ellipse = new EllipseRegionOfInterest( 2 );
         ellipse.setOrigin( new double[] { 1, 1 } );
         ellipse.setRadius( 1 );
+        testEllipse(ellipse);
 
-        Img< BitType > img = new ArrayImgFactory< BitType >().create( new long[] { 3, 3 }, new BitType() );
-
-        RandomAccess< BitType > randomAccess = img.randomAccess();
-
-        Cursor< BitType > cursor = img.cursor();
-        long[] pos = new long[ 2 ];
-        int count = 0;
-        while (cursor.hasNext()) {
-            cursor.fwd();
-            cursor.localize( pos );
-            double d2 = 0;
-            for (int i=0; i < pos.length; i++) d2 += Math.pow(pos[i] - 1, 2);
-            if (d2 <= 1.0) {
-            	cursor.get().set(true);
-            	count++;
-            }
-        }
-        
-        cursor = ellipse.getIterableIntervalOverROI( img ).cursor();
-        while ( cursor.hasNext() )
-        {
-            cursor.fwd();
-            cursor.localize( pos );
-            randomAccess.setPosition( cursor );
-            assertTrue(
-            		String.format("Point %d, %d not in set or doubly visited", pos[0], pos[1]),
-            		randomAccess.get().get());
-            randomAccess.get().set(false);
-            count--;
-        }
-        assertEquals(
-        		String.format("%d point(s) unvisited", count),
-        		0, count);
 	}
 	/**
 	 * Second part of Christian Dietz bug - make sure it works for a sphere
@@ -426,19 +394,51 @@ public class EllipseRegionOfInterestTest {
         EllipseRegionOfInterest ellipse = new EllipseRegionOfInterest( 3 );
         ellipse.setOrigin( new double[] { 1, 1, 1 } );
         ellipse.setRadius( 1 );
+        testEllipse(ellipse);
 
-        Img< BitType > img = new ArrayImgFactory< BitType >().create( new long[] { 3, 3, 3 }, new BitType() );
+	}
+	
+	@Test
+	public void testSphereWithNonIntegerOffset() {
+		// This is a corner case. If all dimensions but one
+		// are within the ellipse, but the last one is,
+		// because it's an integer, above or below (= outside)
+		// then you need to skip.
+        EllipseRegionOfInterest ellipse = new EllipseRegionOfInterest( 3 );
+        ellipse.setOrigin( new double[] { 1, 1, 1.1 } );
+        ellipse.setRadius( 1.05 );
+        testEllipse(ellipse);
+        
+        ellipse = new EllipseRegionOfInterest( 3 );
+        ellipse.setOrigin( new double[] { 1.1, 1, 1 } );
+        ellipse.setRadius( 1.05 );
+        testEllipse(ellipse);        
+	}
+
+	private void testEllipse(EllipseRegionOfInterest ellipse) {
+		final long [] eMin = new long[ellipse.numDimensions()];
+		final long [] eMax = new long[ellipse.numDimensions()];
+		ellipse.min(eMin);
+		for (int i=0; i < eMin.length; i++) {
+			assertTrue("Test code won't work if ellipse extends to negative coordinates", eMin[i] >= 0);
+		}
+		ellipse.max(eMax);
+		for (int i=0; i < eMax.length; i++) eMax[i]++;
+		
+		Img< BitType > img = new ArrayImgFactory< BitType >().create( eMax, new BitType() );
 
         RandomAccess< BitType > randomAccess = img.randomAccess();
 
         Cursor< BitType > cursor = img.cursor();
-        long[] pos = new long[ 3 ];
+        long[] pos = new long[ ellipse.numDimensions() ];
         int count = 0;
         while (cursor.hasNext()) {
             cursor.fwd();
             cursor.localize( pos );
             double d2 = 0;
-            for (int i=0; i < pos.length; i++) d2 += Math.pow(pos[i] - 1, 2);
+            for (int i=0; i < pos.length; i++) {
+            	d2 += Math.pow((pos[i] - ellipse.getOrigin(i)) / ellipse.getRadius(i), 2);
+            }
             if (d2 <= 1.0) {
             	cursor.get().set(true);
             	count++;
@@ -452,7 +452,7 @@ public class EllipseRegionOfInterestTest {
             cursor.localize( pos );
             randomAccess.setPosition( cursor );
             assertTrue(
-            		String.format("Point %d, %d, %d not in set or doubly visited", pos[0], pos[1], pos[2]),
+            		String.format("Point %s not in set or doubly visited", pos.toString()),
             		randomAccess.get().get());
             randomAccess.get().set(false);
             count--;
