fdca5c599feb03c3af42745696c444dbee347edf
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2012-04-18 15:10:01 +0200

    Modified and commented BenchmarkHelper.
    BenchmarkHelper provides utilities for seeting up small benchmarks.
    Call BenchmarkHelper.benchmark(int, Benchmark) with the number of
    iterations and a Runnable to benchmark to obtain a list of run-times
    in milliseconds. Use BenchmarkHelper.median() to compute the median
    run-time from the result.

--- imglib2/core/src/test/java/net/imglib2/img/cell/CellRandomAccessBenchmark.java
+++ imglib2/core/src/test/java/net/imglib2/img/cell/CellRandomAccessBenchmark.java
@@ -9,13 +9,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,7 +27,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of any organization.
@@ -123,7 +123,7 @@ public class CellRandomAccessBenchmark
 		randomAccessBenchmark.createSourceData();
 
 		System.out.println( "benchmarking fill" );
-		BenchmarkHelper.benchmark( 20, false, new BenchmarkHelper.Benchmark()
+		BenchmarkHelper.benchmarkAndPrint( 20, false, new Runnable()
 		{
 			@Override
 			public void run()
@@ -135,7 +135,7 @@ public class CellRandomAccessBenchmark
 
 		randomAccessBenchmark.intImgCopy = new CellImgFactory< IntType >( 32 ).create( randomAccessBenchmark.dimensions, new IntType() );
 		System.out.println( "benchmarking copy to smaller" );
-		BenchmarkHelper.benchmark( 20, false, new BenchmarkHelper.Benchmark()
+		BenchmarkHelper.benchmarkAndPrint( 20, false, new Runnable()
 		{
 			@Override
 			public void run()
@@ -147,7 +147,7 @@ public class CellRandomAccessBenchmark
 
 		randomAccessBenchmark.intImgCopy = new CellImgFactory< IntType >( 50 ).create( randomAccessBenchmark.dimensions, new IntType() );
 		System.out.println( "benchmarking copy to larger" );
-		BenchmarkHelper.benchmark( 20, false, new BenchmarkHelper.Benchmark()
+		BenchmarkHelper.benchmarkAndPrint( 20, false, new Runnable()
 		{
 			@Override
 			public void run()
@@ -159,7 +159,7 @@ public class CellRandomAccessBenchmark
 
 		randomAccessBenchmark.intImgCopy = new CellImgFactory< IntType >( new int[] {32, 64, 16} ).create( randomAccessBenchmark.dimensions, new IntType() );
 		System.out.println( "benchmarking copy to mixed" );
-		BenchmarkHelper.benchmark( 20, false, new BenchmarkHelper.Benchmark()
+		BenchmarkHelper.benchmarkAndPrint( 20, false, new Runnable()
 		{
 			@Override
 			public void run()
