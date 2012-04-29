36c564407e9f15054e6381e987485a8fdf784d34
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2012-04-29 23:13:48 +0200

    clean up Mandelbrot example

--- imglib2/tests/src/test/java/examples/Real2DViewerExample.java
+++ imglib2/tests/src/test/java/examples/Real2DViewerExample.java
@@ -1,10 +1,6 @@
 package examples;
-import fractals.MandelbrotRealRandomAccessible;
-import gui.InteractiveReal2DViewer;
-
 import java.util.ArrayList;
 
-import net.imglib2.RealRandomAccessible;
 import net.imglib2.converter.Converter;
 import net.imglib2.io.ImgIOException;
 import net.imglib2.realtransform.AffineTransform2D;
@@ -12,42 +8,25 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.numeric.integer.LongType;
+import fractals.MandelbrotRealRandomAccessible;
+import gui.InteractiveReal2DViewer;
 
 public class Real2DViewerExample< T extends NumericType< T > & NativeType< T > >
 {
-	private final int width = 800;
-	private final int height = 600;
-
-	public Real2DViewerExample( final RealRandomAccessible< T > source, final Converter< T, ARGBType > converter )
-	{
-		/* center shift */
-		final AffineTransform2D centerShift = new AffineTransform2D();
-		centerShift.set(
-				1, 0, -width / 2.0,
-				0, 1, -height / 2.0 );
-
-		/* center un-shift */
-		final AffineTransform2D centerUnShift = new AffineTransform2D();
-		centerUnShift.set(
-				1, 0, width / 2.0,
-				0, 1, height / 2.0 );
-
-		/* initialize rotation */
-		final AffineTransform2D rotation = new AffineTransform2D();
-		rotation.scale( 200 );
-
-		rotation.preConcatenate( centerUnShift );
-		
-		new InteractiveReal2DViewer< T >( width, height, source, converter, rotation, new ArrayList< Object >() );
-	}
-
 	final static public void main( final String[] args ) throws ImgIOException
 	{
+		final int width = 800;
+		final int height = 600;
+
 		final int maxIterations = 100;
-		
+		final MandelbrotRealRandomAccessible mandelbrot = new MandelbrotRealRandomAccessible( maxIterations );
+
+		final AffineTransform2D transform = new AffineTransform2D();
+		transform.scale( 200 );
+		transform.translate( width / 2.0, height / 2.0 );
+
 		final Converter< LongType, ARGBType > lut = new Converter< LongType, ARGBType >()
 		{
-			
 			final protected int[] rgb = new int[ maxIterations + 1 ];
 			{
 				for ( int i = 0; i <= maxIterations; ++i )
@@ -55,25 +34,23 @@ public class Real2DViewerExample< T extends NumericType< T > & NativeType< T > >
 					final double r = 1.0 - ( double )i / maxIterations;
 					final double g = Math.sin( Math.PI * r );
 					final double b = 0.5 - 0.5 * Math.cos( Math.PI * g );
-					
+
 					final int ri = ( int )Math.round( Math.max( 0, 255 * r ) );
 					final int gi = ( int )Math.round( Math.max( 0, 255 * g ) );
 					final int bi = ( int )Math.round( Math.max( 0, 255 * b ) );
-					
+
 					rgb[ i ] = ( ( ( ri << 8 ) | gi ) << 8 ) | bi | 0xff000000;
 				}
 			}
-			
+
 			@Override
 			public void convert( final LongType input, final ARGBType output )
 			{
 				output.set( rgb[ input.getInteger() ] );
 			}
 		};
-		
-		final MandelbrotRealRandomAccessible mandelbrot = new MandelbrotRealRandomAccessible( maxIterations );
 
-		new Real2DViewerExample< LongType >( mandelbrot, lut );
+		new InteractiveReal2DViewer< LongType >( width, height, mandelbrot, lut, transform, new ArrayList< Object >() );
 	}
 
 }
