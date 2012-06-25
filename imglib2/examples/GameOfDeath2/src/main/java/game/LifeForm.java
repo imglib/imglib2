5fff628d60cc0bb9e94cfccad3222bb29a64bf58
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-06-25 11:06:53 -0400

    Commented the LifeForm class in GameOfDeath

--- imglib2/examples/GameOfDeath2/src/main/java/game/LifeForm.java
+++ imglib2/examples/GameOfDeath2/src/main/java/game/LifeForm.java
@@ -36,8 +36,6 @@
 
 package game;
 
-import java.util.Random;
-
 import net.imglib2.img.NativeImg;
 import net.imglib2.img.NativeImgFactory;
 import net.imglib2.img.basictypeaccess.IntAccess;
@@ -46,25 +44,44 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.NumericType;
 
 /**
- * TODO
+ * The LifeForm class stores the current state of a life form, i.e. its name and its weight. Furthermore,
+ * it implements a subset of the NumericType methods (add, mul) that allows to run a Gaussian convolution
+ * on a dataset consisting of life forms. 
+ * To store the name and weight we use an Integer-backed array where we store two int's per pixel. The second
+ * int storing the weight is bit-wise converted to a float. 
+ * 
+ * The implemented math works as follows:
+ * 
+ * add( LifeForm c )
+ * - if 'c' has the same name, their weights will be added
+ * - if 'c' has a different name, the resulting LifeForm is the one with the higher weight, but the weight
+ * from the "deceased" LifeForm is subtracted
+ * 
+ * mul( double c )
+ * - the weight is multiplied by 'c'
+ * 
+ * Running a Gaussian Convolution on such a dataset will simulate the diffusion of each of the LifeForms to
+ * its neighboring area. 
  *
  * @author Stephan Preibisch
  * @author Stephan Saalfeld
  */
 public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, NativeType<LifeForm>
 {
-	final static protected Random rnd = new Random();
-	
+	// the current index of the cursor or randomAccess
 	protected int i = 0;
 	
+	// the index of name and weight in the current IntAccess
 	protected int nameI = 0, weightI = 1;
 
+	// the underlying container holding the data (could be ArrayImg, CellImg, ...)
 	final NativeImg<LifeForm, ? extends IntAccess> storage;
 	
-	// the (sub)DirectAccessContainer that holds the information 
+	// the current array that holds the information, which one (e.g which cell
+	// of a CellImg) it is is defined by the cursor/randomAccess 
 	IntAccess b;
 	
-	// this is the constructor if you want it to read from an array
+	// this is the constructor for initializing with an array
 	public LifeForm( NativeImg<LifeForm, ? extends IntAccess> lifeFormStorage )
 	{
 		storage = lifeFormStorage;
@@ -82,10 +99,12 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 	// this is the constructor if you want it to be a variable
 	public LifeForm() { this( 0, 0 ); }
 
+	// called by the NativeImgFactory to create a new NativeImg using the type of
+	// data the LifeForm requires (in this case an integer array with 2 entries per pixel)
 	@Override
 	public NativeImg<LifeForm, ? extends IntAccess> createSuitableNativeImg( final NativeImgFactory<LifeForm> storageFactory, final long dim[] )
 	{
-		// create the container
+		// create the container (int, 2 values per pixel)
 		final NativeImg<LifeForm, ? extends IntAccess> container = storageFactory.createIntInstance( dim, 2 );
 		
 		// create a Type that is linked to the container
@@ -97,29 +116,56 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 		return container;
 	}
 	
+	// called by the cursor/randomAccess to update the currently active IntAccess
 	@Override
 	public void updateContainer( final Object c ) 
 	{ 
 		b = storage.update( c );		
 	}
 	
+	/**
+	 * Set the name of this LifeForm
+	 * 
+	 * @param name - the new name
+	 */
+	
 	public void setName( final int name ) { b.setValue( nameI, name ); }
+	/**
+	 * Set the weight of this LifeForm
+	 * 
+	 * @param weight - the new weight
+	 */
 	public void setWeight( final float weight ) { b.setValue( weightI, Float.floatToIntBits(weight) ); }
 	
+	/**
+	 * @return - the name of this LifeForm
+	 */
+	
 	public int getName() { return b.getValue( nameI ); }
+	/**
+	 * @return - the weight of this LifeForm
+	 */
 	public float getWeight() { return Float.intBitsToFloat( b.getValue( weightI ) ); }
 	
+	/**
+	 * Set name and weight of this LifeForm
+	 * @param name - the new name
+	 * @param weight - the new weight
+	 */
 	public void set( final int name, final float weight )
 	{
 		setName( name );
 		setWeight( weight );
 	}
 
+	/**
+	 * @param c - the LifeForm to "add" to this one
+	 */
 	@Override
 	public void add( final LifeForm c )
 	{
-		final float a = getWeight();// * ( 0.9f + 0.2f * rnd.nextFloat() );
-		final float b = c.getWeight();// * ( 0.9f + 0.2f * rnd.nextFloat() );
+		final float a = getWeight();
+		final float b = c.getWeight();
 		
 		final int na = getName();
 		final int nb = c.getName();
@@ -135,48 +181,86 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 		}
 	}
 
+	/**
+	 * @param c - multiply weight of this LifeForm by 'c'
+	 */
+	@Override
+	public void mul( final float c ) { setWeight( getWeight() * c );	}
+
+	/**
+	 * @param c - multiply weight of this LifeForm by 'c'
+	 */
+	@Override
+	public void mul( final double c ) { setWeight( (float)( getWeight() * c ) );	}
+
+	/**
+	 * not required/not defined
+	 */
 	@Override
 	public void div( final LifeForm c )
 	{
 		throw new UnsupportedOperationException( "LifeForm.div() is not supported " );
 	}
 
+	/**
+	 * not required/not defined
+	 */
 	@Override
 	public void mul( final LifeForm c )
 	{
 		throw new UnsupportedOperationException( "LifeForm.mul() is not supported " );
 	}
 
+	/**
+	 * not required/not defined
+	 */
 	@Override
 	public void sub( final LifeForm c )
 	{
 		throw new UnsupportedOperationException( "LifeForm.sub() is not supported " );
 	}
 	
-	@Override
-	public void mul( final float c ) { setWeight( getWeight() * c );	}
-
-	@Override
-	public void mul( final double c ) { setWeight( (float)( getWeight() * c ) );	}
-
+	/**
+	 * set weight of this LifeForm to 1
+	 */
 	@Override
 	public void setOne() { setWeight( 1 ); }
 
+	/**
+	 * set weight of this LifeForm to 0
+	 */
 	@Override
 	public void setZero() { setWeight( 0 ); }
 
+	/**
+	 * create a new, uninitialized LifeForm
+	 */
 	@Override
 	public LifeForm createVariable() { return new LifeForm(); }
 
+	/**
+	 * for internal use, create a new LifeForm that works on the defined NativeImg
+	 */
 	@Override
 	public LifeForm duplicateTypeOnSameNativeImg() { return new LifeForm( storage ); }
 
+	/**
+	 * the currnet index in the IntAccess, definied by the cursor/randomAccess 
+	 */
 	@Override
 	public int getIndex() { return i; }
 
+	/**
+	 * Set the values of this LifeForm to the values of LifeForm 'c'
+	 * @param c - new LifeForm
+	 */
 	@Override
 	public void set( final LifeForm c ) { set( c.getName(), c.getWeight() ); }
 
+	/**
+	 * Compares the weights of two LifeForms
+	 * @param c - LifeForm to compare to
+	 */
 	@Override
 	public int compareTo( final LifeForm c )
 	{
@@ -190,9 +274,9 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 			return 0;
 	}
 	
-	@Override
-	public LifeForm clone() { return new LifeForm( getName(), getWeight() ); }
-
+	/**
+	 * @param i - set the index in the IntAccess (called by cursor/randomAccess)
+	 */
 	@Override
 	public void updateIndex( final int i ) 
 	{ 
@@ -201,6 +285,9 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 		weightI = i * 2 + 1;
 	}
 	
+	/**
+	 * increase the index in the IntAccess by one (called by cursor/randomAccess)
+	 */
 	@Override
 	public void incIndex() 
 	{ 
@@ -208,6 +295,11 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 		nameI += 2;
 		weightI += 2;
 	}
+
+	/**
+	 * increase the index in the IntAccess by 'increment' (called by cursor/randomAccess)
+	 * @param increment
+	 */
 	@Override
 	public void incIndex( final int increment ) 
 	{ 
@@ -217,13 +309,22 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 		nameI += inc2;
 		weightI += inc2;
 	}
-	@Override
+
+	/**
+	 * decrease the index in the IntAccess by one (called by cursor/randomAccess)
+	 */
+	@Override	
 	public void decIndex() 
 	{ 
 		--i; 
 		nameI -= 2;
 		weightI -= 2;
 	}
+
+	/**
+	 * decrease the index in the IntAccess by 'increment' (called by cursor/randomAccess)
+	 * @param increment
+	 */
 	@Override
 	public void decIndex( final int decrement ) 
 	{ 
@@ -233,13 +334,30 @@ public class LifeForm implements NumericType<LifeForm>, Comparable<LifeForm>, Na
 		weightI -= dec2;
 	}	
 	
+	/**
+	 * create a String representation of this LifeForm
+	 */
 	@Override
 	public String toString()
 	{
 		return "Race " + getName() + ", Weight " + getWeight();
 	}
+	
+	/**
+	 * clone this LifeForm
+	 */
+	@Override
+	public LifeForm clone() { return copy(); }
+
+	/**
+	 * copy this LifeForm
+	 */
 	@Override
 	public LifeForm copy(){ return new LifeForm( getName(), getWeight() ); }
+	
+	/**
+	 * @return - how many entities per pixel are used
+	 */
 	@Override
-	public int getEntitiesPerPixel(){ return 0; }
+	public int getEntitiesPerPixel(){ return 2; }
 }
