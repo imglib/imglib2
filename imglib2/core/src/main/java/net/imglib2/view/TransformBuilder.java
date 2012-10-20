21b4d709a7bcb4c368e741a3f7561cb8bfacd2fb
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2012-10-20 22:37:02 +0200

    make TransformBuilder look through ImgPlus to the source Img

--- imglib2/core/src/main/java/net/imglib2/view/TransformBuilder.java
+++ imglib2/core/src/main/java/net/imglib2/view/TransformBuilder.java
@@ -9,13 +9,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,7 +27,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of any organization.
@@ -44,6 +44,7 @@ import net.imglib2.Interval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.ImgPlus;
 import net.imglib2.transform.Transform;
 import net.imglib2.transform.integer.BoundingBox;
 import net.imglib2.transform.integer.BoundingBoxTransform;
@@ -167,6 +168,10 @@ public class TransformBuilder< T >
 		else if ( IntervalView.class.isInstance( randomAccessible ) )
 		{
 			visit( ( ( IntervalView< T > ) randomAccessible ).getSource() );
+		}
+		else if ( ImgPlus.class.isInstance( randomAccessible ) )
+		{
+			visit( ( ( ImgPlus< T > ) randomAccessible ).getImg() );
 		}
 		else
 		{
