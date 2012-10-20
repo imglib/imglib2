753eedcb0128c29d6f1d5922c669e8a0b11addf6
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2012-10-20 14:06:20 +0200

    XY projectors and viewers require a Converter< ? super T, ... > to work. Just T is a too hard constraint. (cherry picked from commit cd7989ae86690de66f6b5fdc789cd634cfcea8c9)

--- imglib2/ui/src/main/java/net/imglib2/ui/InteractiveRealViewer2D.java
+++ imglib2/ui/src/main/java/net/imglib2/ui/InteractiveRealViewer2D.java
@@ -1,3 +1,39 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2012 Stephan Preibisch, Stephan Saalfeld, Tobias
+ * Pietzsch, Albert Cardona, Barry DeZonia, Curtis Rueden, Lee Kamentsky, Larry
+ * Lindsey, Johannes Schindelin, Christian Dietz, Grant Harris, Jean-Yves
+ * Tinevez, Steffen Jaensch, Mark Longair, Nick Perry, and Jan Funke.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
 package net.imglib2.ui;
 
 import net.imglib2.RandomAccessible;
@@ -10,6 +46,13 @@ import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.NumericType;
 
+/**
+ * 
+ * @param <T>
+ *
+ * @author TobiasPietzsch <tobias.pietzsch@gmail.com>
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ */
 public class InteractiveRealViewer2D< T extends NumericType< T > > extends AbstractInteractiveViewer2D< T >
 {
 	/**
@@ -20,24 +63,24 @@ public class InteractiveRealViewer2D< T extends NumericType< T > > extends Abstr
 	/**
 	 * Converts {@link #source} type T to ARGBType for display
 	 */
-	final protected Converter< T, ARGBType > converter;
+	final protected Converter< ? super T, ARGBType > converter;
 
-	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final Converter< T, ARGBType > converter )
+	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final Converter< ? super T, ARGBType > converter )
 	{
 		this( width, height, source, new AffineTransform2D(), converter );
 	}
 
-	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final Converter< T, ARGBType > converter, final DisplayTypes displayType )
+	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final Converter< ? super T, ARGBType > converter, final DisplayTypes displayType )
 	{
 		this( width, height, source, new AffineTransform2D(), converter, displayType );
 	}
 
-	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final AffineTransform2D sourceTransform, final Converter< T, ARGBType > converter )
+	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final AffineTransform2D sourceTransform, final Converter< ? super T, ARGBType > converter )
 	{
 		this( width, height, source, sourceTransform, converter, DisplayTypes.DISPLAY_SWING );
 	}
 
-	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final AffineTransform2D sourceTransform, final Converter< T, ARGBType > converter, final DisplayTypes displayType )
+	public InteractiveRealViewer2D( final int width, final int height, final RealRandomAccessible< T > source, final AffineTransform2D sourceTransform, final Converter< ? super T, ARGBType > converter, final DisplayTypes displayType )
 	{
 		super( width, height, sourceTransform, displayType );
 		this.source = source;
