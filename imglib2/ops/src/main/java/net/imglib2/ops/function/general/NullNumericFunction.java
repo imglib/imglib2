14237ebea1fbc2b409fd9734ea5430dcce487f63
Barry DeZonia <bdezonia@wisc.edu>
2012-10-25 09:26:42 -0500

    OPS: remove some broken functions

--- imglib2/ops/src/main/java/net/imglib2/ops/function/general/NullNumericFunction.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/function/general/NullNumericFunction.java
@@ -38,33 +38,34 @@
 package net.imglib2.ops.function.general;
 
 import net.imglib2.ops.function.Function;
+import net.imglib2.type.numeric.NumericType;
 
 
 /**
  * 
  * @author Barry DeZonia
  */
-public class NullPointFunction<INPUT, T> implements Function<INPUT,T> {
-
+public class NullNumericFunction<INPUT, T extends NumericType<T>>
+	implements Function<INPUT,T>
+{
+	private T type;
+	
+	public NullNumericFunction(T type) {
+		this.type = type;
+	}
+	
 	@Override
 	public void compute(INPUT point, T output) {
-		// do nothing
-		// TODO : Could set to NaN?
+		output.setZero();
 	}
 
 	@Override
 	public T createOutput() {
-		return null;
-		// TODO - returning null is sort of a problem. Though it makes sense.
-		//  However if we only pass NullFunctions at outermost loop maybe we can avoid
-		//  this method ever being called.
-		//  What good is a null function if outermost loop can count on its own? Null
-		//  function idea originally came about as a way to collect stats without
-		//  destroying existing data. That need may now be obsolete. Investigate.
+		return type.createVariable();
 	}
 
 	@Override
-	public NullPointFunction<INPUT,T> copy() {
-		return new NullPointFunction<INPUT,T>();
+	public NullNumericFunction<INPUT,T> copy() {
+		return new NullNumericFunction<INPUT,T>(type.copy());
 	}
 }
