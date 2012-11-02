569a8ac1b9e6af71492a050d1dd393127126bb32
Barry DeZonia <bdezonia@wisc.edu>
2012-11-02 11:33:38 -0500

    OPS: add coordinate adapter functions (double[] / long[])

---
+++ imglib2/ops/src/main/java/net/imglib2/ops/function/general/IntegerCoordinateAdapterFunction.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2012 Stephan Preibisch, Stephan Saalfeld, Tobias
+ * Pietzsch, Albert Cardona, Barry DeZonia, Curtis Rueden, Lee Kamentsky, Larry
+ * Lindsey, Johannes Schindelin, Christian Dietz, Grant Harris, Jean-Yves
+ * Tinevez, Steffen Jaensch, Mark Longair, Nick Perry, and Jan Funke.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package net.imglib2.ops.function.general;
+
+import net.imglib2.ops.function.Function;
+
+/**
+ * This class allows one to pass long[] input coordinates to a {@link Function}
+ * that expects to receive double[] input coordinates.
+ * 
+ * @author Barry DeZonia
+ */
+public class IntegerCoordinateAdapterFunction<T> implements Function<long[], T>
+{
+
+	// -- instance variables --
+
+	private final Function<double[], T> func;
+	private double[] coords;
+
+	// -- constructor --
+
+	public IntegerCoordinateAdapterFunction(Function<double[], T> func) {
+		this.func = func;
+		this.coords = null;
+	}
+
+	// -- Function methods --
+
+	@Override
+	public void compute(long[] input, T output) {
+		if (coords == null) coords = new double[input.length];
+		for (int i = 0; i < input.length; i++)
+			coords[i] = input[i];
+		func.compute(coords, output);
+	}
+
+	@Override
+	public T createOutput() {
+		return func.createOutput();
+	}
+
+	@Override
+	public IntegerCoordinateAdapterFunction<T> copy() {
+		return new IntegerCoordinateAdapterFunction<T>(func.copy());
+	}
+
+}
