fc05b6c8d66d8dad9cea9554f56bc4c3be3a0db1
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-03-19 10:43:07 +0100

    Localization: utils to collect data for subsequent fitting
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- imglib2/algorithms/gpl/src/main/java/net/imglib2/algorithm/localization/LocalizationUtils.java
+++ imglib2/algorithms/gpl/src/main/java/net/imglib2/algorithm/localization/LocalizationUtils.java
@@ -29,6 +29,9 @@ package net.imglib2.algorithm.localization;
 import java.util.Random;
 
 import net.imglib2.Cursor;
+import net.imglib2.Localizable;
+import net.imglib2.algorithm.region.localneighborhood.RectangleCursor;
+import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodGPL;
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
 
@@ -67,5 +70,68 @@ public class LocalizationUtils {
 			lc.get().add(var);
 		}
 	}
+	
+	/**
+	 * Collects the points to build the observation array, by iterating in a hypercube
+	 * around the given location. Points found out of the image are not included.
+	 * @param image  the source image to sample.
+	 * @param point  the location around which to collect the samples
+	 * @param span  the span size of the hypercube to sample, such that in dimension
+	 * <code>d</code>, the cube sampled if a of size <code>2 x span[d] + 1</code>.
+	 * @return  an {@link Observation} object containing the sampled data.
+	 */
+	public static final <T extends RealType<T>> Observation gatherObservationData(final Img<T> image, final Localizable point, final long[] span) {
+		
+		final int ndims = image.numDimensions();
+		RectangleNeighborhoodGPL<T, Img<T>> neighborhood = new RectangleNeighborhoodGPL<T, Img<T>>(image);
+		neighborhood.setSpan(span);
+		neighborhood.setPosition(point);
+
+		int n_pixels = (int) neighborhood.size();
+		double[] tmp_I 		= new double[n_pixels];
+		double[][] tmp_X 	= new double[n_pixels][ndims];
+		
+		RectangleCursor<T> cursor = neighborhood.localizingCursor();
+		long[] pos = new long[image.numDimensions()];
+
+		int index = 0;
+		while (cursor.hasNext()) {
+			
+			cursor.fwd();
+			cursor.localize(pos); // This is the absolute roi position
+			if (cursor.isOutOfBounds()) {
+				continue;
+			}
+
+			for (int i = 0; i < ndims; i++) {
+				tmp_X[index][i] = pos[i];
+			}
+			
+			tmp_I[index] = cursor.get().getRealDouble();
+			index++;
+		} 
+
+		// Now we possibly resize the arrays, in case we have been too close to the 
+		// image border.
+		double[][] X = null;
+		double[] I = null;
+		if (index == n_pixels) {
+			// Ok, we have gone through the whole square
+			X 	= tmp_X;
+			I 	= tmp_I;
+		} else {
+			// Re-dimension the arrays
+			X 	= new double[index][ndims];
+			I 	= new double[index];
+			System.arraycopy(tmp_X, 0, X, 0, index);
+			System.arraycopy(tmp_I, 0, I, 0, index);
+		}
+
+		Observation obs = new Observation();
+		obs.I = I;
+		obs.X = X;
+		return obs;
+	}
+
 
 }
