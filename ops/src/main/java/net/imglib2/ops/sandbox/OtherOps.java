0b1c6e056ae76287087adb3de1ba351b9aebbe0d
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2013-05-02 11:36:26 -0500

    more experimentation with static create and having a SubOp in addition. Expressions doesn't look too bad in comparison anymore.

--- ops/src/main/java/net/imglib2/ops/sandbox/OtherOps.java
+++ ops/src/main/java/net/imglib2/ops/sandbox/OtherOps.java
@@ -56,12 +56,6 @@ public class OtherOps
 
 		protected T output;
 
-		private AddOp()
-		{
-			input1 = null;
-			input2 = null;
-		}
-
 		public void setInput1( final T t )
 		{
 			input1 = t;
@@ -75,6 +69,12 @@ public class OtherOps
 		public void setOutput( final T t )
 		{
 			output = t;
+		}
+
+		protected void compute()
+		{
+			output.set( input1 );
+			output.add( input2 );
 		}
 
 		public AddOp< T > recreate( final Runnable op1, final Runnable op2 )
@@ -91,8 +91,7 @@ public class OtherOps
 						@Override
 						public void run()
 						{
-							output.set( input1 );
-							output.add( input2 );
+							compute();
 						}
 					};
 				else
@@ -102,8 +101,7 @@ public class OtherOps
 						public void run()
 						{
 							op2.run();
-							output.set( input1 );
-							output.add( input2 );
+							compute();
 						}
 					};
 			else if ( op2 instanceof EmptyOp )
@@ -113,8 +111,7 @@ public class OtherOps
 					public void run()
 					{
 						op1.run();
-						output.set( input1 );
-						output.add( input2 );
+						compute();
 					}
 				};
 			else
@@ -125,8 +122,160 @@ public class OtherOps
 					{
 						op1.run();
 						op2.run();
-						output.set( input1 );
-						output.add( input2 );
+						compute();
+					}
+				};
+		}
+	}
+
+	public static abstract class SubOp< T extends NumericType< T > > implements Runnable
+	{
+		protected T input1;
+
+		protected T input2;
+
+		protected T output;
+
+		public void setInput1( final T t )
+		{
+			input1 = t;
+		}
+
+		public void setInput2( final T t )
+		{
+			input2 = t;
+		}
+
+		public void setOutput( final T t )
+		{
+			output = t;
+		}
+
+		protected void compute()
+		{
+			output.set( input1 );
+			output.sub( input2 );
+		}
+
+		public SubOp< T > recreate( final Runnable op1, final Runnable op2 )
+		{
+			return create( op1, op2 );
+		}
+
+		private static < T extends NumericType< T > > SubOp< T > create( final Runnable op1, final Runnable op2 )
+		{
+			if ( op1 instanceof EmptyOp )
+				if ( op2 instanceof EmptyOp )
+					return new SubOp< T >()
+					{
+						@Override
+						public void run()
+						{
+							compute();
+						}
+					};
+				else
+					return new SubOp< T >()
+					{
+						@Override
+						public void run()
+						{
+							op2.run();
+							compute();
+						}
+					};
+			else if ( op2 instanceof EmptyOp )
+				return new SubOp< T >()
+				{
+					@Override
+					public void run()
+					{
+						op1.run();
+						compute();
+					}
+				};
+			else
+				return new SubOp< T >()
+				{
+					@Override
+					public void run()
+					{
+						op1.run();
+						op2.run();
+						compute();
+					}
+				};
+		}
+	}
+
+	public static abstract class BinOp< A, B, C, O extends BinaryOperation< A, B, C > > implements Runnable
+	{
+		protected A input1;
+		protected B input2;
+		protected C output;
+		protected final O op;
+
+		private BinOp( final O op )
+		{
+			this.op = op;
+		}
+
+		public void setInput1( final A t )
+		{
+			input1 = t;
+		}
+
+		public void setInput2( final B t )
+		{
+			input2 = t;
+		}
+
+		public void setOutput( final C t )
+		{
+			output = t;
+		}
+
+		public static < A, B, C, O extends BinaryOperation< A, B, C > > BinOp< A, B, C, O > create( final O op, final Runnable op1, final Runnable op2 )
+		{
+			if ( op1 instanceof EmptyOp )
+				if ( op2 instanceof EmptyOp )
+					return new BinOp< A, B, C, O >( op )
+					{
+						@Override
+						public void run()
+						{
+							op.compute( input1, input2, output );
+						}
+					};
+				else
+					return new BinOp< A, B, C, O >( op )
+					{
+						@Override
+						public void run()
+						{
+							op2.run();
+							op.compute( input1, input2, output );
+						}
+					};
+			else if ( op2 instanceof EmptyOp )
+				return new BinOp< A, B, C, O >( op )
+				{
+					@Override
+					public void run()
+					{
+						op1.run();
+						op.compute( input1, input2, output );
+					}
+				};
+			else
+				return new BinOp< A, B, C, O >( op )
+				{
+					@Override
+					public void run()
+					{
+						op1.run();
+						op2.run();
+						op.compute( input1, input2, output );
 					}
 				};
 		}
@@ -174,6 +323,46 @@ public class OtherOps
 
 		@Override
 		public SumExpression< T > copy()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
+
+	public static class SubExpression< T extends NumericType< T > > implements Sampler< T >
+	{
+		protected Sampler< T > a = null;
+
+		protected Sampler< T > b = null;
+
+		protected Sampler< T > c = null;
+
+		protected void setA( final Sampler< T > s )
+		{
+			a = s;
+		}
+
+		protected void setB( final Sampler< T > s )
+		{
+			b = s;
+		}
+
+		protected void setC( final Sampler< T > s )
+		{
+			c = s;
+		}
+
+		@Override
+		public T get()
+		{
+			final T t = c.get();
+			t.set( a.get() );
+			t.sub( b.get() );
+			return t;
+		}
+
+		@Override
+		public SubExpression< T > copy()
 		{
 			// TODO Auto-generated method stub
 			return null;
@@ -240,12 +429,12 @@ public class OtherOps
 				e1.setB( cb );
 				e1.setC( new Holds< FloatType >( new FloatType() ) );
 				final SumExpression< FloatType > e2 = new SumExpression< FloatType >();
-				e2.setA( cc );
-				e2.setB( cd );
+				e2.setA( e1 );
+				e2.setB( cc );
 				e2.setC( new Holds< FloatType >( new FloatType() ) );
-				final SumExpression< FloatType > e3 = new SumExpression< FloatType >();
-				e3.setA( e1 );
-				e3.setB( e2 );
+				final SubExpression< FloatType > e3 = new SubExpression< FloatType >();
+				e3.setA( e2 );
+				e3.setB( cd );
 				e3.setC( ce );
 
 				while ( cd.hasNext() )
@@ -270,20 +459,50 @@ public class OtherOps
 				final Cursor< FloatType > cc = imgC.cursor();
 				final Cursor< FloatType > cd = imgD.cursor();
 				final AddOp< FloatType > op1 = AddOp.<FloatType>create( new EmptyOp(), new EmptyOp() );
-				final AddOp< FloatType > op2 = AddOp.<FloatType>create( new EmptyOp(), new EmptyOp() );
-				final AddOp< FloatType > op3 = AddOp.<FloatType>create( op1, op2 );
+				final AddOp< FloatType > op2 = AddOp.<FloatType>create( op1, new EmptyOp() );
+				final SubOp< FloatType > op3 = SubOp.<FloatType>create( op2, new EmptyOp() );
 				final FloatType tmp1 = new FloatType();
 				final FloatType tmp2 = new FloatType();
 				op1.setOutput( tmp1 );
+				op2.setInput1( tmp1 );
 				op2.setOutput( tmp2 );
-				op3.setInput1( tmp1 );
-				op3.setInput2( tmp2 );
+				op3.setInput1( tmp2 );
 				for ( final FloatType t : imgE )
 				{
 					op1.setInput1( ca.next() );
 					op1.setInput2( cb.next() );
-					op2.setInput1( cc.next() );
-					op2.setInput2( cd.next() );
+					op2.setInput2( cc.next() );
+					op3.setInput2( cd.next() );
+					op3.setOutput( t );
+					op3.run();
+				}
+			}
+		} );
+
+		BenchmarkHelper.benchmarkAndPrint( 10, true, new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				final Cursor< FloatType > ca = imgA.cursor();
+				final Cursor< FloatType > cb = imgB.cursor();
+				final Cursor< FloatType > cc = imgC.cursor();
+				final Cursor< FloatType > cd = imgD.cursor();
+				final BinOp< FloatType, FloatType, FloatType, AddOpOld< FloatType > > op1 = BinOp.create( new AddOpOld< FloatType >(), new EmptyOp(), new EmptyOp() );
+				final BinOp< FloatType, FloatType, FloatType, AddOpOld< FloatType > > op2 = BinOp.create( new AddOpOld< FloatType >(), op1, new EmptyOp() );
+				final BinOp< FloatType, FloatType, FloatType, SubOpOld< FloatType > > op3 = BinOp.create( new SubOpOld< FloatType >(), op2, new EmptyOp() );
+				final FloatType tmp1 = new FloatType();
+				final FloatType tmp2 = new FloatType();
+				op1.setOutput( tmp1 );
+				op2.setInput1( tmp1 );
+				op2.setOutput( tmp2 );
+				op3.setInput1( tmp2 );
+				for ( final FloatType t : imgE )
+				{
+					op1.setInput1( ca.next() );
+					op1.setInput2( cb.next() );
+					op2.setInput2( cc.next() );
+					op3.setInput2( cd.next() );
 					op3.setOutput( t );
 					op3.run();
 				}
