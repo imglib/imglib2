f2031ff81040bea00282b31309e271dee88b8a35
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2013-05-07 12:35:24 +0200

    added utility methods to create RealIntervals.

--- core/src/main/java/net/imglib2/FinalRealInterval.java
+++ core/src/main/java/net/imglib2/FinalRealInterval.java
@@ -10,13 +10,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,7 +28,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of any organization.
@@ -39,7 +39,7 @@ package net.imglib2;
 
 /**
  * Implementation of the {@link RealInterval} interface.
- * 
+ *
  *
  * @author Stephan Preibisch
  */
@@ -47,7 +47,7 @@ public class FinalRealInterval extends AbstractRealInterval
 {
 	/**
 	 * Creates a new {@link AbstractRealInterval} using an existing {@link RealInterval}
-	 * 
+	 *
 	 * @param interval
 	 */
 	public FinalRealInterval( final RealInterval interval )
@@ -57,7 +57,7 @@ public class FinalRealInterval extends AbstractRealInterval
 
 	/**
 	 * Creates a new {@link AbstractRealInterval} from min and max coordinates
-	 * 
+	 *
 	 * @param min
 	 * @param max
 	 */
@@ -65,14 +65,65 @@ public class FinalRealInterval extends AbstractRealInterval
 	{
 		super( min, max );
 	}
-	
+
 	/**
 	 * Creates a new zero-bounded {@link AbstractRealInterval} with a certain size
-	 * 
+	 *
 	 * @param dimensions
 	 */
 	public FinalRealInterval( final double[] dimensions )
 	{
 		super( dimensions );
-	}	
+	}
+
+
+	/**
+	 * Create a {@link FinalRealInterval} from a parameter list comprising minimum
+	 * coordinates and size. For example, to create a 2D interval from (10, 10)
+	 * to (20, 40) use createMinSize( 10, 10, 11, 31 ).
+	 *
+	 * @param minsize
+	 *            a list of <em>2*n</em> parameters to create a <em>n</em>
+	 *            -dimensional interval. The first <em>n</em> parameters specify
+	 *            the minimum of the interval, the next <em>n</em> parameters
+	 *            specify the dimensions of the interval.
+	 * @return interval with the specified boundaries
+	 */
+	public static FinalRealInterval createMinSize( final double... minsize )
+	{
+		final int n = minsize.length / 2;
+		final double[] min = new double[ n ];
+		final double[] max = new double[ n ];
+		for ( int d = 0; d < n; ++d )
+		{
+			min[ d ] = minsize[ d ];
+			max[ d ] = min[ d ] + minsize[ d + n ] - 1;
+		}
+		return new FinalRealInterval( min, max );
+	}
+
+	/**
+	 * Create a {@link FinalRealInterval} from a parameter list comprising minimum
+	 * and maximum coordinates. For example, to create a 2D interval from (10,
+	 * 10) to (20, 40) use createMinMax( 10, 10, 20, 40 ).
+	 *
+	 * @param minmax
+	 *            a list of <em>2*n</em> parameters to create a <em>n</em>
+	 *            -dimensional interval. The first <em>n</em> parameters specify
+	 *            the minimum of the interval, the next <em>n</em> parameters
+	 *            specify the maximum of the interval.
+	 * @return interval with the specified boundaries
+	 */
+	public static FinalRealInterval createMinMax( final double... minmax )
+	{
+		final int n = minmax.length / 2;
+		final double[] min = new double[ n ];
+		final double[] max = new double[ n ];
+		for ( int d = 0; d < n; ++d )
+		{
+			min[ d ] = minmax[ d ];
+			max[ d ] = minmax[ d + n ];
+		}
+		return new FinalRealInterval( min, max );
+	}
 }
