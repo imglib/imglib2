405359db660249107169643c528cbe32687a54c1
Christian Dietz <christian.dietz@uni-konstanz.de>
2013-07-22 10:23:07 +0200

    Fixed bug in ShrinkLabeling

--- ops/src/main/java/net/imglib2/ops/operation/labeling/unary/ShrinkLabeling.java
+++ ops/src/main/java/net/imglib2/ops/operation/labeling/unary/ShrinkLabeling.java
@@ -37,10 +37,7 @@
 package net.imglib2.ops.operation.labeling.unary;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import net.imglib2.Cursor;
 import net.imglib2.labeling.Labeling;
@@ -58,178 +55,161 @@ import net.imglib2.view.Views;
  * 
  * @param <L>
  */
-public class ShrinkLabeling<L extends Comparable<L>> implements
-		UnaryOperation<Labeling<L>, Labeling<L>> {
+public class ShrinkLabeling< L extends Comparable< L >> implements UnaryOperation< Labeling< L >, Labeling< L >>
+{
 
-	private final ConnectedType m_ct;
+	private final ConnectedType ct;
 
-	private final int m_numIterations;
-
-	public ShrinkLabeling(ConnectedType ct, int numIterations) {
-		m_ct = ct;
-		m_numIterations = numIterations;
+	public ShrinkLabeling( ConnectedType ct )
+	{
+		this.ct = ct;
 	}
 
 	@Override
-	public Labeling<L> compute(final Labeling<L> op, final Labeling<L> r) {
+	public Labeling< L > compute( final Labeling< L > op, final Labeling< L > r )
+	{
 
-		OutOfBounds<LabelingType<L>> opRa = Views.extendValue(op,
-				new LabelingType<L>(new ArrayList<L>())).randomAccess();
-		OutOfBounds<LabelingType<L>> resRa = Views.extendValue(r,
-				new LabelingType<L>(new ArrayList<L>())).randomAccess();
+		OutOfBounds< LabelingType< L >> opRa = Views.extendValue( op, new LabelingType< L >( new ArrayList< L >() ) ).randomAccess();
+		OutOfBounds< LabelingType< L >> resRa = Views.extendValue( r, new LabelingType< L >( new ArrayList< L >() ) ).randomAccess();
 
-		int[] pos = new int[op.numDimensions()];
+		int[] pos = new int[ op.numDimensions() ];
 
-		Set<int[]> nextSeeds = new HashSet<int[]>();
-		for (int i = 0; i < m_numIterations; i++) {
-			Cursor<LabelingType<L>> labelingCursor = op.cursor();
-			if (i == 0) {
-				if (m_ct == ConnectedType.FOUR_CONNECTED) {
+		Cursor< LabelingType< L >> labelingCursor = op.cursor();
+		if ( ct == ConnectedType.FOUR_CONNECTED )
+		{
 
-					while (labelingCursor.hasNext()) {
-						labelingCursor.fwd();
+			while ( labelingCursor.hasNext() )
+			{
+				labelingCursor.fwd();
 
-						if (labelingCursor.get().getLabeling().isEmpty()) {
-							resRa.setPosition(labelingCursor);
-							resRa.get().setLabeling(
-									resRa.get().getMapping().emptyList());
-						} else {
-
-							opRa.setPosition(labelingCursor);
-							opRa.localize(pos);
-
-							nextSeeds.addAll(operate4Connected(pos,
-									labelingCursor.get().getLabeling(), opRa,
-									resRa));
-						}
-					}
-				} else if (m_ct == ConnectedType.EIGHT_CONNECTED) {
-
-					while (labelingCursor.hasNext()) {
-						labelingCursor.fwd();
-
-						if (labelingCursor.get().getLabeling().isEmpty()) {
-							resRa.setPosition(labelingCursor);
-							resRa.get().setLabeling(
-									resRa.get().getMapping().emptyList());
-						} else {
-
-							opRa.setPosition(labelingCursor);
-							opRa.localize(pos);
-
-							// 8 Connected
-							nextSeeds.addAll(operate8Connected(pos,
-									labelingCursor.get().getLabeling(), opRa,
-									resRa));
-						}
-					}
+				if ( labelingCursor.get().getLabeling().isEmpty() )
+				{
+					resRa.setPosition( labelingCursor );
+					resRa.get().setLabeling( resRa.get().getMapping().emptyList() );
 				}
-			} else {
-				Set<int[]> currentSeeds = new HashSet<int[]>();
-				currentSeeds.addAll(nextSeeds);
-				nextSeeds.clear();
-				if (m_ct == ConnectedType.FOUR_CONNECTED) {
+				else
+				{
 
-					for (int[] currentSeed : currentSeeds) {
-						resRa.setPosition(currentSeed);
+					opRa.setPosition( labelingCursor );
+					opRa.localize( pos );
 
-						nextSeeds.addAll(operate4Connected(currentSeed, resRa
-								.get().getLabeling(), resRa, resRa));
-					}
+					operate4Connected( pos, labelingCursor.get().getLabeling(), opRa, resRa );
 				}
+			}
+		}
+		else if ( ct == ConnectedType.EIGHT_CONNECTED )
+		{
 
-				if (m_ct == ConnectedType.EIGHT_CONNECTED) {
-					for (int[] currentSeed : currentSeeds) {
-						resRa.setPosition(currentSeed);
+			while ( labelingCursor.hasNext() )
+			{
+				labelingCursor.fwd();
 
-						nextSeeds.addAll(operate8Connected(currentSeed, resRa
-								.get().getLabeling(), resRa, resRa));
-					}
+				if ( labelingCursor.get().getLabeling().isEmpty() )
+				{
+					resRa.setPosition( labelingCursor );
+					resRa.get().setLabeling( resRa.get().getMapping().emptyList() );
 				}
+				else
+				{
 
+					opRa.setPosition( labelingCursor );
+					opRa.localize( pos );
+
+					// 8 Connected
+					operate8Connected( pos, labelingCursor.get().getLabeling(), opRa, resRa );
+				}
 			}
 		}
 		return r;
 
 	}
 
-	private synchronized Collection<int[]> operate4Connected(int[] currentPos,
-			List<L> currentLabeling, OutOfBounds<LabelingType<L>> opRa,
-			OutOfBounds<LabelingType<L>> resRa) {
+	private synchronized void operate4Connected( int[] currentPos, List< L > currentLabeling, OutOfBounds< LabelingType< L >> opRa, OutOfBounds< LabelingType< L >> resRa )
+	{
 
-		List<int[]> nextSeeds = new ArrayList<int[]>();
+		boolean set = false;
 
 		// 4 Connected
-		opRa.setPosition(currentPos[0] - 1, 0);
-		if (!opRa.get().getLabeling().isEmpty()) {
-			opRa.setPosition(currentPos[0] + 1, 0);
-			if (!opRa.get().getLabeling().isEmpty()) {
-				opRa.setPosition(currentPos[0], 0);
-				opRa.setPosition(currentPos[1] - 1, 1);
-				if (!opRa.get().getLabeling().isEmpty()) {
-					opRa.setPosition(currentPos[1] + 1, 1);
-					if (!opRa.get().getLabeling().isEmpty()) {
-						nextSeeds.add(currentPos.clone());
+		opRa.setPosition( currentPos[ 0 ] - 1, 0 );
+		if ( opRa.get().getLabeling().equals( currentLabeling ) )
+		{
+			opRa.setPosition( currentPos[ 0 ] + 1, 0 );
+			if ( opRa.get().getLabeling().equals( currentLabeling ) )
+			{
+				opRa.setPosition( currentPos[ 0 ], 0 );
+				opRa.setPosition( currentPos[ 1 ] - 1, 1 );
+				if ( opRa.get().getLabeling().equals( currentLabeling ) )
+				{
+					opRa.setPosition( currentPos[ 1 ] + 1, 1 );
+					if ( opRa.get().getLabeling().equals( currentLabeling ) )
+					{
+						set = true;
+						resRa.get().setLabeling( currentLabeling );
 					}
 				}
 			}
 		}
 
 		// Settings result cursor to currentPos
-		resRa.setPosition(currentPos);
+		resRa.setPosition( currentPos );
 
 		// No seeds: Labeling touches empty region
-		if (nextSeeds.size() == 0) {
-			resRa.get().setLabeling(opRa.get().getMapping().emptyList());
-		} else {
-			resRa.get().setLabeling(currentLabeling);
+		if ( !set )
+		{
+			resRa.get().setLabeling( opRa.get().getMapping().emptyList() );
 		}
-
-		return nextSeeds;
 	}
 
-	private synchronized Collection<int[]> operate8Connected(int[] currentPos,
-			List<L> currentLabeling, OutOfBounds<LabelingType<L>> opRa,
-			OutOfBounds<LabelingType<L>> resRa) {
+	private synchronized void operate8Connected( int[] currentPos, List< L > currentLabeling, OutOfBounds< LabelingType< L >> opRa, OutOfBounds< LabelingType< L >> resRa )
+	{
 
-		List<int[]> nextSeeds = new ArrayList<int[]>();
+		boolean set = false;
 
 		// middle left
-		opRa.setPosition(currentPos[0] - 1, 0);
-		if (!opRa.get().getLabeling().isEmpty()) {
+		opRa.setPosition( currentPos[ 0 ] - 1, 0 );
+		if ( opRa.get().getLabeling().equals( currentLabeling ) )
+		{
 
 			// middle right
-			opRa.setPosition(currentPos[0] + 1, 0);
-			if (!opRa.get().getLabeling().isEmpty()) {
+			opRa.setPosition( currentPos[ 0 ] + 1, 0 );
+			if ( opRa.get().getLabeling().equals( currentLabeling ) )
+			{
 
 				// upper right
-				opRa.setPosition(currentPos[1] - 1, 1);
-				if (!opRa.get().getLabeling().isEmpty()) {
+				opRa.setPosition( currentPos[ 1 ] - 1, 1 );
+				if ( opRa.get().getLabeling().equals( currentLabeling ) )
+				{
 
 					// lower right
-					opRa.setPosition(currentPos[1] + 1, 1);
-					if (!opRa.get().getLabeling().isEmpty()) {
+					opRa.setPosition( currentPos[ 1 ] + 1, 1 );
+					if ( opRa.get().getLabeling().equals( currentLabeling ) )
+					{
 
 						// lower middle
-						opRa.setPosition(currentPos[0], 0);
+						opRa.setPosition( currentPos[ 0 ], 0 );
 
-						if (!opRa.get().getLabeling().isEmpty()) {
+						if ( opRa.get().getLabeling().equals( currentLabeling ) )
+						{
 
 							// lower left
-							opRa.setPosition(currentPos[0] - 1, 0);
+							opRa.setPosition( currentPos[ 0 ] - 1, 0 );
 
-							if (!opRa.get().getLabeling().isEmpty()) {
+							if ( opRa.get().getLabeling().equals( currentLabeling ) )
+							{
 
 								// upper left
-								opRa.setPosition(currentPos[1] - 1, 1);
+								opRa.setPosition( currentPos[ 1 ] - 1, 1 );
 
-								if (!opRa.get().getLabeling().isEmpty()) {
+								if ( opRa.get().getLabeling().equals( currentLabeling ) )
+								{
 
 									// upper
 									// middle
-									opRa.setPosition(currentPos[0], 0);
-									if (!opRa.get().getLabeling().isEmpty()) {
-										nextSeeds.add(currentPos.clone());
+									opRa.setPosition( currentPos[ 0 ], 0 );
+									if ( !opRa.get().getLabeling().isEmpty() )
+									{
+										resRa.get().setLabeling( currentLabeling );
+										set = true;
 									}
 								}
 							}
@@ -239,21 +219,16 @@ public class ShrinkLabeling<L extends Comparable<L>> implements
 			}
 		}
 
-		// Settings result cursor to currentPos
-		resRa.setPosition(currentPos);
-
 		// No seeds: Labeling touches empty region
-		if (nextSeeds.size() == 0) {
-			resRa.get().setLabeling(opRa.get().getMapping().emptyList());
-		} else {
-			resRa.get().setLabeling(currentLabeling);
+		if ( !set )
+		{
+			resRa.get().setLabeling( opRa.get().getMapping().emptyList() );
 		}
-
-		return nextSeeds;
 	}
 
 	@Override
-	public UnaryOperation<Labeling<L>, Labeling<L>> copy() {
-		return new ShrinkLabeling<L>(m_ct, m_numIterations);
+	public UnaryOperation< Labeling< L >, Labeling< L >> copy()
+	{
+		return new ShrinkLabeling< L >( ct );
 	}
 }
