a2d0fa076d2cefed8c13f96cd9dba1fdcdac9207
Barry DeZonia <bdezonia@wisc.edu>
2013-08-19 09:55:35 -0500

    Improve CombinedCalibratedRealInterval API
    Now it jsut delegates units and calibration to child axes. Its simpler than
    before and has more public api.

--- meta/src/main/java/net/imglib2/meta/CombinedCalibratedRealInterval.java
+++ meta/src/main/java/net/imglib2/meta/CombinedCalibratedRealInterval.java
@@ -37,11 +37,6 @@
 
 package net.imglib2.meta;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-
 /**
  * @author Barry DeZonia
  */
@@ -49,86 +44,27 @@ public class CombinedCalibratedRealInterval<A extends CalibratedAxis, S extends 
 	extends CombinedRealInterval<A, S>
 {
 
-	// Units are available one per dim. These are the common units that all other
-	// contained CalibratedIntervals convert between.
-
-	private final List<String> units = new ArrayList<String>(super.numDimensions());
+	// NOTE: an older version of this class maintained a list of units (and not
+	// the scales). And one could change units and these could be used to scale
+	// data from underlying CalibratedAxes that already had their own different
+	// units. This may have been the wrong approach but if it isn't you can dig
+	// it out of git. That code was committed to the calibration-changes branch
+	// of imglib on Aug 16 2013. Note by maintaining a list of units I also had
+	// to override the add()'s, remove()'s methods to maintain the unit list also.
 
 	public void setUnit(String unit, int d) {
-		units.set(d, unit);
+		axis(d).setUnit(unit);
 	}
 
 	public String unit(int d) {
-		return units.get(d);
+		return axis(d).unit();
 	}
 
-	// NB - make sure adding and removing axes maintains units list. We override
-	// all the add and remove methods to do so.
-
-	@Override
-	public S remove(int index) {
-		S val = super.remove(index);
-		if (val != null) units.remove(index);
-		return val;
+	public void setCalibration(double cal, int d) {
+		axis(d).setCalibration(cal);
 	}
 
-	@Override
-	public boolean remove(Object o) {
-		for (int i = 0; i < size(); i++) {
-			if (get(i).equals(o)) {
-				return remove(i) != null;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		boolean changes = false;
-		for (Object obj : c) {
-			int i = 0;
-			while (i < size()) {
-				if (get(i) == obj) {
-					changes |= (remove(i) != null);
-				}
-				else i++;
-			}
-		}
-		return changes;
-	}
-
-	@Override
-	public void add(int index, S element) {
-		super.add(index, element);
-		units.add(index, null);
-	}
-
-	@Override
-	public boolean add(S e) {
-		super.add(e);
-		units.add(null);
-		return true;
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends S> c) {
-		boolean changes = false;
-		for (S obj : c) {
-			changes |= add(obj);
-		}
-		return changes;
-	}
-
-	@Override
-	public boolean addAll(int index, Collection<? extends S> c) {
-		boolean changes = false;
-		Iterator<? extends S> iter = c.iterator();
-		int spot = index;
-		while (iter.hasNext()) {
-			S value = iter.next();
-			add(spot++, value);
-			changes = true;
-		}
-		return changes;
+	public double calibration(int d) {
+		return axis(d).calibration();
 	}
 }
