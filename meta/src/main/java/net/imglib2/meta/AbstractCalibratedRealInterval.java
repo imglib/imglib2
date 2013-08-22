7b2abb65d9410f9b1a78b54e537e2507b520bdf5
Barry DeZonia <bdezonia@wisc.edu>
2013-08-22 10:59:50 -0500

    Add CalibratedRealInterval implementations

---
+++ meta/src/main/java/net/imglib2/meta/AbstractCalibratedRealInterval.java
@@ -0,0 +1,129 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2013 Stephan Preibisch, Tobias Pietzsch, Barry DeZonia,
+ * Stephan Saalfeld, Albert Cardona, Curtis Rueden, Christian Dietz, Jean-Yves
+ * Tinevez, Johannes Schindelin, Lee Kamentsky, Larry Lindsey, Grant Harris,
+ * Mark Hiner, Aivar Grislis, Martin Horn, Nick Perry, Michael Zinsmaier,
+ * Steffen Jaensch, Jan Funke, Mark Longair, and Dimiter Prodanov.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package net.imglib2.meta;
+
+import java.util.List;
+
+import net.imglib2.RealInterval;
+import net.imglib2.RealPositionable;
+
+/**
+ * @author Barry DeZonia
+ */
+public abstract class AbstractCalibratedRealInterval<A extends CalibratedAxis>
+	extends AbstractCalibratedSpace<A> implements CalibratedRealInterval<A>
+{
+
+	private final double[] min;
+	private final double[] max;
+
+	/*
+	// TODO FIXME - this seems like a broken ctor. But AbstractData needs a
+	// default ctor.
+
+	public AbstractCalibratedRealInterval() {
+		super(new ArrayList<A>());
+		this.min = new double[0];
+		this.max = new double[0];
+	}
+
+	 */
+
+	public AbstractCalibratedRealInterval(RealInterval interval) {
+		super(interval.numDimensions());
+		this.min = new double[interval.numDimensions()];
+		this.max = min.clone();
+		interval.realMin(min);
+		interval.realMax(max);
+	}
+
+	public AbstractCalibratedRealInterval(RealInterval interval, A... axes) {
+		super(axes);
+		this.min = new double[interval.numDimensions()];
+		this.max = min.clone();
+		interval.realMin(min);
+		interval.realMax(max);
+	}
+
+	public AbstractCalibratedRealInterval(RealInterval interval, List<A> axes) {
+		super(axes);
+		this.min = new double[interval.numDimensions()];
+		this.max = min.clone();
+		interval.realMin(min);
+		interval.realMax(max);
+	}
+
+	@Override
+	public double realMax(int d) {
+		return max[d];
+	}
+
+	@Override
+	public void realMax(double[] dest) {
+		for (int i = 0; i < dest.length; i++) {
+			dest[i] = realMax(i);
+		}
+	}
+
+	@Override
+	public void realMax(RealPositionable dest) {
+		for (int i = 0; i < dest.numDimensions(); i++) {
+			dest.setPosition(realMax(i), i);
+		}
+	}
+
+	@Override
+	public double realMin(int d) {
+		return min[d];
+	}
+
+	@Override
+	public void realMin(double[] dest) {
+		for (int i = 0; i < dest.length; i++) {
+			dest[i] = realMin(i);
+		}
+	}
+
+	@Override
+	public void realMin(RealPositionable dest) {
+		for (int i = 0; i < dest.numDimensions(); i++) {
+			dest.setPosition(realMin(i), i);
+		}
+	}
+
+}
