9cc5ff7b211efd2f79d4be1d70e6029ae613768c
Barry DeZonia <bdezonia@wisc.edu>
2013-09-09 16:07:46 -0500

    Add RodbardAxis and tests

---
+++ meta/src/main/java/net/imglib2/meta/axis/RodbardAxis.java
@@ -0,0 +1,143 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2013 Stephan Preibisch, Tobias Pietzsch, Barry DeZonia,
+ * Stephan Saalfeld, Albert Cardona, Curtis Rueden, Christian Dietz, Jean-Yves
+ * Tinevez, Johannes Schindelin, Lee Kamentsky, Larry Lindsey, Grant Harris,
+ * Mark Hiner, Aivar Grislis, Martin Horn, Nick Perry, Michael Zinsmaier,
+ * Steffen Jaensch, Jan Funke, Mark Longair, and Dimiter Prodanov.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package net.imglib2.meta.axis;
+
+import net.imglib2.meta.AbstractCalibratedAxis;
+import net.imglib2.meta.AxisType;
+import net.imglib2.meta.CalibratedAxis;
+
+/**
+ * RodbardAxis is a {@link CalibratedAxis } that scales raw values by the
+ * equation y = a + (b-a) / (1 + (x/c)^d).
+ * 
+ * @author Barry DeZonia
+ */
+public class RodbardAxis extends AbstractCalibratedAxis {
+
+	// -- fields --
+
+	private double a, b, c, d;
+
+	// -- constructors --
+
+	public RodbardAxis(AxisType type, String unit, double a, double b, double c,
+		double d)
+	{
+		super(type);
+		setUnit(unit);
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	// -- getters --
+
+	public double a() {
+		return a;
+	}
+
+	public double b() {
+		return b;
+	}
+
+	public double c() {
+		return c;
+	}
+
+	public double d() {
+		return d;
+	}
+
+	// -- setters --
+
+	public void setA(double v) {
+		this.a = v;
+	}
+
+	public void setB(double v) {
+		this.b = v;
+	}
+
+	public void setC(double v) {
+		this.c = v;
+	}
+
+	public void setD(double v) {
+		this.d = v;
+	}
+
+	// -- CalibratedAxis methods --
+
+	@Override
+	public double calibratedValue(double rawValue) {
+		return a + (b - a) / (1 + Math.pow((rawValue / c), d));
+	}
+
+	@Override
+	public double rawValue(double calibratedValue) {
+		return Math.pow(((b - calibratedValue) / (calibratedValue - a)), 1 / d) * c;
+	}
+
+	@Override
+	public String equation() {
+		return "y = a + (b-a) / (1 + (x/c)^d)";
+	}
+
+	@Override
+	public String calibratedEquation() {
+		return "y = (" + a + ") + ((" + b + ")-(" + a + ")) / (1 + (x/(" + c +
+			"))^(" + d + "))";
+	}
+
+	@Override
+	public boolean update(CalibratedAxis other) {
+		if (other instanceof RodbardAxis) {
+			RodbardAxis axis = (RodbardAxis) other;
+			setType(axis.type());
+			setUnit(axis.unit());
+			setA(axis.a());
+			setB(axis.b());
+			setC(axis.c());
+			setD(axis.d());
+			return true;
+		}
+		return false;
+	}
+
+}
