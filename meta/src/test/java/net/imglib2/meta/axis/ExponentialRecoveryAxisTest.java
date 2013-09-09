f6db0a142a36fb785b070fa69b086d736ffef686
Barry DeZonia <bdezonia@wisc.edu>
2013-09-09 14:25:37 -0500

    Add an ExponentialRecoveryAxis with tests

---
+++ meta/src/test/java/net/imglib2/meta/axis/ExponentialRecoveryAxisTest.java
@@ -0,0 +1,83 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2013 Stephan Preibisch, Tobias Pietzsch, Barry DeZonia,
+ * Stephan Saalfeld, Albert Cardona, Curtis Rueden, Christian Dietz, Jean-Yves
+ * Tinevez, Johannes Schindelin, Lee Kamentsky, Larry Lindsey, Grant Harris,
+ * Mark Hiner, Aivar Grislis, Martin Horn, Nick Perry, Michael Zinsmaier,
+ * Steffen Jaensch, Jan Funke, Mark Longair, and Dimiter Prodanov.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package net.imglib2.meta.axis;
+
+import static org.junit.Assert.assertEquals;
+import net.imglib2.meta.Axes;
+
+import org.junit.Test;
+
+/**
+ * @author Barry DeZonia
+ */
+public class ExponentialRecoveryAxisTest {
+
+	@Test
+	public void testCtor() {
+		ExponentialRecoveryAxis axis =
+			new ExponentialRecoveryAxis(Axes.X, "km", 4, 3, 2, 1);
+
+		assertEquals(Axes.X, axis.type());
+		assertEquals("km", axis.unit());
+		assertEquals(4, axis.a(), 0);
+		assertEquals(3, axis.b(), 0);
+		assertEquals(2, axis.c(), 0);
+		assertEquals(1, axis.d(), 0);
+		assertEquals(calValue(4, axis), axis.calibratedValue(4), 0);
+	}
+
+	@Test
+	public void testOtherStuff() {
+		LogLinearAxis axis = new LogLinearAxis();
+
+		axis.setA(5);
+		axis.setB(10);
+		axis.setC(15);
+		axis.setD(20);
+		assertEquals(5, axis.a(), 0);
+		assertEquals(10, axis.b(), 0);
+		assertEquals(15, axis.c(), 0);
+		assertEquals(20, axis.d(), 0);
+
+		assertEquals(axis.rawValue(axis.calibratedValue(3)), 3, 0.000001);
+	}
+
+	private double calValue(double raw, ExponentialRecoveryAxis axis) {
+		return axis.a() + axis.b() * (1 - Math.exp(axis.c() + (axis.d() * raw)));
+	}
+}
