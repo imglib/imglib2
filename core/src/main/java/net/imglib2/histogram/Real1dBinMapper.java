b196c1acc86b1b00e36fd497631685dc076b98c4
Barry DeZonia <bdezonia@wisc.edu>
2013-10-03 10:53:51 -0500

    Handle rounding errors on bin number computation

--- core/src/main/java/net/imglib2/histogram/Real1dBinMapper.java
+++ core/src/main/java/net/imglib2/histogram/Real1dBinMapper.java
@@ -57,6 +57,7 @@ public class Real1dBinMapper<T extends RealType<T>> implements BinMapper1d<T> {
 	private final double minVal, maxVal;
 	private final boolean tailBins;
 	private final double binWidth;
+	private final long interiorBins;
 
 	// -- constructor --
 
@@ -87,8 +88,13 @@ public class Real1dBinMapper<T extends RealType<T>> implements BinMapper1d<T> {
 			throw new IllegalArgumentException(
 				"invalid Real1dBinMapper: nonpositive data range specified");
 		}
-		if (tailBins) binWidth = (maxVal - minVal) / (bins - 2);
-		else binWidth = (maxVal - minVal) / (bins);
+		if (tailBins) {
+			this.interiorBins = bins - 2;
+		}
+		else {
+			this.interiorBins = bins;
+		}
+		this.binWidth = (maxVal - minVal) / (interiorBins);
 	}
 
 	// -- BinMapper methods --
@@ -105,7 +111,8 @@ public class Real1dBinMapper<T extends RealType<T>> implements BinMapper1d<T> {
 		if (val >= minVal && val <= maxVal) {
 			double bin = (val - minVal) / binWidth;
 			pos = (long) Math.floor(bin);
-			if (val == maxVal) pos--;
+			// catch out of bounds which can happen for edge values and roundoff errs
+			if (pos >= interiorBins) pos = interiorBins - 1;
 			if (tailBins) pos++;
 		}
 		else if (tailBins) {
