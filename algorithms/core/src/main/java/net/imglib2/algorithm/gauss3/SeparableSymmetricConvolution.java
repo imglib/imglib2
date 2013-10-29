6cb1bcb2bb52e9a22e9a389e56246380139934e8
dietzc <christian.dietz@uni-konstanz.de>
2013-10-29 13:17:42 +0100

    Removed ExecutionService timeout from SeparableSymmetricConvolution

--- algorithms/core/src/main/java/net/imglib2/algorithm/gauss3/SeparableSymmetricConvolution.java
+++ algorithms/core/src/main/java/net/imglib2/algorithm/gauss3/SeparableSymmetricConvolution.java
@@ -378,15 +378,6 @@ public final class SeparableSymmetricConvolution
 			};
 			service.execute( r );
 		}
-
-		try
-		{
-			service.awaitTermination( 1000, TimeUnit.DAYS );
-		}
-		catch ( final InterruptedException e )
-		{
-			e.printStackTrace();
-		}
 	}
 
 	static long[][] getTempImageDimensions( final Dimensions targetsize, final double[][] halfkernels )
