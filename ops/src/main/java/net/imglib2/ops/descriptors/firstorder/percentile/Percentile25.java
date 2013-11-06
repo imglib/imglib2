ba95281da6be86fa6da273abe383be15a08e74ed
Andreas Graumann <andreas.graumann@uni-konstanz.de>
2013-11-06 15:29:42 +0100

    small changes

--- ops/src/main/java/net/imglib2/ops/descriptors/firstorder/percentile/Percentile25.java
+++ ops/src/main/java/net/imglib2/ops/descriptors/firstorder/percentile/Percentile25.java
@@ -1,11 +1,8 @@
 package net.imglib2.ops.descriptors.firstorder.percentile;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import net.imglib2.ops.descriptors.ModuleInput;
 import net.imglib2.ops.descriptors.firstorder.impl.SortedValues;
-import net.imglib2.ops.operation.iterable.unary.MedianOp;
 
 public class Percentile25 extends Percentile 
 {
@@ -20,17 +17,6 @@ public class Percentile25 extends Percentile
 	@Override
 	protected double calculateFeature() {
 
-		
-		ArrayList< Double > statistics = new ArrayList< Double >();
-		
-		double[] val = sortedValues.get();
-		
-		for (int i = 0; i < val.length; i++)
-		{
-			statistics.add(val[i]);
-		}
-		
-		//return select( statistics, 0, statistics.size() - 1, statistics.size() / 2 );
 		return this.calculatePercentile(0.25, sortedValues.get());
 		
 	}
