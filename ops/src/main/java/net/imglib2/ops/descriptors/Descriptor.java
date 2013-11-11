0c1325235f3509052e5ecfde540794909e10f7de
Christian Dietz <christian.dietz@uni-konstanz.de>
2013-11-11 20:41:37 +0100

    removed descriptors from ops

--- ops/src/main/java/net/imglib2/ops/descriptors/Descriptor.java
+++
@@ -1,7 +0,0 @@
-package net.imglib2.ops.descriptors;
-
-public interface Descriptor extends CachedModule< double[] >
-{
-	// marker
-	String name();
-}
--- ops/src/main/java/net/imglib2/ops/descriptors/TreeSourceListener.java
+++
@@ -1,6 +0,0 @@
-package net.imglib2.ops.descriptors;
-
-public interface TreeSourceListener
-{
-	void updated( TreeSource< ? > s );
-}
