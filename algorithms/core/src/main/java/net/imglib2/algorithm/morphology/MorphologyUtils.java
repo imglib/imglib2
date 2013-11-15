de959b68752bada754a25b1ac87b43302f16929a
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-11-15 15:27:43 +0100

    Morpho.: move util method in util class.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- algorithms/core/src/main/java/net/imglib2/algorithm/morphology/MorphologyUtils.java
+++ algorithms/core/src/main/java/net/imglib2/algorithm/morphology/MorphologyUtils.java
@@ -2,10 +2,13 @@ package net.imglib2.algorithm.morphology;
 
 import net.imglib2.EuclideanSpace;
 import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccess;
 import net.imglib2.algorithm.region.localneighborhood.Neighborhood;
 import net.imglib2.algorithm.region.localneighborhood.Shape;
+import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.array.ArrayRandomAccess;
 import net.imglib2.img.basictypeaccess.array.BitArray;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.util.Util;
@@ -21,5 +24,212 @@ class MorphologyUtils
 		final IterableInterval< Neighborhood< BitType >> neighborhoods = shape.neighborhoods( img );
 		final Neighborhood< BitType > neighborhood = neighborhoods.cursor().next();
 		return neighborhood;
+	}
+
+	/**
+	 * Returns a string representation of the specified flat structuring element
+	 * (given as a {@link Shape}), cast over the dimensionality specified by an
+	 * {@link EuclideanSpace}.
+	 * <p>
+	 * This method only prints the first 3 dimensions of the structuring
+	 * element. Dimensions above 3 are skipped.
+	 * 
+	 * @param shape
+	 *            the structuring element to print.
+	 * @param dimensionality
+	 *            the dimensionality to cast it over. This is required as
+	 *            {@link Shape} does not carry a dimensionality, and we need one
+	 *            to generate a neighborhood to iterate.
+	 * @return a string representation of the structuring element.
+	 */
+	public static final String printNeighborhood( final Shape shape, final int dimensionality )
+	{
+		final Img< BitType > neighborhood;
+		{
+			final long[] dimensions = Util.getArrayFromValue( 1l, dimensionality );
+
+			final ArrayImg< BitType, BitArray > img = ArrayImgs.bits( dimensions );
+			final ArrayRandomAccess< BitType > randomAccess = img.randomAccess();
+			randomAccess.setPosition( Util.getArrayFromValue( 0, dimensions.length ) );
+			randomAccess.get().set( true );
+			neighborhood = MorphologicalOperations.dilateFull( img, shape, 1 );
+		}
+
+		final StringBuilder str = new StringBuilder();
+		for ( int d = 3; d < neighborhood.numDimensions(); d++ )
+		{
+			if ( neighborhood.dimension( d ) > 1 )
+			{
+				str.append( "Cannot print structuring elements with n dimensions > 3.\n" + "Skipping dimensions beyond 3.\n\n" );
+				break;
+			}
+		}
+
+		final RandomAccess< BitType > randomAccess = neighborhood.randomAccess();
+		if ( neighborhood.numDimensions() > 2 )
+		{
+			appendManySlice( randomAccess, neighborhood.dimension( 0 ), neighborhood.dimension( 1 ), neighborhood.dimension( 2 ), str );
+		}
+		else if ( neighborhood.numDimensions() > 1 )
+		{
+			appendSingleSlice( randomAccess, neighborhood.dimension( 0 ), neighborhood.dimension( 1 ), str );
+		}
+		else if ( neighborhood.numDimensions() > 0 )
+		{
+			appendLine( randomAccess, neighborhood.dimension( 0 ), str );
+		}
+		else
+		{
+			str.append( "Void structuring element.\n" );
+		}
+
+		return str.toString();
+	}
+
+	private static final void appendSingleSlice( final RandomAccess< BitType > ra, final long maxX, final long maxY, final StringBuilder str )
+	{
+		// Top line
+		str.append( '┌' );
+		for ( long x = 0; x < maxX; x++ )
+		{
+			str.append( '─' );
+		}
+		str.append( "┐\n" );
+		for ( long y = 0; y < maxY; y++ )
+		{
+			str.append( '│' );
+			ra.setPosition( y, 1 );
+			for ( long x = 0; x < maxX; x++ )
+			{
+				ra.setPosition( x, 0 );
+				if ( ra.get().get() )
+				{
+					str.append( '█' );
+				}
+				else
+				{
+					str.append( ' ' );
+				}
+			}
+			str.append( "│\n" );
+		}
+		// Bottom line
+		str.append( '└' );
+		for ( long x = 0; x < maxX; x++ )
+		{
+			str.append( '─' );
+		}
+		str.append( "┘\n" );
+	}
+
+	private static final void appendLine( final RandomAccess< BitType > ra, final long maxX, final StringBuilder str )
+	{
+		// Top line
+		str.append( '┌' );
+		for ( long x = 0; x < maxX; x++ )
+		{
+			str.append( '─' );
+		}
+		str.append( "┐\n" );
+		// Center
+		str.append( '│' );
+		for ( long x = 0; x < maxX; x++ )
+		{
+			ra.setPosition( x, 0 );
+			if ( ra.get().get() )
+			{
+				str.append( '█' );
+			}
+			else
+			{
+				str.append( ' ' );
+			}
+		}
+		str.append( "│\n" );
+		// Bottom line
+		str.append( '└' );
+		for ( long x = 0; x < maxX; x++ )
+		{
+			str.append( '─' );
+		}
+		str.append( "┘\n" );
+	}
+
+	private static final void appendManySlice( final RandomAccess< BitType > ra, final long maxX, final long maxY, final long maxZ, final StringBuilder str )
+	{
+		// Z names
+		final long width = Math.max( maxX + 3, 9l );
+		for ( int z = 0; z < maxZ; z++ )
+		{
+			final String sample = "Z = " + z + ":";
+			str.append( sample );
+			for ( int i = 0; i < width - sample.length(); i++ )
+			{
+				str.append( ' ' );
+			}
+		}
+		str.append( '\n' );
+
+		// Top line
+		for ( int z = 0; z < maxZ; z++ )
+		{
+			str.append( '┌' );
+			for ( long x = 0; x < maxX; x++ )
+			{
+				str.append( '─' );
+			}
+			str.append( "┐ " );
+			for ( int i = 0; i < width - maxX - 3; i++ )
+			{
+				str.append( ' ' );
+			}
+		}
+		str.append( '\n' );
+
+		// Neighborhood
+		for ( long y = 0; y < maxY; y++ )
+		{
+			ra.setPosition( y, 1 );
+
+			for ( int z = 0; z < maxZ; z++ )
+			{
+				ra.setPosition( z, 2 );
+				str.append( '│' );
+				for ( long x = 0; x < maxX; x++ )
+				{
+					ra.setPosition( x, 0 );
+					if ( ra.get().get() )
+					{
+						str.append( '█' );
+					}
+					else
+					{
+						str.append( ' ' );
+					}
+				}
+				str.append( '│' );
+				for ( int i = 0; i < width - maxX - 2; i++ )
+				{
+					str.append( ' ' );
+				}
+			}
+			str.append( '\n' );
+		}
+
+		// Bottom line
+		for ( int z = 0; z < maxZ; z++ )
+		{
+			str.append( '└' );
+			for ( long x = 0; x < maxX; x++ )
+			{
+				str.append( '─' );
+			}
+			str.append( "┘ " );
+			for ( int i = 0; i < width - maxX - 3; i++ )
+			{
+				str.append( ' ' );
+			}
+		}
+		str.append( '\n' );
 	}
 }
--- algorithms/core/src/main/java/net/imglib2/algorithm/morphology/StructuringElements.java
+++ algorithms/core/src/main/java/net/imglib2/algorithm/morphology/StructuringElements.java
@@ -3,8 +3,6 @@ package net.imglib2.algorithm.morphology;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.imglib2.EuclideanSpace;
-import net.imglib2.RandomAccess;
 import net.imglib2.algorithm.region.localneighborhood.CenteredRectangleShape;
 import net.imglib2.algorithm.region.localneighborhood.DiamondShape;
 import net.imglib2.algorithm.region.localneighborhood.DiamondTipsShape;
@@ -13,20 +11,12 @@ import net.imglib2.algorithm.region.localneighborhood.HyperSphereShape;
 import net.imglib2.algorithm.region.localneighborhood.PeriodicLineShape;
 import net.imglib2.algorithm.region.localneighborhood.RectangleShape;
 import net.imglib2.algorithm.region.localneighborhood.Shape;
-import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImg;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.array.ArrayRandomAccess;
-import net.imglib2.img.basictypeaccess.array.BitArray;
-import net.imglib2.type.logic.BitType;
-import net.imglib2.util.Util;
 
 /**
- * A collection of static utilities to facilitate the creation and visualization
- * of morphological structuring elements.
+ * A collection of static utilities to facilitate the creation of morphological
+ * structuring elements.
  *
- * @author Jean-Yves Tinevez <jeanyves.tinevez@gmail.com> Sep - Oct 2013
- *
+ * @author Jean-Yves Tinevez <jeanyves.tinevez@gmail.com> Sep - Nov 2013
  */
 public class StructuringElements
 {
@@ -51,7 +41,7 @@ public class StructuringElements
 	 */
 
 	/**
-	 * Generate a centered disk flat structuring element for morphological
+	 * Generates a centered disk flat structuring element for morphological
 	 * operations.
 	 * <p>
 	 * The structuring element (strel) is returned as a {@link List} of
@@ -111,7 +101,7 @@ public class StructuringElements
 	}
 
 	/**
-	 * Generate a centered disk flat structuring element for morphological
+	 * Generates a centered disk flat structuring element for morphological
 	 * operations.
 	 * <p>
 	 * The structuring element (strel) is returned as a {@link List} of
@@ -134,7 +124,7 @@ public class StructuringElements
 	 *            <li> No approximation is made and a full dimension-generic
 	 *            disk is returned. <li value="4"> The disk is decomposed in 4
 	 *            periodic lines, plus in some cases 2 horizontal lines. <li * *
-	 *            * * value="6"> The disk is decomposed in 6 periodic lines,
+	 *            * * * value="6"> The disk is decomposed in 6 periodic lines,
 	 *            plus in some cases 2 horizontal lines. <li value="8"> The disk
 	 *            is decomposed in 8 periodic lines, plus in some cases 2
 	 *            horizontal lines.
@@ -337,7 +327,7 @@ public class StructuringElements
 	}
 
 	/**
-	 * Generate a symmetric, centered, rectangular flat structuring element for
+	 * Generates a symmetric, centered, rectangular flat structuring element for
 	 * morphological operations.
 	 * <p>
 	 * The structuring element (strel) is returned as a {@link List} of
@@ -345,7 +335,7 @@ public class StructuringElements
 	 * better performance. The rectangle strel can be decomposed in a succession
 	 * of orthogonal lines and yield the exact same results on any of the
 	 * morphological operations.
-	 *
+	 * 
 	 * @param halfSpans
 	 *            an <code>int[]</code> array containing the half-span of the
 	 *            symmetric rectangle in each dimension. The total extent of the
@@ -389,7 +379,7 @@ public class StructuringElements
 	}
 
 	/**
-	 * Generate a symmetric, centered, rectangular flat structuring element for
+	 * Generates a symmetric, centered, rectangular flat structuring element for
 	 * morphological operations.
 	 * <p>
 	 * The structuring element (strel) is returned as a {@link List} of
@@ -398,7 +388,7 @@ public class StructuringElements
 	 * of orthogonal lines and yield the exact same results on any of the
 	 * morphological operations. This method uses a simple heuristic to decide
 	 * whether to decompose the rectangle or not.
-	 *
+	 * 
 	 * @param halfSpans
 	 *            an <code>int[]</code> array containing the half-span of the
 	 *            symmetric rectangle in each dimension. The total extent of the
@@ -437,7 +427,7 @@ public class StructuringElements
 	}
 
 	/**
-	 * Generate a centered flat diamond structuring element for morphological
+	 * Generates a centered flat diamond structuring element for morphological
 	 * operations.
 	 * <p>
 	 * The structuring element (strel) is returned as a {@link List} of
@@ -453,7 +443,7 @@ public class StructuringElements
 	 * dimensions, the theorem does not hold (even in practice), and we have to
 	 * fall back on a linear decomposition, still very effective (see [1] as
 	 * well).
-	 *
+	 * 
 	 * @param radius
 	 *            the desired radius of the diamond structuring element. The
 	 *            strel will extend over <code>2 × radius + 1</code> in all
@@ -463,7 +453,7 @@ public class StructuringElements
 	 *            used with. A structuring element build for one dimension will
 	 *            <b>not</b> work properly for any other dimensions.
 	 * @return the structuring element as a list of {@link Shape}s.
-	 *
+	 * 
 	 * @see <a href =
 	 *      "http://www.sciencedirect.com/science/article/pii/1049965292900553.htm"
 	 *      >[1]</a> Rein van den Boomgard and Richard van Balen, <i>Methods for
@@ -486,7 +476,7 @@ public class StructuringElements
 	}
 
 	/**
-	 * Generate a centered flat diamond structuring element for morphological
+	 * Generates a centered flat diamond structuring element for morphological
 	 * operations.
 	 * <p>
 	 * The structuring element (strel) is returned as a {@link List} of
@@ -502,7 +492,7 @@ public class StructuringElements
 	 * dimensions, the theorem does not hold (even in practice), and we have to
 	 * fall back on a linear decomposition, still very effective (see [1] as
 	 * well).
-	 *
+	 * 
 	 * @param radius
 	 *            the desired radius of the diamond structuring element. The
 	 *            strel will extend over <code>2 × radius + 1</code> in all
@@ -515,7 +505,7 @@ public class StructuringElements
 	 *            if <code>true</code>, this strel will be optimized through
 	 *            decomposition.
 	 * @return the structuring element as a list of {@link Shape}s.
-	 *
+	 * 
 	 * @see <a href =
 	 *      "http://www.sciencedirect.com/science/article/pii/1049965292900553.htm"
 	 *      >[1]</a> Rein van den Boomgard and Richard van Balen, <i>Methods for
@@ -578,212 +568,5 @@ public class StructuringElements
 			shape.add( new DiamondShape( radius ) );
 			return shape;
 		}
-	}
-
-	/**
-	 * Returns a string representation of the specified flat structuring element
-	 * (given as a {@link Shape}), cast over the dimensionality specified by an
-	 * {@link EuclideanSpace}.
-	 * <p>
-	 * This method only prints the first 3 dimensions of the structuring
-	 * element. Dimensions above 3 are skipped.
-	 *
-	 * @param shape
-	 *            the structuring element to print.
-	 * @param dimensionality
-	 *            the dimensionality to cast it over. This is required as
-	 *            {@link Shape} does not carry a dimensionality, and we need one
-	 *            to generate a neighborhood to iterate.
-	 * @return a string representation of the structuring element.
-	 */
-	public static final String printNeighborhood( final Shape shape, final int dimensionality )
-	{
-		final Img< BitType > neighborhood;
-		{
-			final long[] dimensions = Util.getArrayFromValue( 1l, dimensionality );
-
-			final ArrayImg< BitType, BitArray > img = ArrayImgs.bits( dimensions );
-			final ArrayRandomAccess< BitType > randomAccess = img.randomAccess();
-			randomAccess.setPosition( Util.getArrayFromValue( 0, dimensions.length ) );
-			randomAccess.get().set( true );
-			neighborhood = MorphologicalOperations.dilateFull( img, shape, 1 );
-		}
-
-		final StringBuilder str = new StringBuilder();
-		for ( int d = 3; d < neighborhood.numDimensions(); d++ )
-		{
-			if ( neighborhood.dimension( d ) > 1 )
-			{
-				str.append( "Cannot print structuring elements with n dimensions > 3.\n" + "Skipping dimensions beyond 3.\n\n" );
-				break;
-			}
-		}
-
-		final RandomAccess< BitType > randomAccess = neighborhood.randomAccess();
-		if ( neighborhood.numDimensions() > 2 )
-		{
-			appendManySlice( randomAccess, neighborhood.dimension( 0 ), neighborhood.dimension( 1 ), neighborhood.dimension( 2 ), str );
-		}
-		else if ( neighborhood.numDimensions() > 1 )
-		{
-			appendSingleSlice( randomAccess, neighborhood.dimension( 0 ), neighborhood.dimension( 1 ), str );
-		}
-		else if ( neighborhood.numDimensions() > 0 )
-		{
-			appendLine( randomAccess, neighborhood.dimension( 0 ), str );
-		}
-		else
-		{
-			str.append( "Void structuring element.\n" );
-		}
-
-		return str.toString();
-	}
-
-	private static final void appendSingleSlice( final RandomAccess< BitType > ra, final long maxX, final long maxY, final StringBuilder str )
-	{
-		// Top line
-		str.append( '┌' );
-		for ( long x = 0; x < maxX; x++ )
-		{
-			str.append( '─' );
-		}
-		str.append( "┐\n" );
-		for ( long y = 0; y < maxY; y++ )
-		{
-			str.append( '│' );
-			ra.setPosition( y, 1 );
-			for ( long x = 0; x < maxX; x++ )
-			{
-				ra.setPosition( x, 0 );
-				if ( ra.get().get() )
-				{
-					str.append( '█' );
-				}
-				else
-				{
-					str.append( ' ' );
-				}
-			}
-			str.append( "│\n" );
-		}
-		// Bottom line
-		str.append( '└' );
-		for ( long x = 0; x < maxX; x++ )
-		{
-			str.append( '─' );
-		}
-		str.append( "┘\n" );
-	}
-
-	private static final void appendLine( final RandomAccess< BitType > ra, final long maxX, final StringBuilder str )
-	{
-		// Top line
-		str.append( '┌' );
-		for ( long x = 0; x < maxX; x++ )
-		{
-			str.append( '─' );
-		}
-		str.append( "┐\n" );
-		// Center
-		str.append( '│' );
-		for ( long x = 0; x < maxX; x++ )
-		{
-			ra.setPosition( x, 0 );
-			if ( ra.get().get() )
-			{
-				str.append( '█' );
-			}
-			else
-			{
-				str.append( ' ' );
-			}
-		}
-		str.append( "│\n" );
-		// Bottom line
-		str.append( '└' );
-		for ( long x = 0; x < maxX; x++ )
-		{
-			str.append( '─' );
-		}
-		str.append( "┘\n" );
-	}
-
-	private static final void appendManySlice( final RandomAccess< BitType > ra, final long maxX, final long maxY, final long maxZ, final StringBuilder str )
-	{
-		// Z names
-		final long width = Math.max( maxX + 3, 9l );
-		for ( int z = 0; z < maxZ; z++ )
-		{
-			final String sample = "Z = " + z + ":";
-			str.append( sample );
-			for ( int i = 0; i < width - sample.length(); i++ )
-			{
-				str.append( ' ' );
-			}
-		}
-		str.append( '\n' );
-
-		// Top line
-		for ( int z = 0; z < maxZ; z++ )
-		{
-			str.append( '┌' );
-			for ( long x = 0; x < maxX; x++ )
-			{
-				str.append( '─' );
-			}
-			str.append( "┐ " );
-			for ( int i = 0; i < width - maxX - 3; i++ )
-			{
-				str.append( ' ' );
-			}
-		}
-		str.append( '\n' );
-
-		// Neighborhood
-		for ( long y = 0; y < maxY; y++ )
-		{
-			ra.setPosition( y, 1 );
-
-			for ( int z = 0; z < maxZ; z++ )
-			{
-				ra.setPosition( z, 2 );
-				str.append( '│' );
-				for ( long x = 0; x < maxX; x++ )
-				{
-					ra.setPosition( x, 0 );
-					if ( ra.get().get() )
-					{
-						str.append( '█' );
-					}
-					else
-					{
-						str.append( ' ' );
-					}
-				}
-				str.append( '│' );
-				for ( int i = 0; i < width - maxX - 2; i++ )
-				{
-					str.append( ' ' );
-				}
-			}
-			str.append( '\n' );
-		}
-
-		// Bottom line
-		for ( int z = 0; z < maxZ; z++ )
-		{
-			str.append( '└' );
-			for ( long x = 0; x < maxX; x++ )
-			{
-				str.append( '─' );
-			}
-			str.append( "┘ " );
-			for ( int i = 0; i < width - maxX - 3; i++ )
-			{
-				str.append( ' ' );
-			}
-		}
-		str.append( '\n' );
 	}
 }
