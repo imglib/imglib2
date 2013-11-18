b1d3aac5a6f0a01d73e725189cb14be6257a349b
Christian Dietz <christian.dietz@uni-konstanz.de>
2013-11-18 08:47:33 -0600

    Cleanup in separableSymmetricConvolution
    Organized imports

--- algorithms/core/src/main/java/net/imglib2/algorithm/gauss3/SeparableSymmetricConvolution.java
+++ algorithms/core/src/main/java/net/imglib2/algorithm/gauss3/SeparableSymmetricConvolution.java
@@ -40,7 +40,6 @@ package net.imglib2.algorithm.gauss3;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
