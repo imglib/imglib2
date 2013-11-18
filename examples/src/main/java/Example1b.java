8548faa04d777232d8c04fbb8ebf9068a814e2f5
Curtis Rueden <ctrueden@wisc.edu>
2013-11-18 10:52:46 -0600

    Example1b: update to reflect SCIFIO ImgOpener API

--- examples/src/main/java/Example1b.java
+++ examples/src/main/java/Example1b.java
@@ -35,54 +35,55 @@
  * #L%
  */
 import ij.ImageJ;
+import io.scif.img.ImgIOException;
+import io.scif.img.ImgOpener;
+import io.scif.img.ImgOptions;
+import io.scif.img.ImgOptions.ImgMode;
 
 import java.io.File;
 
-import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
-import net.imglib2.img.ImgFactory;
-import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import io.scif.img.ImgIOException;
-import io.scif.img.ImgOpener;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 
 /**
- * Opens a file with ImgOpener Bioformats as an ImgLib2 Img.
+ * Opens a file with SCIFIO's ImgOpener as an ImgLib2 Img.
  */
 public class Example1b
 {
 	// within this method we define <T> to be a RealType and a NativeType which means the
 	// Type is able to map the data into an java basic type array
 	public < T extends RealType< T > & NativeType< T > > Example1b()
-		throws ImgIOException, IncompatibleTypeException
+		throws ImgIOException
 	{
 		// define the file to open
 		File file = new File( "DrosophilaWing.tif" );
+		String path = file.getAbsolutePath();
 
-		// open with ImgOpener using an ArrayImgFactory, here the return type will be
-		// defined by the opener
-		// the opener will ignore the Type of the ArrayImgFactory
-		ImgFactory< ? > imgFactory = new ArrayImgFactory< T >();
-		Img< T > image = new ImgOpener().openImg( file.getAbsolutePath(), imgFactory );
+		// create the ImgOpener and ImgOptions
+		ImgOpener imgOpener = new ImgOpener();
+		ImgOptions imgOptions = new ImgOptions();
+
+		// open with ImgOpener as an ArrayImg
+		imgOptions.setImgModes( ImgMode.ARRAY );
+		Img< T > image = imgOpener.openImg( path, imgOptions );
 
 		// display it via ImgLib using ImageJ
 		ImageJFunctions.show( image );
 
-		// open with ImgOpener as Float using a CellImgFactory, it will be opened as float
-		// independent of the type of the image
+		// open with ImgOpener as a CellImg with FloatType
+		// it will be opened as float independent of the type of the image
 		// to enforce to open it as FloatType, an instance of FloatType has to be passed along
-		Img< FloatType > imageFloat = new ImgOpener().openImg( file.getAbsolutePath(),
-			new CellImgFactory< FloatType >( 10 ), new FloatType() );
+		imgOptions.setImgModes( ImgMode.CELL );
+		Img< FloatType > imageFloat = imgOpener.openImg( path, new FloatType(), imgOptions );
 
 		// display it via ImgLib using ImageJ
 		ImageJFunctions.show( imageFloat );
 	}
 
-	public static void main( String[] args ) throws ImgIOException, IncompatibleTypeException
+	public static void main( String[] args ) throws ImgIOException
 	{
 		// open an ImageJ window
 		new ImageJ();
