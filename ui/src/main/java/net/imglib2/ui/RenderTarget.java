1a436f5af8fb3f63abb1f9044bca57045c26389b
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2013-12-02 17:47:40 +0100

    Fix double-buffering, part 1.

--- ui/src/main/java/net/imglib2/ui/RenderTarget.java
+++ ui/src/main/java/net/imglib2/ui/RenderTarget.java
@@ -10,13 +10,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,7 +28,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of any organization.
@@ -56,9 +56,12 @@ public interface RenderTarget
 	/**
 	 * Set the {@link BufferedImage} that is to be drawn on the canvas.
 	 *
-	 * @param bufferedImage image to draw (may be null).
+	 * @param bufferedImage
+	 *            image to draw (may be null).
+	 * @return a previously set {@link BufferedImage} that is currently not
+	 *         being painted or null. Used for double-buffering.
 	 */
-	public void setBufferedImage( final BufferedImage bufferedImage );
+	public BufferedImage setBufferedImage( final BufferedImage bufferedImage );
 
 	/**
 	 * Get the current canvas width.
