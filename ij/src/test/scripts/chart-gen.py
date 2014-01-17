7cce9af0a22e4e469f0eb7054610dad2ec5289b2
Curtis Rueden <ctrueden@wisc.edu>
2014-01-17 15:29:08 -0600

    Update license and file headers
    This was done using:
      mvn license:update-project-license license:update-file-header
    
    The only real change is the loss of the legal disclaimer at the bottom
    of the license. This disclaimer is not actually part of the BSD-2
    license proper, but rather an addendum. For relevant discussion, see:
      https://jira.codehaus.org/browse/MLICENSE-80

--- ij/src/test/scripts/chart-gen.py
+++ ij/src/test/scripts/chart-gen.py
@@ -2,7 +2,7 @@
 # #%L
 # ImgLib2: a general-purpose, multidimensional image processing library.
 # %%
-# Copyright (C) 2009 - 2013 Stephan Preibisch, Tobias Pietzsch, Barry DeZonia,
+# Copyright (C) 2009 - 2014 Stephan Preibisch, Tobias Pietzsch, Barry DeZonia,
 # Stephan Saalfeld, Albert Cardona, Curtis Rueden, Christian Dietz, Jean-Yves
 # Tinevez, Johannes Schindelin, Lee Kamentsky, Larry Lindsey, Grant Harris,
 # Mark Hiner, Aivar Grislis, Martin Horn, Nick Perry, Michael Zinsmaier,
@@ -28,10 +28,6 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
-# The views and conclusions contained in the software and documentation are
-# those of the authors and should not be interpreted as representing official
-# policies, either expressed or implied, of any organization.
 # #L%
 ###
 
