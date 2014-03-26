9f1bc0a6fb679dc9e0da74e053590ba1b2089f28
Jonathan <squareys@googlemail.com>
2014-03-26 12:34:13 +0100

    Implemented SubIntervalIterable for NativeImgLabeling

--- core/src/main/java/net/imglib2/labeling/NativeImgLabeling.java
+++ core/src/main/java/net/imglib2/labeling/NativeImgLabeling.java
@@ -36,11 +36,14 @@ package net.imglib2.labeling;
 import java.util.Iterator;
 
 import net.imglib2.Cursor;
+import net.imglib2.FlatIterationOrder;
 import net.imglib2.Interval;
 import net.imglib2.IterableRealInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.IntegerType;
+import net.imglib2.view.Views;
+import net.imglib2.view.iteration.SubIntervalIterable;
 
 /**
  * A labeling backed by a native image that takes a labeling type backed by an
@@ -51,7 +54,7 @@ import net.imglib2.type.numeric.IntegerType;
  * 
  * @author Lee Kamentsky, Christian Dietz, Martin Horn
  */
-public class NativeImgLabeling< T extends Comparable< T >, I extends IntegerType< I >> extends AbstractNativeLabeling< T >
+public class NativeImgLabeling< T extends Comparable< T >, I extends IntegerType< I >> extends AbstractNativeLabeling< T > implements SubIntervalIterable< LabelingType< T > >
 {
 
 	protected final long[] generation;
@@ -186,5 +189,51 @@ public class NativeImgLabeling< T extends Comparable< T >, I extends IntegerType
 			}
 
 		};
+	}
+	
+	@SuppressWarnings( "unchecked" )
+	@Override
+	public boolean supportsOptimizedCursor( Interval interval )
+	{
+		if ( this.img instanceof SubIntervalIterable )
+			return ( ( SubIntervalIterable< I > ) this.img ).supportsOptimizedCursor( interval );
+		else
+			return false;
+	}
+
+	@SuppressWarnings( "unchecked" )
+	@Override
+	public Object subIntervalIterationOrder( Interval interval )
+	{
+		if ( this.img instanceof SubIntervalIterable )
+			return ( ( SubIntervalIterable< I > ) this.img ).subIntervalIterationOrder( interval );
+		else
+			return new FlatIterationOrder( interval );
+	}
+
+	@SuppressWarnings( "unchecked" )
+	@Override
+	public Cursor< LabelingType< T > > cursor( Interval interval )
+	{
+		Cursor< I > c = null;
+		if ( this.img instanceof SubIntervalIterable )
+			c = ( ( SubIntervalIterable< I > ) this.img ).cursor( interval );
+		else
+			c = Views.interval( this.img, interval ).cursor();
+
+		return new LabelingConvertedCursor< I, T >( c, generation, mapping );
+	}
+
+	@SuppressWarnings( "unchecked" )
+	@Override
+	public Cursor< LabelingType< T > > localizingCursor( Interval interval )
+	{
+		Cursor< I > c = null;
+		if ( this.img instanceof SubIntervalIterable )
+			c = ( ( SubIntervalIterable< I > ) this.img ).localizingCursor( interval );
+		else
+			c = Views.interval( this.img, interval ).localizingCursor();
+
+		return new LabelingConvertedCursor< I, T >( c, generation, mapping );
 	}
 }
