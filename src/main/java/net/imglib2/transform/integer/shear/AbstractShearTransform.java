bf7c626a930f20d76f7960538ce6560f9eeeba0a
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-05-05 17:17:32 -0400

    added most simple implementation of integer shear transform

---
+++ src/main/java/net/imglib2/transform/integer/shear/AbstractShearTransform.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package net.imglib2.transform.integer.shear;
+
+import net.imglib2.Localizable;
+import net.imglib2.Positionable;
+import net.imglib2.transform.InvertibleTransform;
+import net.imglib2.transform.integer.BoundingBoxTransform;
+
+/**
+ * 
+ * Most simple case of a shear transform that just implements
+ * for a integer valued coordinate:
+ * coordinate[ shearDimension ] += coordinate[ referenceDimension ] (forward)
+ * coordinate[ shearDimension ] -= coordinate[ referenceDimension ] (backward)
+ * 
+ * This abstract class holds the inverse and implements applyInverse in
+ * terms of inverse.apply
+ * 
+ * @author Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
+ *
+ */
+public abstract class AbstractShearTransform implements InvertibleTransform, BoundingBoxTransform  {
+	
+	
+	protected final int nDim;
+	protected final int shearDimension;
+	protected final int referenceDimension;
+	
+	protected AbstractShearTransform inverse;
+	
+	
+	protected AbstractShearTransform(
+			int nDim,
+			int shearDimension,
+			int referenceDimension )
+	{
+		this( nDim, shearDimension, referenceDimension, null );
+	}
+	
+	
+	protected AbstractShearTransform(
+			int nDim,
+			int shearDimension,
+			int referenceDimension,
+			AbstractShearTransform inverse )
+	{
+		this.nDim               = nDim;
+		this.shearDimension     = shearDimension;
+		this.referenceDimension = referenceDimension;
+		this.inverse            = inverse;
+	}
+	
+	
+	public int getReferenceDimension() {
+		return referenceDimension;
+	}
+
+
+	@Override
+	public int numSourceDimensions() 
+	{
+		return this.numDimensions();
+	}
+	
+
+	@Override
+	public int numTargetDimensions() 
+	{
+		return this.numDimensions();
+	}
+	
+	
+	public int numDimensions() 
+	{
+		return nDim;
+	}
+	
+	
+	public int getShearDimension()
+	{
+		return this.shearDimension;
+	}
+	
+
+	@Override
+	public void applyInverse( long[] source, long[] target ) 
+	{
+		// for inverse, source and target need to be switched
+		this.inverse.apply( target, source );
+	}
+	
+
+	@Override
+	public void applyInverse(int[] source, int[] target) 
+	{
+		// for inverse, source and target need to be switched
+		this.inverse.apply( target, source );
+	}
+	
+
+	@Override
+	public void applyInverse( Positionable source, Localizable target ) 
+	{
+		// for inverse, source and target need to be switched
+		this.inverse.apply( target, source );
+	}
+	
+
+	@Override
+	public AbstractShearTransform inverse() 
+	{
+		return this.inverse;
+	}
+	
+	
+	public abstract long[] getShear();
+
+	
+	public abstract AbstractShearTransform copy();
+	
+	
+}
---
+++ src/main/java/net/imglib2/transform/integer/shear/InverseShearTransform.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package net.imglib2.transform.integer.shear;
+
+import net.imglib2.Localizable;
+import net.imglib2.Positionable;
+import net.imglib2.transform.integer.BoundingBox;
+
+/**
+ * Backward implementation of the most simple case of a shear transform:
+ * coordinate[ shearDimension ] -= coordinate[ referenceDimension ]
+ * 
+ * 
+ * @author Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
+ *
+ */
+public class InverseShearTransform extends AbstractShearTransform {
+	
+	
+	/**
+	 * @param nDim					Number of dimensions (source and target dimensions must be the same)
+	 * @param shearDimension		Dimension to be sheared.
+	 * @param referenceDimension	Dimension used as reference for shear.
+	 */
+	public InverseShearTransform(
+			int nDim,
+			int shearDimension,
+			int referenceDimension )
+	{
+		super( nDim, shearDimension, referenceDimension );
+		this.inverse = new ShearTransform( 
+				nDim, 
+				shearDimension,
+				referenceDimension,
+				this );
+	}
+
+	
+	/**
+	 * Protected constructor for passing an inverse to avoid construction of unnecessary objects.
+	 * 
+	 * @param nDim					Number of dimensions (source and target dimensions must be the same)
+	 * @param shearDimension		Dimension to be sheared.
+	 * @param referenceDimension	Dimension used as reference for shear.
+	 * @param inverse				
+	 */
+	protected InverseShearTransform( 
+			int nDim, 
+			int shearDimension,
+			int referenceDimension, 
+			AbstractShearTransform inverse ) {
+		super( nDim, shearDimension, referenceDimension, inverse );
+	}
+
+
+	@Override
+	public void apply( long[] source, long[] target ) 
+	{
+		assert source.length >= this.nDim && target.length >= nDim;
+		System.arraycopy( source, 0, target, 0, source.length );
+		target[ shearDimension ] -= target[ referenceDimension ];
+	}
+	
+
+	@Override
+	public void apply( int[] source, int[] target )
+	{
+		assert source.length >= this.nDim && target.length >= nDim;
+		System.arraycopy( source, 0, target, 0, source.length );
+		target[ shearDimension ] -= target[ referenceDimension ];
+	}
+	
+
+	@Override
+	public void apply( Localizable source, Positionable target ) 
+	{
+		assert source.numDimensions() >= this.nDim && target.numDimensions() >= nDim;
+		target.setPosition( source );
+		target.setPosition( source.getLongPosition( shearDimension ) - source.getLongPosition( referenceDimension ), shearDimension );
+	}
+
+	@Override
+	public AbstractShearTransform copy()
+	{
+		return new InverseShearTransform( nDim, shearDimension, referenceDimension ); // return this; ?
+	}
+
+
+	@Override
+	public long[] getShear() 
+	{
+		long[] shear = new long[ this.nDim ];
+		shear[ this.shearDimension ] = -1;
+		return shear;
+	}
+
+	
+	@Override public BoundingBox transform( BoundingBox bb )
+	{
+		long[] c = bb.corner1;
+		// assume that corner2[ i ] > corner1[ i ]
+		long diff = bb.corner2[ this.referenceDimension ] - c[ this.referenceDimension ];
+		c[ this.shearDimension ] -=diff;
+		return bb;
+	}
+
+	
+}
