26b128a3d37840ef86fb413fcba386a5f99759cc
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2016-12-06 21:12:54 +0100

    added volatile and dirty volatile accesses

---
+++ src/main/java/net/imglib2/img/basictypeaccess/volatiles/array/VolatileLongArray.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * BigDataViewer core classes with minimal dependencies
+ * %%
+ * Copyright (C) 2012 - 2016 Tobias Pietzsch, Stephan Saalfeld, Stephan Preibisch,
+ * Jean-Yves Tinevez, HongKee Moon, Johannes Schindelin, Curtis Rueden, John Bogovic
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.img.basictypeaccess.volatiles.array;
+
+import net.imglib2.img.basictypeaccess.array.AbstractLongArray;
+import net.imglib2.img.basictypeaccess.array.LongArray;
+import net.imglib2.img.basictypeaccess.volatiles.VolatileLongAccess;
+
+/**
+ * A {@link LongArray} with an {@link #isValid()} flag.
+ *
+ * @author Stephan Saalfeld
+ * @author Tobias Pietzsch
+ */
+public class VolatileLongArray extends AbstractLongArray< VolatileLongArray > implements VolatileLongAccess
+{
+	final protected boolean isValid;
+
+	public VolatileLongArray( final int numEntities, final boolean isValid )
+	{
+		super( numEntities );
+		this.isValid = isValid;
+		this.data = new long[ numEntities ];
+	}
+
+	public VolatileLongArray( final long[] data, final boolean isValid )
+	{
+		super( data );
+		this.isValid = isValid;
+	}
+
+	@Override
+	public VolatileLongArray createArray( final int numEntities )
+	{
+		return new VolatileLongArray( numEntities, true );
+	}
+
+	@Override
+	public boolean isValid()
+	{
+		return isValid;
+	}
+}
