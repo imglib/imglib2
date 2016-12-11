0dff7682d1810e0e26e1280a30f84952d2df6ac5
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2016-12-11 20:16:22 +0100

    added basic cache and loader interfaces and soft- and weak reference implementation

---
+++ src/main/java/net/imglib2/cache/SoftReferenceCache.java
@@ -0,0 +1,96 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2016 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imglib2.cache;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.util.Map;
+
+/**
+ * A {@link SoftReference} based implementation of {@link ReferenceCache}
+ *
+ * @param <K>
+ *            key type.
+ * @param <V>
+ *            value type.
+ *
+ * @author Tobias Pietzsch
+ * @author Stephan Saalfeld
+ */
+abstract public class SoftReferenceCache< K, V > extends ReferenceCache< K, V >
+{
+	protected class Reference extends SoftReference< V > implements net.imglib2.cache.ReferenceCache.CacheReference< K >
+	{
+		final protected K key;
+		final protected Map< K, ? super Reference > map;
+
+		public Reference(
+				final K key,
+				final V referent,
+				final Map< K, ? super Reference > map,
+				final ReferenceQueue< ? super V > q )
+		{
+			super( referent, q );
+			this.key = key;
+			this.map = map;
+
+		}
+
+		@Override
+		public K getKey()
+		{
+			return key;
+		}
+
+		@Override
+		public Map< K, ? super Reference > getMap()
+		{
+			return map;
+		}
+	}
+
+	public SoftReferenceCache(
+			final Loader< K, V > loader,
+			final ReferenceQueue< ? super V > referenceQueue )
+	{
+		super( loader, referenceQueue );
+	}
+
+	@Override
+	protected Reference createReference( final K key, final V value )
+	{
+		return new Reference( key, value, map, referenceQueue );
+	}
+}
