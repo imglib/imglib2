944ea2b408e07a922c25f137efe20f304f1ffad2
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-06-27 16:12:32 -0400

    Add subsampling test for multiple dimensions
    tests subsampling on 5 dimensional interval (constant data), verifying
    the size of each dimension after subsampling

--- src/test/java/net/imglib2/view/SubsampleIntervalViewTest.java
+++ src/test/java/net/imglib2/view/SubsampleIntervalViewTest.java
@@ -4,10 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.IntArray;
 import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.ConstantUtils;
 
 public class SubsampleIntervalViewTest
 {
@@ -47,6 +51,52 @@ public class SubsampleIntervalViewTest
 				for(int i = 0; i < expectedSubsamples[subsamplesize].length || subIntCursor.hasNext(); i ++) {
 					Assert.assertEquals( expectedSubsamples[subsamplesize][i], subIntCursor.next().get() );
 				}
+			}
+		}
+	}
+	
+	@Test
+	public void testDimSubsampling() {
+		final long[] minValues = new long[] {   0, 30, -10, 303, -302};
+		final long[] maxValues = new long[] { 100, 53,   7, 305, 1431};
+		final int dimension = minValues.length;
+		
+		final FinalInterval interval = new FinalInterval(minValues, maxValues);
+		
+		final long[][] expectedDimensionSizes = new long[][] {
+			{},
+			{}, // not testing a subsampling size of 0 or 1
+			{51, 12, 9, 2, 867},
+			{34, 8, 6, 1, 578},
+			{26, 6, 5, 1, 434},
+			{21, 5, 4, 1, 347},
+			{17, 4, 3, 1, 289},
+			{15, 4, 3, 1, 248},
+			{13, 3, 3, 1, 217},
+		};
+		
+		// tests a bunch of random shifts in each dimension
+		final long[][] testShifts = new long[][] {
+			{-56,   11,  82, -83,  -33},
+			{  4,   18,  10,   8,   13},
+			{  4,  -85,   0, -58,  -73},
+			{ -7,    3, -75, -23,   31},
+			{ 79,   -7,  54,  44,    1},
+			{  0, -802, 968, 185, 1072}
+		};
+
+		IntervalView< FloatType > shiftedInterval;
+		SubsampleIntervalView< FloatType > subInterval;
+		
+		RandomAccessibleInterval< FloatType > randAccessInterval = ConstantUtils.constantRandomAccessibleInterval( 
+			     new FloatType( 1f ), 5, interval );
+				
+		for(long[] shift : testShifts) {
+			shiftedInterval = Views.translate( randAccessInterval, shift );
+			for(int subsamplesize = 2; subsamplesize <= 8; subsamplesize ++ ) {
+				subInterval = Views.subsample( shiftedInterval, subsamplesize);
+				// checks that size of each dimension is what it should be after subsampling
+				Assert.assertArrayEquals( expectedDimensionSizes[subsamplesize], subInterval.dimensions );
 			}
 		}
 	}
