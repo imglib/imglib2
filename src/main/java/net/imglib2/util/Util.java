18345f55759ca8db7d28cf49914f39b2eb4f1ef9
random <hallo@frauzufall.de>
2017-10-05 23:20:26 +0200

    percentile using KthElement instead of quicksort

--- src/main/java/net/imglib2/util/Util.java
+++ src/main/java/net/imglib2/util/Util.java
@@ -159,10 +159,11 @@ public class Util
 	{
 		final double temp[] = values.clone();
 		final int length = temp.length;
+		final int pos = Math.min( length - 1, Math.max( 0, ( int ) Math.round( ( length - 1 ) * percentile ) ) );
 
-		quicksort( temp );
+		KthElement.kthElement( pos, temp );
 
-		return temp[ Math.min( length - 1, Math.max( 0, ( int ) Math.round( ( length - 1 ) * percentile ) ) ) ];
+		return temp[ pos ];
 	}
 
 	public static double averageDouble( final List< Double > values )
