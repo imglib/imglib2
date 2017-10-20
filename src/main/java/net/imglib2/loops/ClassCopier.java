8f16d0f2c4e29b83881e81778d6176745577b1e3
Matthias Arzt <arzt@mpi-cbg.de>
2017-10-20 16:40:11 +0200

    Add ClassCopyProvider to speed up byte code used in the following commits

---
+++ src/main/java/net/imglib2/loops/ClassCopier.java
@@ -0,0 +1,98 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2016 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.loops;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+/**
+ * {@link ClassCopier} helps copying a class. The resulting class works as the original.
+ * <p>
+ * Why would you want to copy a class?
+ * <p>
+ * The problem is described in <a href="https://github.com/tpietzsch/none/">https://github.com/tpietzsch/none/</a>.
+ * The solution suggested there is to copy a specific method of a class, but copying a whole class is easier.
+ *
+ * @author Matthias Arzt
+ */
+class ClassCopier< T >
+{
+
+	private Class< ? extends T > original;
+
+	private byte[] bytes;
+
+	public ClassCopier( Class< ? extends T > original, Class< T > interfaceOfOriginal )
+	{
+		if ( !interfaceOfOriginal.isAssignableFrom( original ) || interfaceOfOriginal.equals( original ) )
+			throw new IllegalArgumentException( "\"original\" must be a implementation of interface \"interfaceOfOriginal\"" );
+		this.original = original;
+		this.bytes = classToBytes( original );
+	}
+
+	public Class< ? extends T > copy()
+	{
+		System.err.println( "Copy class: " + original );
+		@SuppressWarnings( "unchecked" )
+		Class< ? extends T > copy = ( Class< ? extends T > ) new ClassCopyLoader().bytesToClass( original.getName(), bytes );
+		return copy;
+	}
+
+	private byte[] classToBytes( Class< ? > aClass )
+	{
+		String className = aClass.getName();
+		String classAsPath = className.replace( '.', '/' ) + ".class";
+		try (DataInputStream stream = new DataInputStream( aClass.getClassLoader().getResourceAsStream( classAsPath ) ))
+		{
+			byte[] bytes = new byte[ stream.available() ];
+			stream.readFully( bytes );
+			return bytes;
+		}
+		catch ( IOException e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+
+	private static class ClassCopyLoader extends ClassLoader
+	{
+
+		private Class< ? > bytesToClass( String className, byte[] bytes )
+		{
+			Class< ? > copiedClass = super.defineClass( className, bytes, 0, bytes.length );
+			super.resolveClass( copiedClass );
+			return copiedClass;
+		}
+	}
+}
---
+++ src/main/java/net/imglib2/loops/ClassCopyProvider.java
@@ -0,0 +1,111 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2016 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.loops;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+/**
+ * @author Matthias Arzt
+ */
+class ClassCopyProvider< T >
+{
+
+	private final Map< Object, Class< ? extends T > > map = new ConcurrentHashMap<>();
+
+	private final ClassCopier< T > copier;
+
+	private final Class< ? >[] signature;
+
+	public ClassCopyProvider( Class< ? extends T > clazz, Class< T > interfaceOfClazz, Class< ? >... constructorSignature )
+	{
+		this.copier = new ClassCopier<>( clazz, interfaceOfClazz );
+		this.signature = ( constructorSignature == null || constructorSignature.length == 0 ) ?
+				assumeConstructorSignature( clazz ) : constructorSignature;
+	}
+
+	private static Class< ? >[] assumeConstructorSignature( Class< ? > clazz )
+	{
+		if ( hasDefaultConstructor( clazz ) )
+			return new Class[ 0 ];
+		Constructor< ? >[] constructors = clazz.getConstructors();
+		if ( constructors.length == 1 )
+			return constructors[ 0 ].getParameterTypes();
+		if ( constructors.length == 0 )
+			throw new IllegalArgumentException( "ClassCopyProvider: Public constructor in class (" + clazz.getName() + ") needed." );
+		throw new IllegalArgumentException( "ClassCopyProvider: Please specify constructor signature." );
+	}
+
+	private static boolean hasDefaultConstructor( Class< ? > clazz )
+	{
+		return Stream.of( clazz.getConstructors() )
+				.anyMatch( constructor -> constructor.getParameterCount() == 0 );
+	}
+
+	Class< ? extends T > classForKey( Object key )
+	{
+		return map.computeIfAbsent( key, k -> copier.copy() );
+	}
+
+	public boolean matches( Object... parameters )
+	{
+		if ( parameters.length != signature.length )
+			return false;
+		for ( int i = 0; i < signature.length; i++ )
+			if ( !signature[ i ].isInstance( parameters[ i ] ) )
+				return false;
+		return true;
+	}
+
+	public List< Class< ? > > getSignature()
+	{
+		return Arrays.asList( signature );
+	}
+
+	T newInstanceForKey( Object key, Object... parameters )
+	{
+		try
+		{
+			return classForKey( key ).getConstructor( signature ).newInstance( parameters );
+		}
+		catch ( Exception e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+}
