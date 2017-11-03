06205e92359b25b9e3cfb47a2fad8767482bd644
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-11-03 12:39:29 -0400

    make Volatile[Real|Numeric]Type default valid (consistent with BigDataViewer)

--- src/main/java/net/imglib2/type/volatiles/VolatileNumericType.java
+++ src/main/java/net/imglib2/type/volatiles/VolatileNumericType.java
@@ -11,13 +11,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,7 +38,7 @@ import net.imglib2.type.numeric.NumericType;
 
 /**
  * Something volatile that has a value and is either VALID or INVALID.
- * 
+ *
  * @author Stephan Saalfeld
  */
 public class VolatileNumericType< T extends NumericType< T > > extends AbstractVolatileNumericType< T, VolatileNumericType< T > >
@@ -50,18 +50,18 @@ public class VolatileNumericType< T extends NumericType< T > > extends AbstractV
 
 	public VolatileNumericType( final T t )
 	{
-		this( t, false );
+		this( t, true );
 	}
 
 	@Override
 	public VolatileNumericType< T > createVariable()
 	{
-		return new VolatileNumericType< T >( t.createVariable(), false );
+		return new VolatileNumericType< T >( t.createVariable(), true );
 	}
 
 	@Override
 	public VolatileNumericType< T > copy()
 	{
-		return new VolatileNumericType< T >( t.copy(), false );
+		return new VolatileNumericType< T >( t.copy(), valid );
 	}
 }
