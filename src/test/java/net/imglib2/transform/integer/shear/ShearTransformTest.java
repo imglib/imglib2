dccb262d9429e5b1f38d0c030cfab425a1f065d9
Keith Schulze <keith.schulze@gmail.com>
2018-03-04 08:42:32 +1100

    Fix mistakenly replaced word in license

--- src/test/java/net/imglib2/transform/integer/shear/ShearTransformTest.java
+++ src/test/java/net/imglib2/transform/integer/shear/ShearTransformTest.java
@@ -9,10 +9,10 @@
  * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
  * Jean-Yves Tinevez and Michael Zinsmaier.
  * %%
- * Redistribution and use in refDim and binary forms, with or without
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of refDim code must retain the above copyright notice,
+ * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
