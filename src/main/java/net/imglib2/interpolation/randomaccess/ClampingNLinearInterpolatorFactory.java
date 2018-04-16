fc7442a89801a9684cb94f9beef997f632c06449
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-04-16 14:13:31 +0200

    Move clamping interpolators from bigdataviewer-core to imglib2

---
+++ src/main/java/net/imglib2/interpolation/randomaccess/ClampingNLinearInterpolatorFactory.java
@@ -0,0 +1,85 @@
+/*
+ * #%L
+ * BigDataViewer core classes with minimal dependencies
+ * %%
+ * Copyright (C) 2012 - 2016 Tobias Pietzsch, Stephan Saalfeld, Stephan Preibisch,
+ * Jean-Yves Tinevez, HongKee Moon, Johannes Schindelin, Curtis Rueden, John Bogovic
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imglib2.interpolation.randomaccess;
+
+import net.imglib2.RandomAccessible;
+import net.imglib2.RealInterval;
+import net.imglib2.RealRandomAccess;
+import net.imglib2.Volatile;
+import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.volatiles.VolatileARGBType;
+
+/**
+ * Provides clamping n-linear interpolators for volatile and non-volatile types.
+ *
+ * @param <T>
+ *
+ * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
+ */
+public class ClampingNLinearInterpolatorFactory< T extends NumericType< T > > implements InterpolatorFactory< T, RandomAccessible< T > >
+{
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
+	@Override
+	public RealRandomAccess< T > create( final RandomAccessible< T > randomAccessible )
+	{
+		final T type = randomAccessible.randomAccess().get();
+		if ( type instanceof RealType )
+		{
+			if ( type instanceof Volatile )
+				return new ClampingNLinearInterpolatorVolatileRealType( randomAccessible );
+			else
+				return new ClampingNLinearInterpolatorRealType( randomAccessible );
+		}
+		else if ( ARGBType.class.isInstance( type ) )
+		{
+			return ( RealRandomAccess ) new NLinearInterpolatorARGB( ( RandomAccessible ) randomAccessible );
+		}
+		else if ( VolatileARGBType.class.isInstance( type ) )
+		{
+			return ( RealRandomAccess ) new ClampingNLinearInterpolatorVolatileARGB< VolatileARGBType >( ( RandomAccessible ) randomAccessible );
+		}
+		else
+			// fall back to (non-clamping) NLinearInterpolator
+			return new NLinearInterpolator<>( randomAccessible );
+	}
+
+	/**
+	 * For now, ignore the {@link RealInterval} and return
+	 * {@link #create(RandomAccessible)}.
+	 */
+	@Override
+	public RealRandomAccess< T > create( final RandomAccessible< T > randomAccessible, final RealInterval interval )
+	{
+		return create( randomAccessible );
+	}
+}
