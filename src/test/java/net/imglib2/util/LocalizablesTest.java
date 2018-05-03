db939c7ede8c27caeb485c32eec83108bf6e1b0b
Matthias Arzt <arzt@mpi-cbg.de>
2018-05-03 10:54:25 +0200

    Add Localizables utility class
    Utility class for Localizable. It has a method, that returns
    the position of a Localizable as long[] and methods to create
    a RandomAccessible<Localizable>, where each pixel value is simply
    the position of the pixel.

---
+++ src/test/java/net/imglib2/util/LocalizablesTest.java
@@ -0,0 +1,66 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2018 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imglib2.util;
+
+import net.imglib2.Localizable;
+import net.imglib2.Point;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class LocalizablesTest
+{
+	@Test
+	public void testAsLongArray() {
+		Localizable input = new Point( 5,7 );
+		long[] result = Localizables.asLongArray( input );
+		assertArrayEquals( new long[] { 5, 7 }, result );
+	}
+
+	@Test
+	public void testRandomAccessible() {
+		long[] position = { 4, 2 };
+		int n = position.length;
+		RandomAccessible< Localizable > randomAccessible = Localizables.randomAccessible( n );
+		RandomAccess< Localizable > randomAccess = randomAccessible.randomAccess();
+		assertEquals( n, randomAccessible.numDimensions() );
+		assertEquals( n, randomAccess.numDimensions() );
+		randomAccess.setPosition( position );
+		assertArrayEquals( position, Localizables.asLongArray( randomAccess.get() ) );
+	}
+}
