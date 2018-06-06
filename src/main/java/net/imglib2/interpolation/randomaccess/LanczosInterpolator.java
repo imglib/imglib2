4b287551a0377b72611e5104b5b3189a5f69e106
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2018-06-06 14:15:02 -0400

    fix 212?

--- src/main/java/net/imglib2/interpolation/randomaccess/LanczosInterpolator.java
+++ src/main/java/net/imglib2/interpolation/randomaccess/LanczosInterpolator.java
@@ -11,13 +11,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -43,7 +43,7 @@ import net.imglib2.type.numeric.RealType;
 
 /**
  * n-dimensional double-based Lanczos Interpolation
- * 
+ *
  * @author Stephan Preibisch
  * @author Stephan Saalfeld
  */
@@ -75,7 +75,7 @@ public class LanczosInterpolator< T extends RealType< T > > extends FloorOffset<
 
 	/**
 	 * Creates a new Lanczos-interpolation
-	 * 
+	 *
 	 * @param randomAccessible
 	 *            - the {@link RandomAccessible} to work on
 	 * @param alpha
@@ -141,7 +141,7 @@ public class LanczosInterpolator< T extends RealType< T > > extends FloorOffset<
 
 	final static private double[] createLanczosLUT( final int max, final int scale )
 	{
-		final double[] lut = new double[ max * scale + 2 ];
+		final double[] lut = new double[ max * scale + 22 ];
 		for ( int i = 0; i < lut.length; ++i )
 		{
 			final double x = ( double ) i / ( double ) lutScale;
