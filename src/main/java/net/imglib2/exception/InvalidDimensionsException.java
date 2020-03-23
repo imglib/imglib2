2a11c606956a89b4b4fb71c45f6600c9b52d314b
tpietzsch <tobias.pietzsch@gmail.com>
2020-03-23 16:57:23 +0100

    Add Dimensions.verify(int...) and deprecate CellImg.verifyDimensions(int[])

--- src/main/java/net/imglib2/exception/InvalidDimensionsException.java
+++ src/main/java/net/imglib2/exception/InvalidDimensionsException.java
@@ -1,5 +1,7 @@
 package net.imglib2.exception;
 
+import net.imglib2.util.Util;
+
 public class InvalidDimensionsException extends IllegalArgumentException
 {
 	private final long[] dimensions;
@@ -8,6 +10,11 @@ public class InvalidDimensionsException extends IllegalArgumentException
 	{
 		super( message );
 		this.dimensions = dimensions.clone();
+	}
+
+	public InvalidDimensionsException( final int[] dimensions, final String message )
+	{
+		this( Util.int2long( dimensions ), message );
 	}
 
 	public long[] getDimenionsCopy()
