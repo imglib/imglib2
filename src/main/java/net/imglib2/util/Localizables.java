0c35e67d26a037220a79612e11f1ba9c7705ab8f
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2022-04-19 13:36:24 -0400

    feat: add Localizables.realRandomAccessible analog to randomAccessible       for real coordinates
    unlike a FunctionRealRandomAccessible, the resulting RealReandomAccessible
    returns the RealRandomAccess as a value instead of a separate instance
    with the same coordinates, this could be more efficient but has similar
    benchmark performance as a FunctionRealRandomAccessible using a RealPoint

--- src/main/java/net/imglib2/util/Localizables.java
+++ src/main/java/net/imglib2/util/Localizables.java
@@ -41,7 +41,11 @@ import net.imglib2.Point;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealInterval;
 import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.RealRandomAccess;
+import net.imglib2.RealRandomAccessible;
 import net.imglib2.view.Views;
 
 public class Localizables
@@ -73,6 +77,18 @@ public class Localizables
 	public static RandomAccessibleInterval< Localizable > randomAccessibleInterval( final Interval interval )
 	{
 		return Views.interval( randomAccessible( interval.numDimensions() ), interval );
+	}
+
+	/**
+	 * Create an n-dimensional {@link RealRandomAccessible} whose value domain is
+	 * its source domain.
+	 *
+	 * @param n
+	 * @return
+	 */
+	public static RealRandomAccessible< RealLocalizable > realRandomAccessible( final int n )
+	{
+		return new RealLocationRealRandomAccessible( n );
 	}
 
 	/**
@@ -208,6 +224,54 @@ public class Localizables
 		public RandomAccess< Localizable > copy()
 		{
 			return new LocationRandomAccess( this );
+		}
+	}
+
+	private static class RealLocationRealRandomAccessible extends AbstractEuclideanSpace implements RealRandomAccessible< RealLocalizable >
+	{
+		public RealLocationRealRandomAccessible( final int n )
+		{
+			super( n );
+		}
+
+		@Override
+		public RealLocationRealRandomAccess realRandomAccess()
+		{
+			return new RealLocationRealRandomAccess( n );
+		}
+
+		@Override
+		public RealLocationRealRandomAccess realRandomAccess( final RealInterval interval )
+		{
+			return realRandomAccess();
+		}
+	}
+
+	/**
+	 * A RandomAccess that returns it's current position as value.
+	 */
+	private static class RealLocationRealRandomAccess extends RealPoint implements RealRandomAccess< RealLocalizable >
+	{
+		public RealLocationRealRandomAccess( final int n )
+		{
+			super( n );
+		}
+
+		public RealLocationRealRandomAccess( final RealLocalizable initialPosition )
+		{
+			super( initialPosition );
+		}
+
+		@Override
+		public RealLocationRealRandomAccess get()
+		{
+			return this;
+		}
+
+		@Override
+		public RealLocationRealRandomAccess copy()
+		{
+			return new RealLocationRealRandomAccess( this );
 		}
 	}
 }
