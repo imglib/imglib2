9f2f6cfd08e2f76f01f3628326f7bc2679856590
tpietzsch <tobias.pietzsch@gmail.com>
2022-05-31 14:15:56 +0200

    fix typo

--- src/main/java/net/imglib2/interpolation/randomaccess/ClampingNLinearInterpolatorVolatileRealType.java
+++ src/main/java/net/imglib2/interpolation/randomaccess/ClampingNLinearInterpolatorVolatileRealType.java
@@ -40,7 +40,7 @@ import net.imglib2.type.volatiles.AbstractVolatileRealType;
 
 /**
  * N-linear interpolator for {@link RealType} values with overflow check.
- * Interpoalted values are clamped to the range {@link RealType#getMinValue()},{@link RealType#getMaxValue()}.
+ * Interpolated values are clamped to the range {@link RealType#getMinValue()},{@link RealType#getMaxValue()}.
  *
  * @param <T>
  *
