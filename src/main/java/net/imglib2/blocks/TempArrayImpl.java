1d1f8d0ca757dd8b79279798bdc5cbbcbde4a36b
tpietzsch <tobias.pietzsch@gmail.com>
2023-05-22 10:49:07 +0200

    Only keep WeakRef to re-usable temp arrays

--- src/main/java/net/imglib2/blocks/TempArrayImpl.java
+++ src/main/java/net/imglib2/blocks/TempArrayImpl.java
@@ -1,22 +1,28 @@
 package net.imglib2.blocks;
 
+import java.lang.ref.WeakReference;
+
 class TempArrayImpl< T > implements TempArray< T >
 {
 	private final PrimitiveTypeProperties< T, ? > props;
 
-	private T array;
+	private WeakReference< T > arrayRef;
 
 	TempArrayImpl( PrimitiveTypeProperties< T, ? > props )
 	{
+		arrayRef = new WeakReference<>( null );
 		this.props = props;
-		array = props.allocate( 0 );
 	}
 
 	@Override
 	public T get( final int minSize )
 	{
-		if ( props.length( array ) < minSize )
+		T array = arrayRef.get();
+		if ( array == null || props.length( array ) < minSize )
+		{
 			array = props.allocate( minSize );
+			arrayRef = new WeakReference<>( array );
+		}
 		return array;
 	}
 
