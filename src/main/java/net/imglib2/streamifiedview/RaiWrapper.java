57ce721f19f27fb2d428a452a2d6d66f3c42f8eb
Michael Innerberger <innerbergerm@hhmi.org>
2023-10-12 09:17:11 -0400

    Use .view() instead of explicit instantiation

--- src/main/java/net/imglib2/streamifiedview/RaiWrapper.java
+++ src/main/java/net/imglib2/streamifiedview/RaiWrapper.java
@@ -14,16 +14,16 @@ public class RaiWrapper<T> implements StreamifiedView<T> {
 
 	@Override
 	public StreamifiedView<T> expandValue(final T value, long... border) {
-		return new RaiWrapper<>(Views.expandValue(rai, value, border));
+		return Views.expandValue(rai, value, border).view();
 	}
 
 	@Override
 	public StreamifiedView<T> permute(final int from, final int to) {
-		return new RaiWrapper<>(Views.permute(rai, from, to));
+		return Views.permute(rai, from, to).view();
 	}
 
 	@Override
 	public StreamifiedView<T> translate(long... translation) {
-		return new RaiWrapper<>(Views.translate(rai, translation));
+		return Views.translate(rai, translation).view();
 	}
 }
