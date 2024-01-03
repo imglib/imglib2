ddd9f24bbd36aae99d803092277aad516f810b0b
Michael Innerberger <innerbergerm@hhmi.org>
2024-01-03 16:12:28 -0500

    Swap implementation of RealSum to Neumaier's algorithm

--- src/main/java/net/imglib2/util/RealSum.java
+++ src/main/java/net/imglib2/util/RealSum.java
@@ -39,99 +39,53 @@ package net.imglib2.util;
  * summing up a very large number of double precision numbers. Numerical
  * problems occur when a small number is added to an already very large sum. In
  * such case, the reduced accuracy of the very large number may lead to the
- * small number being entirely ignored. The method here stores and updates
- * intermediate sums for all power of two elements such that the final sum can
- * be generated from intermediate sums that result from equal number of
- * summands.
- * 
- * @author Stephan Saalfeld
+ * small number being entirely ignored. The method here is Neumaier's
+ * improvement of the Kahan summation algorithm.
+ * See <a href="https://en.wikipedia.org/wiki/Kahan_summation_algorithm">this
+ * Wikipedia article</a> for details.
+ *
+ * @author Michael Innerberger, Stephan Saalfeld
  */
-public class RealSum
-{
-	protected boolean[] flags;
-
-	protected double[] sums;
+public class RealSum {
+	private double sum = 0.0;
+	private double compensation = 0.0;
 
 	/**
-	 * Create a new {@link RealSum}. The fields for intermediate sums is
-	 * initialized with a single element and expanded on demand as new elements
-	 * are added.
+	 * Create a new {@link RealSum} initialized to zero.
 	 */
-	public RealSum()
-	{
-		flags = new boolean[ 1 ];
-		sums = new double[ 1 ];
+	public RealSum() {}
+
+	/**
+	 * Create a new {@link RealSum} initialized to zero. This constructor
+	 * was used in a previous version of {@link RealSum} and is kept for
+	 * backwards compatibility.
+	 *
+	 * @param capacity unused
+	 */
+	@Deprecated
+	public RealSum(final int capacity) {
+		this();
 	}
 
 	/**
-	 * Create a new {@link RealSum}. The fields for intermediate sums is
-	 * initialized with a given number of elements and will only be expanded on
-	 * demand as new elements are added and the number of existing elements is
-	 * not sufficient. This may be faster in cases where the required number of
-	 * elements is known in prior.
-	 * 
-	 * @param capacity
+	 * Get the current sum.
 	 */
-	public RealSum( final int capacity )
-	{
-		final int ldu = Util.ldu( capacity ) + 1;
-		flags = new boolean[ ldu ];
-		sums = new double[ ldu ];
+	public double getSum() {
+		return sum + compensation;
 	}
 
 	/**
-	 * Get the current sum by summing up all intermediate sums. Do not call this
-	 * method repeatedly when the sum has not changed.
+	 * Add an element to the sum.
+	 *
+	 * @param value the summand to be added
 	 */
-	final public double getSum()
-	{
-		double sum = 0;
-		for ( final double s : sums )
-			sum += s;
-
-		return sum;
-	}
-
-	final protected void expand( final double s )
-	{
-		final double[] oldSums = sums;
-		sums = new double[ oldSums.length + 1 ];
-		System.arraycopy( oldSums, 0, sums, 0, oldSums.length );
-		sums[ oldSums.length ] = s;
-
-		final boolean[] oldFlags = flags;
-		flags = new boolean[ sums.length ];
-		System.arraycopy( oldFlags, 0, flags, 0, oldFlags.length );
-		flags[ oldSums.length ] = true;
-	}
-
-	/**
-	 * Add an element to the sum. All intermediate sums are updated and the
-	 * capacity is increased on demand.
-	 * 
-	 * @param a
-	 *            the summand to be added
-	 */
-	final public void add( final double a )
-	{
-		int i = 0;
-		double s = a;
-		try
-		{
-			while ( flags[ i ] )
-			{
-				flags[ i ] = false;
-				s += sums[ i ];
-				sums[ i ] = 0.0;
-				++i;
-			}
-			flags[ i ] = true;
-			sums[ i ] = s;
-			return;
+	public void add(double value) {
+		double t = sum + value;
+		if (Math.abs(sum) >= Math.abs(value)) {
+			compensation += (sum - t) + value;
+		} else {
+			compensation += (value - t) + sum;
 		}
-		catch ( final IndexOutOfBoundsException e )
-		{
-			expand( s );
-		}
+		sum = t;
 	}
 }
