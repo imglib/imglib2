7c343aa6c3757b872bcb2680153e1f3f8deff8bb
Michael Innerberger <innerbergerm@hhmi.org>
2024-03-12 17:32:15 -0400

    Add benchmark for RealSum

---
+++ src/test/java/net/imglib2/util/RealSumBenchmark.java
@@ -0,0 +1,94 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2023 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imglib2.util;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.stream.DoubleStream;
+
+/**
+ * Benchmark for {@link RealSum}, comparing it with naive double summation.
+ * 
+ * @author Michael Innerberger
+ */
+public class RealSumBenchmark {
+	@Benchmark
+	public static void sumNaive(Blackhole blackhole, RandomValues values) {
+		double sum = 0.0;
+		for (double d : values.get()) {
+			sum += d;
+		}
+		blackhole.consume(sum);
+	}
+
+	@Benchmark
+	public static void sumRealSum(Blackhole blackhole, RandomValues values) {
+		RealSum sum = new RealSum();
+		for (double d : values.get()) {
+			sum.add(d);
+		}
+		blackhole.consume(sum.getSum());
+	}
+
+	@State(Scope.Benchmark)
+	public static class RandomValues {
+		final int N = 10_000_000;
+		final double[] values = DoubleStream.generate(Math::random).limit(N).toArray();
+
+		public double[] get() {
+			return values;
+		}
+	}
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(RealSumBenchmark.class.getSimpleName())
+				.forks(0)
+				.warmupIterations(4)
+				.measurementIterations(8)
+				.warmupTime(TimeValue.milliseconds(100))
+				.measurementTime(TimeValue.milliseconds(100))
+				.build();
+		new Runner(opt).run();
+	}
+}
