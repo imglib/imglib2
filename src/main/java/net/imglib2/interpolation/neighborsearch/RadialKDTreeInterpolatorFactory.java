89da3dff53c44522c69baee020d44fba1ad4ef45
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2024-06-12 08:56:04 -0400

    style: apply imglib2 style

--- src/main/java/net/imglib2/interpolation/neighborsearch/RadialKDTreeInterpolatorFactory.java
+++ src/main/java/net/imglib2/interpolation/neighborsearch/RadialKDTreeInterpolatorFactory.java
@@ -16,30 +16,32 @@ import net.imglib2.type.numeric.NumericType;
  * The contribution of each point is weighted according to a function of its
  * squared radius.
  */
-public class RadialKDTreeInterpolatorFactory<T extends NumericType<T>> implements InterpolatorFactory<T, KDTree<T>> {
-
+public class RadialKDTreeInterpolatorFactory< T extends NumericType< T > > implements InterpolatorFactory< T, KDTree< T > >
+{
 	protected final double maxRadius;
+
 	protected final DoubleUnaryOperator squaredRadiusFunction;
+
 	protected final T val;
 
 	public RadialKDTreeInterpolatorFactory(
-			final DoubleUnaryOperator squaredRadiusFunction, final double maxRadius, T t) {
-
+			final DoubleUnaryOperator squaredRadiusFunction,
+			final double maxRadius, final T t )
+	{
 		this.maxRadius = maxRadius;
 		this.squaredRadiusFunction = squaredRadiusFunction;
 		this.val = t;
 	}
 
 	@Override
-	public RadialKDTreeInterpolator<T> create(final KDTree<T> tree) {
-
-		return new RadialKDTreeInterpolator<T>(tree, squaredRadiusFunction, maxRadius, val);
+	public RadialKDTreeInterpolator< T > create( final KDTree< T > tree )
+	{
+		return new RadialKDTreeInterpolator< T >( tree, squaredRadiusFunction, maxRadius, val );
 	}
 
 	@Override
-	public RealRandomAccess<T> create(final KDTree<T> tree, final RealInterval interval) {
-
-		return create(tree);
+	public RealRandomAccess< T > create( final KDTree< T > tree, final RealInterval interval )
+	{
+		return create( tree );
 	}
-
 }
